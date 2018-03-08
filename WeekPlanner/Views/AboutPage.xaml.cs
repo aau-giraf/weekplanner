aa49dabc624354b53060a3fac7953ec9bd3f30b4
Kasper Dissing Bargsteen <kasper@bargsteen.com>
2018-03-08 15:41:51 +0100

    First

---
+++ WeekPlanner/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace WeekPlanner
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WeekPlanner/Views/LoginPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Schema;
+using Xamarin.Forms;
+
+namespace WeekPlanner.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoginButtonClicked(object sender, EventArgs e)
+        {
+            string username = UsernameEntry.Text;
+            string password = PasswordEntry.Text;
+            
+        }
+    }
+}
