d32848a3e7924fc51154bf6a11fe25a14db8fa2b
Randers114 <srande15@student.aau.dk>
2018-03-15 16:50:42 +0100

    Made enum to recognize UserMode. Added button to change between modes.

--- WeekPlanner/ViewModels/UserModeSwitchViewModel.cs
+++ WeekPlanner/ViewModels/UserModeSwitchViewModel.cs
@@ -12,30 +12,50 @@ namespace WeekPlanner.ViewModels
 {
     class UserModeSwitchViewModel : ViewModelBase
     {
-		private GirafUserDTO _citizen;
 
+        private GirafUserDTO _citizen;
+        public enum UserMode { Guardian, Citizen };
+        private UserMode _userMode;
+        public GirafUserDTO Citizen
+        {
+            get => _citizen;
+            set
+            {
+                _citizen = value;
+                RaisePropertyChanged(() => Citizen);
+            }
+        }
+
+        public UserMode Mode
+        {
+            get => _userMode;
+            set
+            {
+                _userMode = value;
+                RaisePropertyChanged(() => Mode);
+            }
+        }
+
+        
+
+        public UserModeSwitchViewModel()
+        {
+            Mode = UserMode.Guardian;
+        }
 
         public ICommand SwitchUserModeCommand => new Command(() => SwitchUserMode());
 
-
         private void SwitchUserMode()
         {
-            
+            if (_userMode == UserMode.Guardian)
+            {
+                Mode = UserMode.Citizen;
+
+            }
+            else
+            {
+                Mode = UserMode.Guardian;
+            }
         }
-        //public GirafUserDTO Citizen
-        //{
-        //	get => _citizen;
-        //	set
-        //	{
-        //		_citizen = new GirafUserDTO() { Username = "Kurt" };
-
-        //	}
-        //}
-
-
-        //public override Task InitializeAsync(object navigationData)
-        //{
-        //	return base.InitializeAsync(navigationData);
-        //}
     }
 }
