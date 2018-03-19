9d71366888ed3d435a05223d467660962e76b16d
CGrosen <cgrose15@student.aau.dk>
2018-03-19 10:45:07 +0100

    Added a new view and viewmodel called ModifySchedule

---
+++ WeekPlanner/ViewModels/ModifyScheduleViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using IO.Swagger.Model;
+using System.Threading.Tasks;
+using WeekPlanner.ViewModels.Base;
+using Xamarin.Forms;
+using WeekPlanner.Services.Navigation;
+
+namespace WeekPlanner.ViewModels
+{
+    public class ModifyScheduleViewModel : ViewModelBase
+    {
+        /*
+        public ModifyScheduleViewModel(INavigationService navigationService, Week schedule) : base(navigationService)
+        {
+            //TODO: input the schedules activities within the view
+        }
+        */
+
+        public ModifyScheduleViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            
+        }
+
+
+    }
+}
