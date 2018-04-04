3298419aa67e88419d7febe308af1769e6c8c397
Kasper Dissing Bargsteen <kbargs15@student.aau.dk>
2018-04-04 09:33:35 +0200

    Added a few tests to pictogramsearchviewmodel

---
+++ WeekPlanner.Tests/UnitTests/ViewModels/PictogramSearchViewModelTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using AutoFixture;
+using IO.Swagger.Model;
+using Moq;
+using WeekPlanner.Services.Navigation;
+using WeekPlanner.ViewModels;
+using WeekPlanner.ViewModels.Base;
+using Xamarin.Forms;
+using Xunit;
+
+namespace WeekPlanner.Tests.UnitTests.ViewModels
+{
+    public class PictogramSearchViewModelTests : ViewModelTestsBase
+    {
+        // FuncNavn_Conditions_Result
+        [Fact]
+        public void ItemTappedCommand_Executed_SendsMessage()
+        {
+            // Arrange
+            var messageReceived = false;
+            MessagingCenter.Subscribe<PictogramSearchViewModel, PictogramDTO>(this, MessageKeys.PictoSearchChosenItem,
+                (sender, args) => messageReceived = true);
+            var pictogramDTO = Fixture.Create<PictogramDTO>();
+            
+            var sut = Fixture.Create<PictogramSearchViewModel>();
+            
+            // Act
+            sut.ItemTappedCommand.Execute(pictogramDTO);
+            
+            // Assert
+            Assert.True(messageReceived);
+        }
+
+        [Fact]
+        public void ItemTappedCommand_Executed_InvokesNavigationPop()
+        {
+            
+            // Arrange
+            var navServiceMock = Fixture.Freeze<Mock<INavigationService>>();
+            var pictogramDTO = Fixture.Create<PictogramDTO>();
+            var sut = Fixture.Create<PictogramSearchViewModel>();
+            
+            // Act
+            sut.ItemTappedCommand.Execute(pictogramDTO);
+            
+            // Assert
+            navServiceMock.Verify(n => n.PopAsync(), Times.Once);
+            navServiceMock.Verify
+        }
+    }
+}
