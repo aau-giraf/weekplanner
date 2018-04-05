c83ac3424e0a2b5f8155db6b7e5ccd87483f1886
Kia Husum Aardestrup <kaarde15@student.aau.dk>
2018-04-05 13:57:12 +0200

    New test class for WeekPlannerViewModel

---
+++ WeekPlanner.Tests/ViewModels/WeekPlannerViewModelTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xunit;
+using AutoFixture;
+using Moq;
+using WeekPlanner.ViewModels;
+using WeekPlanner.ViewModels.Base;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using IO.Swagger.Model;
+using System.Collections.Generic;
+using WeekPlanner.Services.Navigation;
+
+namespace WeekPlanner.Tests.ViewModels
+{
+    public class WeekPlannerViewModelTests : UnitTests.ViewModels.ViewModelTestsBase
+    {
+        public WeekPlannerViewModelTests()
+        {
+        }
+
+        [Theory]
+        public void EditModeEnabled_UnToggle_VariableChanched()
+        {
+            //Arrange
+            var sut = Fixture.Create<WeekPlannerViewModel>();
+            sut.EditModeEnabled = true;
+
+            bool result = false;
+
+            //Act
+            sut.ToggleEditModeCommand = result;
+
+            //Assert
+        }
+    }
+
+}
