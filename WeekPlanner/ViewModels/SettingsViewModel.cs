2e487de875e08a18d282dd1382f794d34026f6a0
CGrosen <cgrose15@student.aau.dk>
2018-04-17 10:33:02 +0200

    Rebase
    Started Implementing the settings view, but needed to rebase

---
+++ WeekPlanner/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using IO.Swagger.Api;
+using IO.Swagger.Client;
+using IO.Swagger.Model;
+using WeekPlanner.Helpers;
+using WeekPlanner.Services.Login;
+using WeekPlanner.Services.Navigation;
+using WeekPlanner.Services.Settings;
+using WeekPlanner.Validations;
+using WeekPlanner.ViewModels.Base;
+
+namespace WeekPlanner.ViewModels
+{
+    public class SettingsViewModel : ViewModelBase
+    {
+        private readonly ISettingsService _settingsService;
+
+        public SettingsViewModel(ISettingsService settingsService, INavigationService navigationService) : base(navigationService)
+        {
+            _settingsService = settingsService;
+        }
+    }
+}
