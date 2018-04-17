b2651d92f2a8a764bf978f172097a6612630ae97
Christopher Nielsen <Nillerduden@hotmail.com>
2018-04-17 16:17:32 +0200

    Introduced orientation lock for IOS. This is not tested though.

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -1,13 +1,28 @@
 ﻿using FFImageLoading.Forms.Touch;
 using Foundation;
+using ObjCRuntime;
 using UIKit;
+using WeekPlanner.Views;
 
 namespace WeekPlanner.iOS
 {
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
-        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+		public override UIInterfaceOrientationMask GetSupportedInterfaceOrientations(UIApplication application, [Transient] UIWindow forWindow)
+		{
+			var mainPage = Xamarin.Forms.Application.Current.MainPage;
+			var last = mainPage.Navigation.NavigationStack.Count - 1;
+
+			if (mainPage.Navigation.NavigationStack[last] is WeekPlannerPage)
+			{
+				return UIInterfaceOrientationMask.AllButUpsideDown;
+			}
+
+			return UIInterfaceOrientationMask.Portrait;
+		}
+
+		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
             CachedImageRenderer.Init();
@@ -15,5 +30,7 @@ namespace WeekPlanner.iOS
             LoadApplication(new App());
             return base.FinishedLaunching(app, options);
         }
+
+
     }
 }
