1074935fd80b3c32605d522b75df799bcb55de74
Lau Kondrup <lkondr13@student.aau.dk>
2018-04-18 19:33:49 +0200

    Clean unused MessageKeys

--- WeekPlanner/ViewModels/Base/MessageKeys.cs
+++ WeekPlanner/ViewModels/Base/MessageKeys.cs
@@ -5,16 +5,7 @@ namespace WeekPlanner.ViewModels.Base
     {
         public const string LoginSucceeded = "LoginSucceeded";
         public const string LoginFailed = "LoginFailed";
-        public const string CitizenListRetrievalFailed = "CitizenListRetrievalFailed";
-        public const string ServerError = "ServerError";
-        public const string RetrieveWeekPlanFailed = "RetrieveWeekPlanFailed";
-        public const string RequestSucceeded = "RequestSucceeded";
         public const string RequestFailed = "RequestFailed";
-        public const string ScheduleSaveRequest = "ScheduleSaveRequest";
-        public const string ScheduleSaveFailed = "SaveFailed";
-        public const string ScheduleSaveSucceeded = "SaveSucceeded";
-        public const string ScheduleUpdateFailed = "UpdateFailed";
-        public const string ScheduleUpdateSucceeded = "UpdateSucceeded";
         public const string PictoSearchChosenItem = "PictoSearchChosenItem";
         public const string DeleteActivity = "DeleteActivity";
     }
