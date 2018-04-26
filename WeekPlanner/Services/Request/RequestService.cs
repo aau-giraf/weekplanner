1bdb731d6f6a1d40e4a2ddaac8be1ced78b0c7f1
Casper Thomsen <casper.thomsen100@gmail.com>
2018-04-26 15:07:22 +0200

    Fixed exception from CitizenScheduleViewModel

--- WeekPlanner/Services/Request/RequestService.cs
+++ WeekPlanner/Services/Request/RequestService.cs
@@ -8,68 +8,72 @@ using Xamarin.Forms;
 
 namespace WeekPlanner.Services.Request
 {
-    public class RequestService : IRequestService
-    {
-        private readonly IDialogService _dialogService;
+	public class RequestService : IRequestService
+	{
+		private readonly IDialogService _dialogService;
 
-        public RequestService(IDialogService dialogService)
-        {
-            _dialogService = dialogService;
-        }
+		public RequestService(IDialogService dialogService)
+		{
+			_dialogService = dialogService;
+		}
 
-        public async Task SendRequestAndThenAsync<TS, TR>(TS sender, Func<Task<TR>> requestAsync, Func<TR, Task> onSuccessAsync,
-            Func<Task> onExceptionAsync = null,
-            Func<Task> onRequestFailedAsync = null,
-            string exceptionErrorMessageKey = MessageKeys.RequestFailed,
-            string requestFailedMessageKey = MessageKeys.RequestFailed) where TS : class
-        {
-            // TODO: Check for internet connection first.
-            dynamic result;
-            try
-            {
-                result = await requestAsync.Invoke();
-            }
-            catch (ApiException)
-            {
-                if (onExceptionAsync != null)
-                {
-                    await onExceptionAsync.Invoke();
-                } else {
-                    var friendlyErrorMessage = ErrorCodeHelper.ToFriendlyString(ResponseString.ErrorKeyEnum.Error);
-                    await _dialogService.ShowAlertAsync(message: friendlyErrorMessage, title: "Fejl");
-                }
-                return;
-            }
+		public async Task SendRequestAndThenAsync<TS, TR>(TS sender, Func<Task<TR>> requestAsync, Func<TR, Task> onSuccessAsync,
+			Func<Task> onExceptionAsync = null,
+			Func<Task> onRequestFailedAsync = null,
+			string exceptionErrorMessageKey = MessageKeys.RequestFailed,
+			string requestFailedMessageKey = MessageKeys.RequestFailed) where TS : class
+		{
+			// TODO: Check for internet connection first.
+			dynamic result;
+			try
+			{
+				result = await requestAsync.Invoke();
+			}
+			catch (ApiException)
+			{
+				if (onExceptionAsync != null)
+				{
+					await onExceptionAsync.Invoke();
+				}
+				else
+				{
+					var friendlyErrorMessage = ErrorCodeHelper.ToFriendlyString(ResponseString.ErrorKeyEnum.Error);
+					await _dialogService.ShowAlertAsync(message: friendlyErrorMessage, title: "Fejl");
+				}
+				return;
+			}
 
-            if (result.Success == true)
-            {
-                await onSuccessAsync.Invoke(result);
-            }
-            else
-            {
-                if (onRequestFailedAsync != null)
-                {
-                    await onRequestFailedAsync.Invoke();
-                } else {
-                    var friendlyErrorMessage = ErrorCodeHelper.ToFriendlyString(result.ErrorKey);
-                }
-            }
-        }
+			if (result.Success == true)
+			{
+				await onSuccessAsync.Invoke(result);
+			}
+			else
+			{
+				if (onRequestFailedAsync != null)
+				{
+					await onRequestFailedAsync.Invoke();
+				}
+				else
+				{
+					var friendlyErrorMessage = ErrorCodeHelper.ToFriendlyString();
+				}
+			}
+		}
 
-        public async Task SendRequestAndThenAsync<TS, TR>(TS sender, Func<Task<TR>> requestAsync,
-            Action<TR> onSuccess,
-            Func<Task> onExceptionAsync = null,
-            Func<Task> onRequestFailedAsync = null,
-            string exceptionErrorMessageKey = MessageKeys.RequestFailed,
-            string requestFailedMessageKey = MessageKeys.RequestFailed) where TS : class
-        {
-            async Task OnSuccessAsync(TR result)
-            {
-                onSuccess.Invoke(result);
-            }
-               
-            await SendRequestAndThenAsync(sender, requestAsync, OnSuccessAsync,
-                onExceptionAsync, onRequestFailedAsync, exceptionErrorMessageKey, requestFailedMessageKey);
-        }
-    }
+		public async Task SendRequestAndThenAsync<TS, TR>(TS sender, Func<Task<TR>> requestAsync,
+			Action<TR> onSuccess,
+			Func<Task> onExceptionAsync = null,
+			Func<Task> onRequestFailedAsync = null,
+			string exceptionErrorMessageKey = MessageKeys.RequestFailed,
+			string requestFailedMessageKey = MessageKeys.RequestFailed) where TS : class
+		{
+			async Task OnSuccessAsync(TR result)
+			{
+				onSuccess.Invoke(result);
+			}
+
+			await SendRequestAndThenAsync(sender, requestAsync, OnSuccessAsync,
+				onExceptionAsync, onRequestFailedAsync, exceptionErrorMessageKey, requestFailedMessageKey);
+		}
+	}
 }
