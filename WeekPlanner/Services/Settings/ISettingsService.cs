19fab1ffa90d376013ccfecdbe28dc81c1ed5cb4
Kasper Dissing Bargsteen <kbargs15@student.aau.dk>
2018-04-26 22:29:02 +0200

    Created appsettings.json

--- WeekPlanner/Services/Settings/ISettingsService.cs
+++ WeekPlanner/Services/Settings/ISettingsService.cs
@@ -4,6 +4,7 @@ namespace WeekPlanner.Services.Settings
 {
     public interface ISettingsService
     {
+        string BaseEndpoint { get; set; }
         bool UseMocks { get; set; }
         string GuardianAuthToken { get; set; }
         string CitizenAuthToken { get; set; }
