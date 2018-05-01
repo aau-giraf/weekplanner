222f7a13439814a393b47d592243edb2cbc0ed51
Lau Kondrup <lkondr13@student.aau.dk>
2018-05-01 10:20:31 +0200

    Clear username and password after logging in

--- WeekPlanner/ViewModels/LoginViewModel.cs
+++ WeekPlanner/ViewModels/LoginViewModel.cs
@@ -62,7 +62,10 @@ namespace WeekPlanner.ViewModels
             {
                 bool enableGuardianMode = true;
                 await _loginService.LoginAndThenAsync(
-                    () => NavigationService.PopAsync(enableGuardianMode),
+                    () => {
+                        ClearUsernameAndPasswordFields();
+                        return NavigationService.PopAsync(enableGuardianMode);
+                    },
                     UserType.Guardian, 
                     Username.Value, 
                     Password.Value
@@ -71,7 +74,10 @@ namespace WeekPlanner.ViewModels
             else
             {
                 await _loginService.LoginAndThenAsync(
-                    () => NavigationService.NavigateToAsync<ChooseCitizenViewModel>(),
+                    () => {
+                        ClearUsernameAndPasswordFields();
+                        return NavigationService.NavigateToAsync<ChooseCitizenViewModel>();
+                    },
                     UserType.Guardian, 
                     Username.Value, 
                     Password.Value
