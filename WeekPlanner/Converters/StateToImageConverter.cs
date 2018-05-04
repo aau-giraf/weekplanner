8f5277389fa9cf28585c6437a26c8df93f40866f
Lau Kondrup <lkondr13@student.aau.dk>
2018-05-04 15:41:53 +0200

    Activity state image

---
+++ WeekPlanner/Converters/StateToImageConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+using static IO.Swagger.Model.ActivityDTO;
+
+namespace WeekPlanner.Converters
+{
+    public class StateToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            StateEnum state = (StateEnum)value;
+
+            switch(state) {
+                case StateEnum.Canceled:
+                    return ImageSource.FromFile("icon_cross.png");
+                case StateEnum.Completed:
+                    return ImageSource.FromFile("icon_checkmark.png");
+                default:
+                    return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
