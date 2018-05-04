868cb73e327c71ae68846905e13e93dda570c770
Lau Kondrup <lkondr13@student.aau.dk>
2018-05-04 23:16:22 +0200

    Extract Weekday color settings to a class for better separation of concerns.
    Quite a bit of refactorings to weekday color settings
    
    RequestService now has a SendRequest without a onSuccess. It takes a Task instead of Func<Task> since we only ever use Task.

--- WeekPlanner/ViewModels/SettingsViewModel.cs
+++ WeekPlanner/ViewModels/SettingsViewModel.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using IO.Swagger.Api;
 using IO.Swagger.Model;
+using WeekPlanner.Models;
 using WeekPlanner.Services;
 using WeekPlanner.Services.Login;
 using WeekPlanner.Services.Navigation;
@@ -21,6 +22,8 @@ namespace WeekPlanner.ViewModels
         private readonly IRequestService _requestService;
         private readonly IDialogService _dialogService;
         private readonly IUserApi _userApi;
+
+        public WeekdayColors WeekdayColors { get; set; }
 
         private GirafUserDTO _girafCitizen;
 
@@ -99,194 +102,6 @@ namespace WeekPlanner.ViewModels
                 UpdateSettingsAsync();
             }
         }
-        #region Weekplan Colours
-        Dictionary<string, Color> _weekdayColorsDict = new Dictionary<string, Color>
-        {
-            {"Aqua", Color.Aqua},
-            {"Sort", Color.Black},
-            {"Blå", Color.FromHex("#0017ff")},
-            {"Fucshia", Color.Fuchsia},
-            {"Grå", Color.Gray},
-            {"Grøn", Color.FromHex("#067700")},
-            {"Lime", Color.Lime},
-            {"Maroon", Color.Maroon},
-            {"Navy", Color.Navy},
-            {"Oliven", Color.Olive},
-            {"Lilla", Color.FromHex("#8c1086")},
-            {"Rød", Color.FromHex("#ff0102")},
-            {"Sølv", Color.Silver},
-            {"Teal", Color.Teal},
-            {"Hvid", Color.FromHex("#ffffff")},
-            {"Gul", Color.FromHex("#ffdd00")},
-            {"Orange", Color.FromHex("#ff7f00")}
-        };
-
-        public List<string> WeekdayColors => _weekdayColorsDict.Keys.ToList();
-
-        private void UpdateSettings(string key, Color value, int day)
-        {
-            var weekPlanTheme = WeekdayHexColors;
-            _settingsService.CurrentCitizenSettingDTO.WeekDayColors = _settingsService.CurrentCitizenSettingDTO.WeekDayColors.OrderBy(wd => wd.Day).ToList();
-            _settingsService.CurrentCitizenSettingDTO.WeekDayColors[day].HexColor = ColorToHex(value);
-            App.Current.Resources[key] = value;
-            UpdateSettingsAsync();
-        }
-
-        public static string ColorToHex(Color color)
-        {
-            var red = (int)(color.R * 255);
-            var green = (int)(color.G * 255);
-            var blue = (int)(color.B * 255);
-            var alpha = (int)(color.A * 255);
-            var hex = $"#{alpha:X2}{red:X2}{green:X2}{blue:X2}";
-
-            return hex;
-        }
-
-        public string[] WeekdayHexColors
-        {
-            get
-            {
-                return new string[7]
-                {
-                    ColorToHex(_weekdayColorsDict[MondaySelectedColor]),
-                    ColorToHex(_weekdayColorsDict[TuesdaySelectedColor]),
-                    ColorToHex(_weekdayColorsDict[WednesdaySelectedColor]),
-                    ColorToHex(_weekdayColorsDict[ThursdaySelectedColor]),
-                    ColorToHex(_weekdayColorsDict[FridaySelectedColor]),
-                    ColorToHex(_weekdayColorsDict[SaturdaySelectedColor]),
-                    ColorToHex(_weekdayColorsDict[SundaySelectedColor])
-                };
-            }
-        }
-
-        string _mondaySelectedColor = "Grøn";
-
-        public string MondaySelectedColor
-        {
-            get { return _weekdayColorsDict.First(color => color.Value == Color.FromHex(_settingsService.CurrentCitizenSettingDTO.WeekDayColors[0].HexColor)).Key; }
-            set
-            {
-                RaisePropertyChanged(() => MondayColorSelected);
-                _mondaySelectedColor = value;
-                UpdateSettings("MondayColor", _weekdayColorsDict[value], 0);
-            }
-        }
-
-        public Color MondayColorSelected
-        {
-            get { return Color.FromHex(_settingsService.CurrentCitizenSettingDTO.WeekDayColors[0].HexColor); }
-        }
-
-        string _tuesdaySelectedColor = "Lilla";
-        public string TuesdaySelectedColor
-        {
-            get { return _weekdayColorsDict.First(color => color.Value == Color.FromHex(_settingsService.CurrentCitizenSettingDTO.WeekDayColors[1].HexColor)).Key; }
-            set
-            {
-                RaisePropertyChanged(() => TuesdayColorSelected);
-                _tuesdaySelectedColor = value;
-                UpdateSettings("TuesdayColor", _weekdayColorsDict[value], 1);
-            }
-        }
-
-        public Color TuesdayColorSelected
-        {
-            get { return Color.FromHex(_settingsService.CurrentCitizenSettingDTO.WeekDayColors[1].HexColor); }
-        }
-
-        string _wednesdaySelectedColor = "Orange";
-
-        public string WednesdaySelectedColor
-        {
-            get { return _weekdayColorsDict.First(color => color.Value == Color.FromHex(_settingsService.CurrentCitizenSettingDTO.WeekDayColors[2].HexColor)).Key; }
-            set
-            {
-                RaisePropertyChanged(() => WednesdayColorSelected);
-
-                _wednesdaySelectedColor = value;
-                UpdateSettings("WednesdayColor", _weekdayColorsDict[value], 2);
-            }
-        }
-
-        public Color WednesdayColorSelected
-        {
-            get { return Color.FromHex(_settingsService.CurrentCitizenSettingDTO.WeekDayColors[2].HexColor); }
-        }
-
-        string _thursdaySelectedColor = "Blå";
-
-        public string ThursdaySelectedColor
-        {
-            get { return _weekdayColorsDict.First(color => color.Value == Color.FromHex(_settingsService.CurrentCitizenSettingDTO.WeekDayColors[3].HexColor)).Key; }
-            set
-            {
-                RaisePropertyChanged(() => ThursdayColorSelected);
-                _thursdaySelectedColor = value;
-                UpdateSettings("ThursdayColor", _weekdayColorsDict[value], 3);
-            }
-        }
-
-        public Color ThursdayColorSelected
-        {
-            get { return Color.FromHex(_settingsService.CurrentCitizenSettingDTO.WeekDayColors[3].HexColor); }
-        }
-
-        string _fridaySelectedColor = "Gul";
-
-        public string FridaySelectedColor
-        {
-            get { return _weekdayColorsDict.First(color => color.Value == Color.FromHex(_settingsService.CurrentCitizenSettingDTO.WeekDayColors[4].HexColor)).Key; }
-            set
-            {
-                RaisePropertyChanged(() => FridayColorSelected);
-                _fridaySelectedColor = value;
-                UpdateSettings("FridayColor", _weekdayColorsDict[value], 4);
-            }
-        }
-
-        public Color FridayColorSelected
-        {
-            get { return Color.FromHex(_settingsService.CurrentCitizenSettingDTO.WeekDayColors[4].HexColor); }
-        }
-
-        string _saturdaySelectedColor = "Rød";
-
-        public string SaturdaySelectedColor
-        {
-            get { return _weekdayColorsDict.First(color => color.Value == Color.FromHex(_settingsService.CurrentCitizenSettingDTO.WeekDayColors[5].HexColor)).Key; }
-            set
-            {
-                RaisePropertyChanged(() => SaturdayColorSelected);
-                _saturdaySelectedColor = value;
-                UpdateSettings("SaturdayColor", _weekdayColorsDict[value], 5);
-            }
-        }
-
-        public Color SaturdayColorSelected
-        {
-            get { return Color.FromHex(_settingsService.CurrentCitizenSettingDTO.WeekDayColors[5].HexColor); }
-        }
-
-        string _sundaySelectedColor = "Hvid";
-
-        public string SundaySelectedColor
-        {
-            get { return _weekdayColorsDict.First(color => color.Value == Color.FromHex(_settingsService.CurrentCitizenSettingDTO.WeekDayColors[6].HexColor)).Key; }
-            set
-            {
-                RaisePropertyChanged(() => SundayColorSelected);
-                _sundaySelectedColor = value;
-                UpdateSettings("SundayColor", _weekdayColorsDict[value], 6);
-            }
-        }
-
-        public Color SundayColorSelected
-        {
-            get { return Color.FromHex(_settingsService.CurrentCitizenSettingDTO.WeekDayColors[6].HexColor); }
-        }
-
-        #endregion
 
         public ICommand HandleSwitchChangedCommand => new Command(() =>
         {
@@ -301,12 +116,10 @@ namespace WeekPlanner.ViewModels
         });
 
 
-        private async void UpdateSettingsAsync()
+        private async Task UpdateSettingsAsync()
         {
             _settingsService.UseTokenFor(UserType.Citizen);
-            await _requestService.SendRequestAndThenAsync(
-                requestAsync: () => _userApi.V1UserSettingsPatchAsync(Settings),
-                onSuccess: dto => { });
+            await _requestService.SendRequest(_userApi.V1UserSettingsPatchAsync(Settings));
         }
 
 
@@ -330,6 +143,10 @@ namespace WeekPlanner.ViewModels
             _requestService = requestService;
             _userApi = userApi;
             _dialogService = dialogService;
+
+            WeekdayColors = new WeekdayColors(_settingsService.CurrentCitizenSettingDTO);
+            // Update settings regardless of which property calls 'RaisePropertyChanged'
+            WeekdayColors.PropertyChanged += (sender, e) => UpdateSettingsAsync();
         }
 
         public override async Task InitializeAsync(object navigationData)
@@ -346,9 +163,7 @@ namespace WeekPlanner.ViewModels
                 onSuccess: result =>
                 {
                     GirafCitizen = result.Data;
-                },
-                onExceptionAsync: async () => await NavigationService.PopAsync(),
-                onRequestFailedAsync: async () => await NavigationService.PopAsync());
+                });
         }
 
     }
