49d874b6a6b6f4b936966dd885866635ea56ae44
Kasper Dissing Bargsteen <kbargs15@student.aau.dk>
2018-05-07 14:03:30 +0200

    Fix settings multi click and multiple settings views

--- WeekPlanner/ViewModels/MasterViewModel.cs
+++ WeekPlanner/ViewModels/MasterViewModel.cs
@@ -1,59 +1,40 @@
-﻿using System.Windows.Input;
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
 using WeekPlanner.Services.Navigation;
 using Xamarin.Forms;
 using WeekPlanner.ViewModels.Base;
-using System;
-using IO.Swagger.Model;
-using System.Threading.Tasks;
 using WeekPlanner.Services.Settings;
-using WeekPlanner.Services.Login;
 
 namespace WeekPlanner.ViewModels
 {
-    class MasterViewModel : ViewModelBase
+    public class MasterViewModel : ViewModelBase
     {
 		public ISettingsService SettingsService { get; }
-		private readonly ILoginService _loginService;
 
-	    private UserNameDTO _girafCitizen;
-		public UserNameDTO GirafCitizen
-		{
-			get => _girafCitizen;
-			set
-			{
-				_girafCitizen = value;
-				RaisePropertyChanged(() => GirafCitizen);
-			}
-		}
-
-		public MasterViewModel(ISettingsService settingsService, INavigationService navigationService, ILoginService loginService) : base(navigationService)
+		public MasterViewModel(ISettingsService settingsService, INavigationService navigationService) : base(navigationService)
 		{
 			SettingsService = settingsService;
-			_loginService = loginService;
 		}
 
-		public override async Task InitializeAsync(object navigationData)
-		{
-			if (navigationData is UserNameDTO userNameDTO)
-			{
-				await _loginService.LoginAndThenAsync(UserType.Citizen,
-					userNameDTO.UserName, "", () => InitializeCitizen(userNameDTO));
-			}
-			else
-			{
-				throw new ArgumentException("Must be of type userNameDTO", nameof(navigationData));
-			}
-		}
+		public ICommand NavigateToSettingsCommand => new Command(NavigateToSettingsAndHideMaster);
 
-		private async Task InitializeCitizen(UserNameDTO userNameDTO)
-		{
-			GirafCitizen = userNameDTO;
-		}
+	    private async void NavigateToSettingsAndHideMaster()
+	    {
+		    if (IsBusy) return;
 
-		public ICommand NavigateToSettingsCommand =>
-        new Command(async () => {
-            MessagingCenter.Send(this, MessageKeys.HideMasterPage);
-            await NavigationService.NavigateToAsync<SettingsViewModel>(GirafCitizen);
-        });
+		    IsBusy = true;
+		    
+		    MessagingCenter.Send(this, MessageKeys.HideMasterPage);
+		    
+		    if (NavigationService.CurrentPageViewModel is SettingsViewModel)
+		    {
+			    IsBusy = false;
+			    return;
+		    }
+		    
+		    await NavigationService.NavigateToAsync<SettingsViewModel>();
+		    
+		    IsBusy = false;
+	    }
     }
 }
