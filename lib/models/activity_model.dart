518471243702c9d584650b68cb2e6c2c04129f58
Bogi Napoleon Wennerstrøm <bogi.wennerstrom@gmail.com>
2019-03-09 18:23:31 +0100

    added tests and updated model

--- lib/models/activity_model.dart
+++ lib/models/activity_model.dart
@@ -1,40 +1,50 @@
 import 'package:meta/meta.dart';
 import 'package:weekplanner/models/model.dart';
 import 'package:weekplanner/models/pictogram_model.dart';
-import 'activityState_enum.dart';  
+import 'activity_state_enum.dart';
 
-class ActivityModel implements Model{
-	int id;
-	PictogramModel pictogram;
-	int order;
-	ActivityState state;
+class ActivityModel implements Model {
+  int id;
 
+  PictogramModel pictogram;
 
-	ActivityModel({ 
-    @required int id,
-    @required PictogramModel pictogram,
-    @required int order,
-    @required ActivityState state,
-    });
+  /// The order that the activity will appear on in a weekschedule. If two has same order it is a choice
+  int order;
 
-  ActivityModel.fromJson(Map<String, dynamic> json) {    
-        if (json == null) {
+  /// The current ActivityState
+  ActivityState state;
+
+  /// This is used in the WeekPlanner app by the frontend groups and should never be set from our side
+  bool isChoiceBoard;
+
+  ActivityModel({
+    @required this.id,
+    @required this.pictogram,
+    @required this.order,
+    @required this.state,
+    @required this.isChoiceBoard,
+  });
+
+  ActivityModel.fromJson(Map<String, dynamic> json) {
+    if (json == null) {
       throw FormatException("[ActivityModel]: Cannot initialize from null");
     }
 
-      id  = json['id'];
-      pictogram = json['pictogram'].map((value) => PictogramModel.fromJson(value));
-      order = json['order'];
-      state = json['state'].map((value) => ActivityModel.fromJson(value));   
+    id = json['id'];
+    pictogram = PictogramModel.fromJson(json['pictogram']);
+    order = json['order'];
+    state = ActivityState.values[(json['state']) - 1];
+    isChoiceBoard = json['isChoiceBoard'] as bool;
   }
 
   @override
   Map<String, dynamic> toJson() {
     return {
       'id': id,
-      'pictogram':pictogram.toJson() ,
+      'pictogram': pictogram.toJson(),
       'order': order,
-      'state': state,
+      'state': state.index + 1,
+      'isChoiceBoard': isChoiceBoard,
     };
   }
 }
