31dd09f75773aa9b42c34cc91f79102ec38712d6
Bogi Napoleon Wennerstrøm <bogi.wennerstrom@gmail.com>
2019-03-10 23:22:39 +0100

    added docs to week_api

--- lib/providers/api/week_api.dart
+++ lib/providers/api/week_api.dart
@@ -8,6 +8,9 @@ class WeekApi {
 
   WeekApi(this._http);
 
+  /// Get week names from the user with the given ID
+  ///
+  /// [id] User ID
   Observable<List<WeekNameModel>> getNames(String id) {
     return _http.get("/$id/week").map((Response res) {
       return (res.json["data"] as List)
@@ -16,12 +19,24 @@ class WeekApi {
     });
   }
 
+  /// Gets the Week with the specified week number and year for the user with
+  /// the given id.
+  ///
+  /// [id] User ID
+  /// [year] Year the week is in
+  /// [weekNumber] The week-number of the week
   Observable<WeekModel> get(String id, int year, int weekNumber) {
     return _http.get("/$id/week/$year/$weekNumber").map((Response res) {
       return WeekModel.fromJson(res.json["data"]);
     });
   }
 
+  /// Updates the entire information of the week with the given year and week
+  /// number.
+  ///
+  /// [id] User ID
+  /// [year] Year the week is in
+  /// [weekNumber] The week-number of the week
   Observable<WeekModel> update(
       String id, int year, int weekNumber, WeekModel week) {
     return _http
@@ -31,6 +46,12 @@ class WeekApi {
     });
   }
 
+  /// Deletes all information for the entire week with the given year and week
+  /// number.
+  ///
+  /// [id] User ID
+  /// [year] Year the week is in
+  /// [weekNumber] The week-number of the week
   Observable<bool> delete(String id, int year, int weekNumber) {
     return _http.delete("/$id/week/$year/$weekNumber").map((Response res) {
       return res.json["success"];
