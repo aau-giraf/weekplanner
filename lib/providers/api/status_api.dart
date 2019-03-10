ec349c3d0015c637c191a3f839002f6f94612b38
TobiasPalludan <tpallu16@student.aau.dk>
2019-03-10 20:21:12 +0100

    Added api for status

---
+++ lib/providers/api/status_api.dart
@@ -0,0 +1,30 @@
+import 'package:meta/meta.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:weekplanner/models/giraf_user_model.dart';
+import 'package:weekplanner/models/response_model.dart';
+import 'package:weekplanner/models/role_enum.dart';
+import 'package:weekplanner/providers/http/http.dart';
+import 'package:weekplanner/providers/peristence/persistence.dart';
+
+class StatusApi {
+  final Http _http;
+
+  /// End-point for checking if the API is running.
+  Observable<String> Status() {
+    return _http.get("/Status")
+  }
+
+  /// End-point for checking connection to the database.
+  Observable<String> databaseStatus() {
+    return _http.get("/Status/database").map((Response res) {
+      return res.json['success'];
+    });
+  }
+
+  /// End-point for getting git version info, i.e. branch and commit hash
+  Observable<String> versionInfo() {
+    return _http
+        .get("/Status/database")
+        .map((Response res) => res.json["data"] as String);
+  }
+}
