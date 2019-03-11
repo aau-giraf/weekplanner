1db4761e02bab6eac9c9b720d799e072acd0b2cb
Bogi Napoleon Wennerstrøm <bogi.wennerstrom@gmail.com>
2019-03-11 15:39:35 +0100

    fixed status tests

--- test/providers/api/status_api_test.dart
+++ test/providers/api/status_api_test.dart
@@ -1,17 +1,14 @@
 import 'package:test_api/test_api.dart';
 import 'package:weekplanner/providers/api/status_api.dart';
 import 'package:weekplanner/providers/http/http_mock.dart';
-import 'package:weekplanner/providers/peristence/persistence_mock.dart';
 
 void main() {
   StatusApi statusApi;
   HttpMock httpMock;
-  PersistenceMock persistenceMock;
 
   setUp(() {
     httpMock = HttpMock();
-    persistenceMock = PersistenceMock();
-    statusApi = StatusApi(httpMock, persistenceMock);
+    statusApi = StatusApi(httpMock);
   });
 
   test("Should call status endpoint", () {
