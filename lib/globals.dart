47dcd9d6aacec946ebdbf42824875e034af8dcf2
Thue Iversen <Thue-V-Iversen@hotmail.com>
2019-03-18 18:47:12 +0100

    Lavet Loading Screen, med callback og timeout

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,11 +1,52 @@
+import 'dart:async';
+
 import 'package:weekplanner/blocs/application_bloc.dart';
 import 'package:weekplanner/blocs/auth_bloc.dart';
 import 'package:weekplanner/blocs/settings_bloc.dart';
 import 'package:weekplanner/providers/api/api.dart';
+import 'package:flutter/material.dart';
 
-class Globals{
+import 'dart:io';
+
+class Globals {
   static final api = new Api("http://web.giraf.cs.aau.dk:5000");
   static final ApplicationBloc appBloc = ApplicationBloc(authBloc);
   static final AuthBloc authBloc = AuthBloc(api);
   static final SettingsBloc settingsBloc = SettingsBloc();
+
+  static Future<bool> hasInternet() async {
+    try {
+      final result = await InternetAddress.lookup('web.giraf.cs.aau.dk');
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        print('connected');
+        return true;
+      }
+    } on SocketException catch (_) {
+      print('not connected');
+      return false;
+    }
+  }
+
+  static void showLoadingScreen(BuildContext context, bool Dismissible,
+      [void callback(), int timeoutMS]) {
+    if (callback != null) {
+      if (timeoutMS == null) {
+        timeoutMS = 2000;
+      }
+      Timer(Duration(milliseconds: timeoutMS), callback);
+    }
+    showDialog(
+        barrierDismissible: Dismissible,
+        context: context,
+        builder: (BuildContext context) {
+          return Center(
+            child: Transform.scale(
+                scale: 2,
+                child: CircularProgressIndicator(
+                  valueColor:
+                      AlwaysStoppedAnimation(Color.fromRGBO(255, 157, 0, 0.8)),
+                )),
+          );
+        });
+  }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:weekplanner/blocs/auth_bloc.dart';
+import 'package:weekplanner/globals.dart';
 import 'package:weekplanner/widgets/bloc_provider_tree_widget.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -8,100 +11,124 @@ class LoginScreen extends StatelessWidget {
   final TextEditingController usernameCtrl = TextEditingController();
   final TextEditingController passwordCtrl = TextEditingController();
 
+  final timeout = const Duration(seconds: 2);
+  final ms = const Duration(milliseconds: 1);
+
+  BuildContext loginContext = null;
+  bool LoggedInSuccessfull = false;
+
+  void handleTimeout() {
+    if (!LoggedInSuccessfull) {
+      Navigator.pop(loginContext);
+      wrongUsernameOrPassword(loginContext);
+    }
+  }
+
+  Future<bool> loginAction(BuildContext context, String nextScreen) async {
+    loginContext = context;
+    Globals.showLoadingScreen(context, true, handleTimeout, 1500);
+
+    authBloc.loggedIn.listen((status) {
+      if (status) {
+        LoggedInSuccessfull = true;
+        Navigator.pop(context);
+        Navigator.pushNamed(context, "/weekplan");
+      }
+    });
+    authBloc.authenticate(usernameCtrl.value.text, passwordCtrl.value.text);
+    return true;
+  }
+
+  void wrongUsernameOrPassword(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          //TODO: Lav en pæn dialog
+          return Center(
+            child: Text("Forkert brugernavn eller adgangskode"),
+          );
+        });
+  }
+
   AuthBloc authBloc;
 
   @override
   Widget build(BuildContext context) {
     authBloc = BlocProviderTree.of<AuthBloc>(context);
-
+    Key ScaffoldKey = Key("scaffoldKe");
     final Size screenSize = MediaQuery.of(context).size;
 
     return Scaffold(
+        key: ScaffoldKey,
         body: Container(
-      width: screenSize.width,
-      height: screenSize.height,
-      padding: new EdgeInsets.all(20.0),
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/login_screen_background_image.png"),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Container(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-            Image(image: AssetImage("assets/giraf_splash_logo.png")),
-            Expanded(
-                child: Form(
-              key: this._formKey,
-              child: ListView(
-                children: <Widget>[
-                  TextField(
-                    controller: usernameCtrl,
-                    keyboardType: TextInputType.text,
-                    // Use email input type for emails.
-                    decoration: InputDecoration(
-                      hintText: '',
-                      labelText: 'Brugernavn',
-                      fillColor: Colors.white,
-                    ),
-                  ),
-                  TextField(
-                    controller: passwordCtrl,
-                    obscureText: true,
-                    // Use email input type for emails.
-                    decoration: InputDecoration(
-                      hintText: '',
-                      labelText: 'Adgangskode',
-                      fillColor: Colors.white,
-                    ),
-                  ),
-                  Container(
-                    child: new RaisedButton(
-                      child: new Text(
-                        'Login',
-                        style: new TextStyle(color: Colors.white),
+          width: screenSize.width,
+          height: screenSize.height,
+          padding: new EdgeInsets.all(20.0),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/login_screen_background_image.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Container(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                Image(image: AssetImage("assets/giraf_splash_logo.png")),
+                Expanded(
+                    child: Form(
+                  key: this._formKey,
+                  child: ListView(
+                    children: <Widget>[
+                      TextField(
+                        controller: usernameCtrl,
+                        keyboardType: TextInputType.text,
+                        // Use email input type for emails.
+                        decoration: InputDecoration(
+                          hintText: '',
+                          labelText: 'Brugernavn',
+                          fillColor: Colors.white,
+                        ),
                       ),
-                      onPressed: () {
-                        authBloc.loggedIn.take(1).listen((status){
-                          if(status){
-                            Navigator.pushNamed(context, "/weekplan");
-                          }
-                        });
-                        authBloc.authenticate(
-                            usernameCtrl.value.text,
-                            passwordCtrl.value.text
-                        );
-                      },
-                      color: Colors.blue,
-                    ),
-                  ),
-                  Container(
-                    child: new RaisedButton(
-                      child: new Text(
-                        'Auto-Login',
-                        style: new TextStyle(color: Colors.white),
+                      TextField(
+                        controller: passwordCtrl,
+                        obscureText: true,
+                        // Use email input type for emails.
+                        decoration: InputDecoration(
+                          hintText: '',
+                          labelText: 'Adgangskode',
+                          fillColor: Colors.white,
+                        ),
                       ),
-                      onPressed: () {
-                        authBloc.loggedIn.take(1).listen((status){
-                          if(status){
-                            Navigator.pushNamed(context, "/weekplan");
-                          }
-                        });
-                        authBloc.authenticate(
-                            "graatand",
-                            "password"
-                        );
-                      },
-                      color: Colors.blue,
-                    ),
+                      Container(
+                        child: new RaisedButton(
+                          child: new Text(
+                            'Login',
+                            style: new TextStyle(color: Colors.white),
+                          ),
+                          onPressed: () async {
+                            await loginAction(context, "/weekplan");
+                          },
+                          color: Colors.blue,
+                        ),
+                      ),
+                      Container(
+                        child: new RaisedButton(
+                          child: new Text(
+                            'Auto-Login',
+                            style: new TextStyle(color: Colors.white),
+                          ),
+                          onPressed: () async {
+                            await loginAction(context, "/weekplan");
+                          },
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ))
-          ])),
-    ));
+                ))
+              ])),
+        ));
   }
 }
