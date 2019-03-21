e9e0dbd3c4993a82aec2b794996a7668532bc067
Andreas Stenshøj <and8700and@gmail.com>
2019-03-21 17:14:20 +0100

    able to show activities for different days

--- lib/screens/weekplan_screen.dart
+++ lib/screens/weekplan_screen.dart
@@ -4,7 +4,10 @@ import 'package:weekplanner/blocs/settings_bloc.dart';
 import 'package:weekplanner/blocs/weekplan_bloc.dart';
 import 'package:weekplanner/bootstrap.dart';
 import 'package:weekplanner/di.dart';
+import 'package:weekplanner/models/activity_model.dart';
 import 'package:weekplanner/models/enums/giraf_theme_enum.dart';
+import 'package:weekplanner/models/enums/weekday_enum.dart';
+import 'package:weekplanner/models/week_model.dart';
 import 'package:weekplanner/widgets/giraf_app_bar_widget.dart';
 
 class WeekplanScreen extends StatelessWidget {
@@ -29,54 +32,50 @@ class WeekplanScreen extends StatelessWidget {
         appBar: GirafAppBar(
           title: 'Ugeplan',
         ),
-        body: new Row(
-          children: <Widget>[
-            StreamBuilder<GirafTheme>(
-              stream: this.settingsBloc.theme,
-              initialData: GirafTheme.AndroidBlue,
+        body:
+            StreamBuilder<WeekModel>(
+              stream: this.weekplanBloc.week,
+              initialData: null,
               builder:
-                  (BuildContext context, AsyncSnapshot<GirafTheme> snapshot) {
-                return Text(snapshot.data.toString());
+                  (BuildContext context, AsyncSnapshot<WeekModel> snapshot) {
+                    if(snapshot.hasData) {
+                      return buildWeeks(snapshot.data);
+                    } else {
+                      return Text("Data not ready");
+                    }
               },
             ),
-            Expanded(
-                child: Card(
-                    color: Color(0xFF007700), child: Day('Mandag', myList))),
-            Expanded(
-                child: Card(
-                    color: Color(0xFF800080), child: Day('Tirsdag', myList))),
-            Expanded(
-                child: Card(
-                    color: Color(0xFFFF8500), child: Day('Onsdag', myList))),
-            Expanded(
-                child: Card(
-                    color: Color(0xFF0000FF), child: Day('Torsdag', myList))),
-            Expanded(
-                child: Card(
-                    color: Color(0xFFFFDD00), child: Day('Fredag', myList))),
-            Expanded(
-                child: Card(
-                    color: Color(0xFFFF0000), child: Day('Lørdag', myList))),
-            Expanded(
-                child: Card(
-                    color: Color(0xFFFFFFFF), child: Day('Søndag', myList))),
-          ],
-        ));
+    );
   }
 }
 
-Column Day(String day, List<Widget> myList) {
+
+Row buildWeeks(WeekModel weekModel) {
+  List<List<int>> weekColors = [[8,160,69],[84,13,110], [247,127,0], [0,71,119], [249,200,14], [219,43,57], [255,255,255]];
+  List<Widget> weekDays = List<Widget>();
+  for (var i = 0; i < weekModel.days.length; i++) {
+    weekDays.add(new Expanded(
+                child: Card(
+                    color: Color.fromARGB(255, weekColors[i][0],weekColors[i][1],weekColors[i][2]), child: Day(weekModel.days[i].day.toString(), weekModel.days[i].activities))));
+  }
+  return new Row(
+    children: weekDays);
+}
+
+Column Day(String day, List<ActivityModel> myList) {
   return Column(
     children: <Widget>[
       Text(day, style: TextStyle(fontWeight: FontWeight.bold)),
       Expanded(
-        child: ListView.builder(
+        child: 
+        ListView.builder(
           itemBuilder: (BuildContext context, int index) {
-            return myList[index];
-            return Card(
-              color: Colors.white,
-              child: IconButton(icon: Image.asset('assets/read.jpg')),
-            );
+            if(myList.isEmpty || myList == null) {
+              return Text("No Pictograms");
+            }
+            else
+            return Text(myList[index].pictogram.imageUrl);
+            
           },
           itemCount: myList.length,
         ),
