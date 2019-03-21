30b5ce8b048e9b1ad33c4195f3a3e4f525e61af1
swuf <aagers13@student.aau.dk>
2019-03-21 13:11:42 +0100

    Implement disabled button.

--- lib/widgets/giraf_button_widget.dart
+++ lib/widgets/giraf_button_widget.dart
@@ -34,14 +34,14 @@ class _GirafDefaultButtonState extends State<GirafDefaultButton> {
   ], begin: Alignment(0.5, -1.0), end: Alignment(0.5, 1.0));
 
   final Gradient gradientDisabled = const LinearGradient(colors: <Color>[
-    Color.fromARGB(0xa6, 0xff, 0xcd, 0x59),
-    Color.fromARGB(0xa6, 0xff, 0x9d, 0x00)
+    Color.fromARGB(0x46, 0xff, 0xcd, 0x59),
+    Color.fromARGB(0x46, 0xff, 0x9d, 0x00)
   ], begin: Alignment(0.5, -1.0), end: Alignment(0.5, 1.0));
 
   final Color borderDefault = const Color.fromARGB(0xff, 0x8a, 0x6e, 0x00);
   final Color borderPressed = const Color.fromARGB(0xff, 0x49, 0x37, 0x00);
   final Color borderDisabled = const Color.fromARGB(0xa6, 0x8a, 0x6e, 0x00);
-  
+
   bool isPressed = false;
   bool isEnabled = true;
 
@@ -51,51 +51,62 @@ class _GirafDefaultButtonState extends State<GirafDefaultButton> {
       width: widget.width,
       height: widget.height,
       decoration: BoxDecoration(
-          gradient: isEnabled ? (isPressed ? gradientPressed : gradientDefault) : gradientDisabled,
-          borderRadius: BorderRadius.circular(15),
-          border: Border.all(color: isEnabled ? (isPressed ? borderPressed : borderDefault) : borderDisabled),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey[500],
-              offset: Offset(0.0, 1.5),
-              blurRadius: 1.5,
-            ),
-          ]),
+        gradient: isEnabled
+            ? (isPressed ? gradientPressed : gradientDefault)
+            : gradientDisabled,
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(
+            color: isEnabled
+                ? (isPressed ? borderPressed : borderDefault)
+                : borderDisabled,
+            width: 2),
+      ),
       child: Material(
         color: Colors.transparent,
         child: InkWell(
-            onTap: () => _onTapped(widget.onPressed),
-            child: widgetsOnButton()),
+            onTap: () => _onTapped(widget.onPressed), child: widgetsOnButton()),
       ),
     );
     isPressed = false;
     return result;
   }
 
-  void _onTapped(Function widgetOnPressed){
-    setState(() => {isPressed = true});
-    widgetOnPressed();
+  void _onTapped(Function widgetOnPressed) {
+    if (isEnabled) {
+      setState(() => {isPressed = true});
+      widgetOnPressed();
+    }
   }
 
-  Widget widgetsOnButton(){
-    TextStyle textStyle = TextStyle(color: Colors.black,);
+  Widget widgetsOnButton() {
+    TextStyle textStyle = TextStyle(
+      color: Colors.black,
+    );
     if (widget.text != null && widget.icon != null)
       return Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Icon(widget.icon),
-                SizedBox(width: 10,),
-                Text(
-                  widget.text,
-                  style: textStyle,
-                ),
-              ],
-            );
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(widget.icon),
+          SizedBox(
+            width: 10,
+          ),
+          Text(
+            widget.text,
+            style: textStyle,
+          ),
+        ],
+      );
     else if (widget.text != null)
-      return Center(child: Text(widget.text, style: textStyle,));
+      return Center(
+          child: Text(
+        widget.text,
+        style: textStyle,
+      ));
     else if (widget.icon != null)
-      return Center(child: Icon(widget.icon),);
-    
+      return Center(
+        child: Icon(widget.icon),
+      );
+
     return null;
   }
 }
