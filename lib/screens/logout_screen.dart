05d2038b06febcdf30a0b1a83363fd8b85bee38a
Daniel Moesgaard Andersen <moesgaarda@gmail.com>
2019-03-26 12:05:37 +0100

    Fixed indention

--- lib/screens/logout_screen.dart
+++ lib/screens/logout_screen.dart
@@ -5,7 +5,6 @@ import 'package:weekplanner/di.dart';
 
 // Screen used to confirm the users logout.
 class LogoutScreen extends StatelessWidget {
-
   final AuthBloc _authBloc;
 
   // The logout screen uses the AuthBloc to determine if the user has logged in or not and will use its function to logout.
@@ -18,44 +17,47 @@ class LogoutScreen extends StatelessWidget {
       appBar: GirafAppBar(
         title: 'Ugeplan',
       ),
-        body: Container(
-          width: screenSize.width,
-          height: screenSize.height,
-          padding: new EdgeInsets.all(20.0),
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/login_screen_background_image.png"),
-              fit: BoxFit.cover,
-            ),
+      body: Container(
+        width: screenSize.width,
+        height: screenSize.height,
+        padding: new EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/login_screen_background_image.png"),
+            fit: BoxFit.cover,
           ),
-      child: Container(
+        ),
+        child: Container(
           child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Image(image: AssetImage("assets/giraf_splash_logo.png")),
-                Expanded(
-                    child: Form(
-                      child: ListView(
-                        children: <Widget>[
-                          Container(
-                            child: new RaisedButton(
-                              child: new Text(
-                                'Log ud',
-                                style: new TextStyle(color: Colors.white),
-                              ),
-                              onPressed: () {
-                                // Once the logout function has been called the user will be redirected to the login screen.
-                                this._authBloc.logout();
-                              },
-                              color: Colors.blue,
-                            ),
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image(image: AssetImage("assets/giraf_splash_logo.png")),
+              Expanded(
+                child: Form(
+                  child: ListView(
+                    children: <Widget>[
+                      Container(
+                        child: new RaisedButton(
+                          child: new Text(
+                            'Log ud',
+                            style: new TextStyle(color: Colors.white),
                           ),
-                        ],
-                      ),
-                    ))
-              ])),
-    ));
-
+                          onPressed: () {
+                            // Once the logout function has been called the user will be redirected to the login screen.
+                            this._authBloc.logout();
+                          },
+                          color: Colors.blue,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
