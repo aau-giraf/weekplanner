2065058767634668f6e21029f7dd2ac7457a748c
HaZz <magnustboisen@gmail.com>
2019-04-01 10:34:22 +0200

    Removed giraf usermodel & weekplan selector screen
    Were previously used for testing...

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:weekplanner/blocs/auth_bloc.dart';
 import 'package:weekplanner/bootstrap.dart';
 import 'package:weekplanner/di.dart';
-import 'package:weekplanner/models/giraf_user_model.dart';
 import 'package:weekplanner/screens/login_screen.dart';
 import 'package:weekplanner/screens/weekplan_screen.dart';
-import 'package:weekplanner/screens/weekplan_selector_screen.dart';
 
 void main() {
   // Register all dependencies for injector
