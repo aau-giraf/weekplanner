3182b3ca3bc42c8b853d9fd58c1625c268ea3fcf
Jonas Alsen <grann.alsen@gmail.com>
2019-04-02 10:48:55 +0200

    Update activity_bloc.dart

--- lib/blocs/activity_bloc.dart
+++ lib/blocs/activity_bloc.dart
@@ -15,6 +15,7 @@ class ActivityBloc extends BlocBase {
   WeekModel _weekModel;
   GirafUserModel _user;
 
+  /// Loads the WeekModel, ActivityModel and the GirafUser.
   void load(WeekModel weekModel, ActivityModel activityModel, GirafUserModel user) {
     _activityModel = activityModel;
     _weekModel = weekModel;
@@ -23,21 +24,35 @@ class ActivityBloc extends BlocBase {
 
   /// Mark the selected activity as complete.
   void completeActivity() {
+    final ActivityState tempState = _activityModel.state;
     _activityModel.state = ActivityState.Completed;
+    if (!update()) {
+      _activityModel.state = tempState;
+    }
   }
 
   /// Mark the selected activity as cancelled.
   void cancelActivity() {
+    final ActivityState tempState = _activityModel.state;
     _activityModel.state = ActivityState.Canceled;
+    if (!update()) {
+      _activityModel.state = tempState;
+    }
   }
 
   /// Update the weekmodel with the new state.
-  void update() {
-    _api.week.update(_user.id, _weekModel.weekYear, _weekModel.weekNumber, _weekModel);
+  bool update() {
+    var result = _api.week.update(_user.id, _weekModel.weekYear, _weekModel.weekNumber, _weekModel);
+    if (result is WeekModel) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
   @override
   void dispose() {
-    update();
+    
   }
 }
