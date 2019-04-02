f2e56c11d29669c5e2b6f0e0f1648b9ff5688c13
Luke <Lukasronsholt@gmail.com>
2019-04-02 13:18:19 +0200

    Fixed linter error
    Unused import

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,7 +1,6 @@
 import 'package:injector/injector.dart';
 import 'package:weekplanner/blocs/auth_bloc.dart';
 import 'package:weekplanner/blocs/pictogram_image_bloc.dart';
-import 'package:weekplanner/blocs/settings_bloc.dart';
 import 'package:weekplanner/blocs/weekplans_bloc.dart';
 import 'package:weekplanner/blocs/pictogram_bloc.dart';
 import 'package:weekplanner/blocs/toolbar_bloc.dart';
