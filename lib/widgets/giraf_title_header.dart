f4717bb9c78b146c3ec7c9468931e6aef2e48777
Martin Simonsen <msimon16@student.aau.dk>
2019-04-03 13:02:00 +0200

    copy fra feature_088

---
+++ lib/widgets/giraf_title_header.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+/// The GirafDialogHeader is to be used at the title location of widgets
+class GirafTitleHeader extends StatelessWidget implements PreferredSizeWidget {
+  ///The header takes the title as input, so it is similar for all titles.
+  const GirafTitleHeader({Key key, this.title}) : super(key: key);
+
+  @override
+  Size get preferredSize => const Size.fromHeight(56.0);
+
+  ///title of the header
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+              child: Center(child: Text(title)),
+            ),
+            decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: <double>[
+                      0.33,
+                      0.66
+                    ],
+                    colors: <Color>[
+                      Color.fromRGBO(254, 215, 108, 1),
+                      Color.fromRGBO(253, 187, 85, 1),
+                    ])),
+          ),
+        ),
+      ],
+    );
+  }
+}
