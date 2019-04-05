be15e0b612baeb477570e46e97946f708c9377b6
Thomas Lundsgaard Hansen <thomaslhansen@live.dk>
2019-04-05 12:40:35 +0200

    Code clean up and added functionality to pop context.

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,21 @@ import 'package:weekplanner/blocs/auth_bloc.dart';
 import 'package:weekplanner/bootstrap.dart';
 import 'package:weekplanner/di.dart';
 import 'package:weekplanner/providers/environment_provider.dart' as environment;
+import 'package:weekplanner/routes.dart';
 import 'package:weekplanner/screens/choose_citizen_screen.dart';
 import 'package:weekplanner/screens/login_screen.dart';
 
 void main() {
-  // Register all dependencies for injector
+  /// Register all dependencies for injector
   Bootstrap.register();
 
   if (_isInDebugMode) {
-    // If in DEBUG mode
+    /// If in DEBUG mode
     environment.setFile('assets/environments.json').whenComplete(() {
       _runApp();
     });
   } else {
-    // Else Production
+    /// Else Production
     environment.setFile('assets/environments.prod.json').whenComplete(() {
       _runApp();
     });
@@ -29,9 +30,16 @@ void _runApp() {
       home: StreamBuilder<bool>(
           initialData: false,
           stream: di.getDependency<AuthBloc>().loggedIn,
-          builder: (_, AsyncSnapshot<bool> snapshot) =>
-              // In case logged in show ChooseCitizenScreen, otherwise login
-              snapshot.data ? ChooseCitizenScreen() : LoginScreen())));
+          builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+            if (snapshot.data) {
+              /// In case logged in show ChooseCitizenScreen
+              return ChooseCitizenScreen();
+            } else {
+              /// Not loggedIn pop context to login screen.
+              Routes.goHome(context);
+              return LoginScreen();
+            }
+          })));
 }
 
 bool get _isInDebugMode {
