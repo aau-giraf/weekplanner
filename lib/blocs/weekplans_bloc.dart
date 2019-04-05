d50d3c72d1dba004a7f35fe863ee3bccf25d1e29
Unknown <jenspeturtrondarson@gmail.com>
2019-04-05 10:49:40 +0200

    Fixed bug

--- lib/blocs/weekplans_bloc.dart
+++ lib/blocs/weekplans_bloc.dart
@@ -5,7 +5,7 @@ import 'package:weekplanner/models/week_model.dart';
 import 'package:weekplanner/models/week_name_model.dart';
 import 'package:weekplanner/providers/api/api.dart';
 
-  /// WeekplansBloc to get weekplans for a user
+/// WeekplansBloc to get weekplans for a user
 class WeekplansBloc extends BlocBase {
   /// This bloc obtains a list of all [WeekModel]'s
   /// for a given [UsernameModel].
@@ -22,7 +22,7 @@ class WeekplansBloc extends BlocBase {
 
   final BehaviorSubject<List<WeekModel>> _weekModel =
       BehaviorSubject<List<WeekModel>>();
-      
+
   final BehaviorSubject<List<WeekNameModel>> _weekNameModelsList =
       BehaviorSubject<List<WeekNameModel>>();
 
@@ -50,15 +50,15 @@ class WeekplansBloc extends BlocBase {
   /// needed for getting all [WeekModel]'s.
   /// The result are published in [_weekModel].
   void getAllWeekInfo(List<WeekNameModel> weekNameModels) {
-    if (weekNameModels == null) {
-      print(weekNameModels);
-      return;
-    }
     final List<WeekModel> weekModels = <WeekModel>[];
-    
+
     /// This is used by weekplan_selector_screen for adding a new weekplan.
     if (_addWeekplan) {
-      weekModels.add(WeekModel(name: 'Tilføj Ugeplan'));
+      weekModels.add(WeekModel(name: 'Tilføj ugeplan'));
+      _weekModel.add(weekModels);
+    }
+    if (weekNameModels == null) {
+      return;
     }
 
     for (WeekNameModel weekNameModel in weekNameModels) {
