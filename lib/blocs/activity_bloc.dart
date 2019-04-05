f21618c659d7a9fa0d6e1f21434478db943d5fc7
Jonas Alsen <grann.alsen@gmail.com>
2019-04-05 12:29:35 +0200

    Update activity_bloc.dart

--- lib/blocs/activity_bloc.dart
+++ lib/blocs/activity_bloc.dart
@@ -16,53 +16,44 @@ class ActivityBloc extends BlocBase {
   WeekModel _weekModel;
   GirafUserModel _user;
 
-  WeekModel weekModelTEST;
-  ActivityModel accModelTEST;
-  String userIDTest = '379d057b-85b1-41b6-a1bd-6448c132745b';
-
-  Stream<WeekModel> get weekModelStream => _weekModelB.stream;
+  Stream<WeekModel> get weekModelStream => _weekModelStream.stream;
 
   // Start with providing false as the logged in status
-  final BehaviorSubject<WeekModel> _weekModelB = BehaviorSubject<WeekModel>();
+  final BehaviorSubject<WeekModel> _weekModelStream = BehaviorSubject<WeekModel>();
 
   /// Loads the WeekModel, ActivityModel and the GirafUser.
   void load(
       WeekModel weekModel, ActivityModel activityModel, GirafUserModel user) {
     _activityModel = activityModel;
     _weekModel = weekModel;
-    _user = user;
-
-    _api.week.get('379d057b-85b1-41b6-a1bd-6448c132745b', 2018, 21)
-    .listen((onData) {
-      weekModelTEST = onData;
-      accModelTEST = weekModelTEST.days[1].activities[0];
-    });
     
+    _user = user;
   }
 
   /// Mark the selected activity as complete.
   void completeActivity() {
-    final ActivityState tempState = accModelTEST.state;
-    accModelTEST.state = ActivityState.Completed;
+    final ActivityState tempState = _activityModel.state;
+    _activityModel.state = ActivityState.Completed;
     if (!update()) {
-      accModelTEST.state = tempState;
+      _activityModel.state = tempState;
     }
   }
 
   /// Mark the selected activity as cancelled.
   void cancelActivity() {
-    final ActivityState tempState = accModelTEST.state;
-    accModelTEST.state = ActivityState.Canceled;
+    final ActivityState tempState = _activityModel.state;
+    _activityModel.state = ActivityState.Canceled;
     if (!update()) {
-      accModelTEST.state = tempState;
+      _activityModel.state = tempState;
     }
   }
 
   /// Update the weekmodel with the new state.
   bool update() {
     _api.week.update(
-      userIDTest, weekModelTEST.weekYear, weekModelTEST.weekNumber, weekModelTEST)
-        .listen(_weekModelB.add);
+      _user.id, _weekModel.weekYear, _weekModel.weekNumber, _weekModel)
+        .listen(_weekModelStream.add);
+    // TODO: Do something with this.
     return true;
   }
 
