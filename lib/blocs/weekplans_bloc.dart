11f1229b2a9f706661513ddc261cc03cd046a381
Tobias Palludan <tobiaspall@yahoo.dk>
2019-04-09 13:33:08 +0200

    Back-merging release into develop (#136)
    Back-merge from release 2019S2R1

--- lib/blocs/weekplans_bloc.dart
+++ lib/blocs/weekplans_bloc.dart
@@ -1,14 +1,14 @@
 import 'package:rxdart/rxdart.dart';
 import 'package:weekplanner/blocs/bloc_base.dart';
-import 'package:weekplanner/models/giraf_user_model.dart';
+import 'package:weekplanner/models/username_model.dart';
 import 'package:weekplanner/models/week_model.dart';
 import 'package:weekplanner/models/week_name_model.dart';
 import 'package:weekplanner/providers/api/api.dart';
 
-  /// WeekplansBloc to get weekplans for a user
+/// WeekplansBloc to get weekplans for a user
 class WeekplansBloc extends BlocBase {
   /// This bloc obtains a list of all [WeekModel]'s
-  /// for a given [GirafUserModel].
+  /// for a given [UsernameModel].
   WeekplansBloc(this._api);
 
   /// This is a stream where all the [WeekNameModel] are put in,
@@ -22,12 +22,12 @@ class WeekplansBloc extends BlocBase {
 
   final BehaviorSubject<List<WeekModel>> _weekModel =
       BehaviorSubject<List<WeekModel>>();
-      
+
   final BehaviorSubject<List<WeekNameModel>> _weekNameModelsList =
       BehaviorSubject<List<WeekNameModel>>();
 
   final Api _api;
-  GirafUserModel _user;
+  UsernameModel _user;
 
   /// To control adding an extra result for creating a new [WeekModel]
   /// for the weekplan_selector_screen.
@@ -38,7 +38,7 @@ class WeekplansBloc extends BlocBase {
   ///  for adding a new [WeekModel].
 
   /// The result are published in [_weekNameModelsList].
-  void load(GirafUserModel user, [bool addWeekplan = false]) {
+  void load(UsernameModel user, [bool addWeekplan = false]) {
     _user = user;
     _addWeekplan = addWeekplan;
     weekNameModels.listen(getAllWeekInfo);
@@ -50,15 +50,15 @@ class WeekplansBloc extends BlocBase {
   /// needed for getting all [WeekModel]'s.
   /// The result are published in [_weekModel].
   void getAllWeekInfo(List<WeekNameModel> weekNameModels) {
-    if (weekNameModels == null) {
-      print(weekNameModels);
-      return;
-    }
     final List<WeekModel> weekModels = <WeekModel>[];
-    
-    /// This is used by weekplan_selector_screen for adding a new weekplan.
+
+    // This is used by weekplan_selector_screen for adding a new weekplan.
     if (_addWeekplan) {
-      weekModels.add(WeekModel(name: 'Tilføj Ugeplan'));
+      weekModels.add(WeekModel(name: 'Tilføj ugeplan'));
+      _weekModel.add(weekModels);
+    }
+    if (weekNameModels == null) {
+      return;
     }
 
     for (WeekNameModel weekNameModel in weekNameModels) {
