1c1dee062ef747cb782956414381254301730c89
Thue Iversen <Thue-V-Iversen@hotmail.com>
2019-04-09 14:37:00 +0200

    Added comments

--- lib/widgets/loading_spinner_widget.dart
+++ lib/widgets/loading_spinner_widget.dart
@@ -1,12 +1,20 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 
+/// Callback is optional and gives functionality that allows a
+/// method to be called after the timeout
+///
+/// Callback should be a void method, with no parameters
+///
+/// timeoutMS defaults to 2000 ms
 void showLoadingSpinner(BuildContext context, bool dismissible,
     [void callback(), int timeoutMS]) {
+  // If there is no callback method, no need for a timer
   if (callback != null) {
     timeoutMS ??= 2000;
     Timer(Duration(milliseconds: timeoutMS), callback);
   }
+
   showDialog<Center>(
       barrierDismissible: dismissible,
       context: context,
