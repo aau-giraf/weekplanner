32fe7e53a5f693a8b920f10af2d4bb456c014c68
swuf <swuf@users.noreply.github.com>
2019-04-10 22:01:30 +0200

    Add functionality to listen to a stream to determine whether the button should be enabled or disabled.
    Add documentation on all public members.

--- lib/widgets/giraf_button_widget.dart
+++ lib/widgets/giraf_button_widget.dart
@@ -1,93 +1,140 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
 
-class GirafDefaultButton extends StatefulWidget {
-  final String text;
-  final IconData icon;
-  final double width;
-  final double height;
-  final Function onPressed;
-
-  const GirafDefaultButton({
+/// A button for the Giraf application.
+class GirafButton extends StatefulWidget {
+  /// A button for the Giraf application.
+  /// The button can contain some text and an icon both of which are optional.
+  /// isEnabled determines whether the button is enabled or disabled by default.
+  /// isEnabledStream is a stream which is listened to, to update the
+  /// enabled/disabled state of the button.
+  const GirafButton({
     Key key,
     this.text,
     this.icon,
     this.width = double.infinity,
     this.height = 50.0,
     this.onPressed,
+    this.isEnabled = true,
+    this.isEnabledStream,
   }) : super(key: key);
 
+  /// The text placed at the center of the button.
+  final String text;
+
+  /// The icon placed next to the text on the button.
+  final IconData icon;
+
+  /// The width of the button.
+  final double width;
+
+  /// The height of the button.
+  final double height;
+
+  /// The function to be called when the button is pressed.
+  /// The function must be a void funtion with no input parameters.
+  final VoidCallback onPressed;
+
+  /// Determines whether the button is enabled or disabled by default. If
+  /// isEnabledStream is also supplied, the latest emitted item from the stream
+  /// will determine whether the button is enabled. If the stream emits a null
+  /// value, isEnabled will be used instead.
+  final bool isEnabled;
+
+  /// A stream which tells whether the button should be enabled or disabled.
+  /// If the stream emits a null value, the value of isEnabled will be used
+  /// instead.
+  final Observable<bool> isEnabledStream;
+
   @override
-  _GirafDefaultButtonState createState() => _GirafDefaultButtonState();
+  _GirafButtonState createState() => _GirafButtonState();
 }
 
-class _GirafDefaultButtonState extends State<GirafDefaultButton> {
-  final Gradient gradientDefault = const LinearGradient(colors: <Color>[
+class _GirafButtonState extends State<GirafButton> {
+  @override
+  void initState() {
+    _isEnabled = widget.isEnabled;
+    _isPressed = false;
+    if (widget.isEnabledStream != null) {
+      _isEnabledSubscription = widget.isEnabledStream.listen((bool value) =>
+          setState(() => {
+                _isEnabled = value ?? widget.isEnabled
+              })); // If a null value is emitted reset enabled state to default.
+    }
+    super.initState();
+  }
+
+  final Gradient _gradientDefault = const LinearGradient(colors: <Color>[
     Color.fromARGB(0xff, 0xff, 0xcd, 0x59),
     Color.fromARGB(0xff, 0xff, 0x9d, 0x00)
   ], begin: Alignment(0.5, -1.0), end: Alignment(0.5, 1.0));
 
-  final Gradient gradientPressed = const LinearGradient(colors: <Color>[
+  final Gradient _gradientPressed = const LinearGradient(colors: <Color>[
     Color.fromARGB(0xff, 0xd4, 0xad, 0x2f),
     Color.fromARGB(0xff, 0xff, 0x9d, 0x00)
   ], begin: Alignment(0.5, -1.0), end: Alignment(0.5, 1.0));
 
-  final Gradient gradientDisabled = const LinearGradient(colors: <Color>[
+  final Gradient _gradientDisabled = const LinearGradient(colors: <Color>[
     Color.fromARGB(0x46, 0xff, 0xcd, 0x59),
     Color.fromARGB(0x46, 0xff, 0x9d, 0x00)
   ], begin: Alignment(0.5, -1.0), end: Alignment(0.5, 1.0));
 
-  final Color borderDefault = const Color.fromARGB(0xff, 0x8a, 0x6e, 0x00);
-  final Color borderPressed = const Color.fromARGB(0xff, 0x49, 0x37, 0x00);
-  final Color borderDisabled = const Color.fromARGB(0xa6, 0x8a, 0x6e, 0x00);
+  final Color _borderDefault = const Color.fromARGB(0xff, 0x8a, 0x6e, 0x00);
+  final Color _borderPressed = const Color.fromARGB(0xff, 0x49, 0x37, 0x00);
+  final Color _borderDisabled = const Color.fromARGB(0xa6, 0x8a, 0x6e, 0x00);
 
-  bool isPressed = false;
-  bool isEnabled = true;
+  bool _isPressed;
+  bool _isEnabled;
+  StreamSubscription<bool> _isEnabledSubscription;
 
   @override
   Widget build(BuildContext context) {
-    Widget result = Container(
+    final Widget result = Container(
       width: widget.width,
       height: widget.height,
       decoration: BoxDecoration(
-        gradient: isEnabled
-            ? (isPressed ? gradientPressed : gradientDefault)
-            : gradientDisabled,
+        gradient: _isEnabled
+            ? (_isPressed ? _gradientPressed : _gradientDefault)
+            : _gradientDisabled,
         borderRadius: BorderRadius.circular(15),
         border: Border.all(
-            color: isEnabled
-                ? (isPressed ? borderPressed : borderDefault)
-                : borderDisabled,
+            color: _isEnabled
+                ? (_isPressed ? _borderPressed : _borderDefault)
+                : _borderDisabled,
             width: 2),
       ),
       child: Material(
         color: Colors.transparent,
-        child: InkWell(
-            onTap: () => _onTapped(widget.onPressed), child: widgetsOnButton()),
+        child: _isEnabled
+            ? InkWell(
+                onTap: () => _onTapped(widget.onPressed),
+                child: _widgetsOnButton())
+            : _widgetsOnButton(),
       ),
     );
-    isPressed = false;
     return result;
   }
 
-  void _onTapped(Function widgetOnPressed) {
-    if (isEnabled) {
-      setState(() => {isPressed = true});
+  void _onTapped(VoidCallback widgetOnPressed) {
+    if (_isEnabled) {
+      setState(() => {_isPressed = true});
       widgetOnPressed();
     }
   }
 
-  Widget widgetsOnButton() {
-    TextStyle textStyle = TextStyle(
+  Widget _widgetsOnButton() {
+    final TextStyle textStyle = TextStyle(
       color: Colors.black,
     );
+
     if (widget.text != null && widget.icon != null)
       return Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Icon(widget.icon),
-          SizedBox(
+          const SizedBox(
             width: 10,
           ),
           Text(
@@ -102,11 +149,18 @@ class _GirafDefaultButtonState extends State<GirafDefaultButton> {
         widget.text,
         style: textStyle,
       ));
-    else if (widget.icon != null)
+    else if (widget.icon != null) {
       return Center(
         child: Icon(widget.icon),
       );
+    }
 
     return null;
+  }
+
+  @override
+  void dispose() {
+    _isEnabledSubscription?.cancel();
+    super.dispose();
   }
 }
