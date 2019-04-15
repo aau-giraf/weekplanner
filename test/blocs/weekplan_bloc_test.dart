57c7943d1c3e282d3b51eaddd22f3172fdaae8ff
Tobias Palludan <tobiaspall@yahoo.dk>
2019-04-15 15:46:17 +0200

    Added addActivity test

--- test/blocs/weekplan_bloc_test.dart
+++ test/blocs/weekplan_bloc_test.dart
@@ -1,10 +1,14 @@
 import 'package:api_client/api/api.dart';
 import 'package:api_client/api/user_api.dart';
 import 'package:api_client/api/week_api.dart';
+import 'package:api_client/models/activity_model.dart';
 import 'package:api_client/models/enums/role_enum.dart';
 import 'package:api_client/models/giraf_user_model.dart';
+import 'package:api_client/models/pictogram_model.dart';
 import 'package:api_client/models/username_model.dart';
 import 'package:api_client/models/week_model.dart';
+import 'package:api_client/models/weekday_model.dart';
+import 'package:async_test/async_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:test_api/test_api.dart';
@@ -29,12 +33,29 @@ class MockUserApi extends Mock implements UserApi {
 void main() {
   WeekplanBloc weekplanBloc;
   Api api;
+  final WeekModel week = WeekModel(
+      thumbnail: PictogramModel(
+          imageUrl: null,
+          imageHash: null,
+          accessLevel: null,
+          title: null,
+          id: null,
+          lastEdit: null),
+      days: <WeekdayModel>[
+        WeekdayModel(activities: <ActivityModel>[], day: null)
+      ],
+      name: 'Week',
+      weekNumber: 1,
+      weekYear: 2019);
 
   setUp(() {
     api = Api('any');
 
     api.user = MockUserApi();
     api.week = MockWeekApi();
+    when(api.week.update(any, any, any, any)).thenAnswer((_) {
+      return Observable<WeekModel>.just(week);
+    });
 
     weekplanBloc = WeekplanBloc(api);
   });
@@ -49,12 +70,30 @@ void main() {
     });
   });
 
-  test('Adds an activity to a given weekplan', () {
-    final WeekModel week = WeekModel(name: 'test week');
-    final UsernameModel user = ;
+  test('Adds an activity to a given weekplan', async((DoneFn done) {
+    final UsernameModel user =
+        UsernameModel(role: Role.Guardian.toString(), name: 'User', id: '1');
 
+    final ActivityModel activity = ActivityModel(
+        order: null,
+        isChoiceBoard: null,
+        state: null,
+        id: null,
+        pictogram: null);
+
+    weekplanBloc.userWeek.skip(1).listen((UserWeekModel userWeek) {
+      verify(api.week.update(any, any, any, any));
+      expect(userWeek.week, week);
+      expect(userWeek.user, user);
+      expect(userWeek.week.days.first.activities.length, 1);
+      expect(userWeek.week.days.first.activities.first, activity);
+      done();
+    });
+
+    // Used by the addActivity
     weekplanBloc.setWeek(week, user);
 
-
-  });
+    weekplanBloc.addActivity(activity, 0);
+    
+  }));
 }
