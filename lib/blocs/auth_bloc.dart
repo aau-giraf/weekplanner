9fb9da96b98efa574e4f08de99749f51715734da
Thue Iversen <Thue-V-Iversen@hotmail.com>
2019-04-16 11:19:35 +0200

    Flutter/feature 138 (#156)
    * Made wrong username/password NotifyDialog for login screen
    
    * Working on tests
    
    * Finished tests
    
    * Rewrote authbloc to avoid sending context around, and fixed tests
    
    * small change
    
    * Fixed small errors and fixed Linter
    
    * Added some documentation
    
    * More comments fixed
    
    * Update lib/screens/login_screen.dart
    
    Co-Authored-By: Tricky12321 <Thue-V-Iversen@hotmail.com>

--- lib/blocs/auth_bloc.dart
+++ lib/blocs/auth_bloc.dart
@@ -1,9 +1,6 @@
-import 'package:flutter/material.dart';
+import 'package:api_client/api/api.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:weekplanner/blocs/bloc_base.dart';
-import 'package:api_client/api/api.dart';
-import 'package:weekplanner/routes.dart';
-import 'package:weekplanner/widgets/loading_spinner_widget.dart';
 
 /// All about Authentication. Login, logout, etc.
 class AuthBloc extends BlocBase {
@@ -17,18 +14,23 @@ class AuthBloc extends BlocBase {
   final Api _api;
 
   /// Whether or not the user is logged in
-  Stream<bool> get loggedIn => _loggedIn.stream;
+  Observable<bool> get loggedIn => _loggedIn.stream;
 
   /// Start with providing false as the logged in status
   final BehaviorSubject<bool> _loggedIn = BehaviorSubject<bool>.seeded(false);
 
   /// Authenticates the user with the given [username] and [password]
-  void authenticate(String username, String password, BuildContext context) {
-    showLoadingSpinner(context, false);
+  void authenticate(String username, String password) {
+    // Show the Loading Spinner, with a callback of 2 seconds.
+    // Call the API login function
     _api.account.login(username, password).take(1).listen((bool status) {
-      Routes.pop(context);
-      _loggedIn.add(status);
-      loggedInUsername = username;
+      // Set the status
+      // If there is a successful login, remove the loading spinner,
+      // and push the status to the stream
+      if (status) {
+        _loggedIn.add(status);
+        loggedInUsername = username;
+      }
     });
   }
 
