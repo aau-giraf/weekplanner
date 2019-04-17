cce8095612b08d10c2ffc53ae546e55a18ce44bf
Daniel Moesgaard <moesgaarda@gmail.com>
2019-04-17 13:32:02 +0200

    Commented public members

--- lib/models/enums/app_bar_icons_enum.dart
+++ lib/models/enums/app_bar_icons_enum.dart
@@ -1,24 +1,45 @@
 /// Enum to choose which icons are shown in the GirafAppBar.
 enum AppBarIcon{
+  /// Item for accepting
   accept,
+  /// Item for general addition
   add,
+  /// Item for adding a timer
   addTimer,
+  /// Item for going back
   back,
+  /// Item for opening burger menu
   burgerMenu,
+  /// Item for opening camera
   camera,
+  /// Item for cancelling action
   cancel,
+  /// Item for changing to citizen mode
   changeToCitizen,
+  /// Item for changing to guardian mode
   changeToGuardian,
+  /// Item for copying content
   copy,
+  /// Item for deleting content
   delete,
+  /// Item for editing content
   edit,
+  /// Item for requesting help
   help,
+  /// Item for going back to home screen
   home,
+  /// Item for logging out
   logout,
+  /// Item for going to profile
   profile,
+  /// Item for redoing an action
   redo,
+  /// Item for saving
   save,
+  /// Item for starting a search
   search,
+  /// Item for going to settings page
   settings,
+  /// Item for undoing action
   undo
 }
