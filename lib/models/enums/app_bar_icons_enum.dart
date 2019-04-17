911475d57c456631263e24b31f839be2992733bd
Daniel Moesgaard <moesgaarda@gmail.com>
2019-04-17 13:32:48 +0200

    Fixied typ0s

--- lib/models/enums/app_bar_icons_enum.dart
+++ lib/models/enums/app_bar_icons_enum.dart
@@ -1,45 +1,65 @@
 /// Enum to choose which icons are shown in the GirafAppBar.
-enum AppBarIcon{
-  /// Item for accepting
+enum AppBarIcon {
+  /// Icon for accepting
   accept,
-  /// Item for general addition
+
+  /// Icon for general addition
   add,
-  /// Item for adding a timer
+
+  /// Icon for adding a timer
   addTimer,
-  /// Item for going back
+
+  /// Icon for going back
   back,
-  /// Item for opening burger menu
+
+  /// Icon for opening burger menu
   burgerMenu,
-  /// Item for opening camera
+
+  /// Icon for opening camera
   camera,
-  /// Item for cancelling action
+
+  /// Icon for cancelling action
   cancel,
-  /// Item for changing to citizen mode
+
+  /// Icon for changing to citizen mode
   changeToCitizen,
-  /// Item for changing to guardian mode
+
+  /// Icon for changing to guardian mode
   changeToGuardian,
-  /// Item for copying content
+
+  /// Icon for copying content
   copy,
-  /// Item for deleting content
+
+  /// Icon for deleting content
   delete,
-  /// Item for editing content
+
+  /// Icon for editing content
   edit,
-  /// Item for requesting help
+
+  /// Icon for requesting help
   help,
-  /// Item for going back to home screen
+
+  /// Icon for going back to home screen
   home,
-  /// Item for logging out
+
+  /// Icon for logging out
   logout,
-  /// Item for going to profile
+
+  /// Icon for going to profile
   profile,
-  /// Item for redoing an action
+
+  /// Icon for redoing an action
   redo,
-  /// Item for saving
+
+  /// Icon for saving
   save,
-  /// Item for starting a search
+
+  /// Icon for starting a search
   search,
-  /// Item for going to settings page
+
+  /// Icon for going to settings page
   settings,
-  /// Item for undoing action
+
+  /// Icon for undoing action
   undo
 }
