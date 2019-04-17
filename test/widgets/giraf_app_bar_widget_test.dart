ea89a0f4f2c0d4e54d77b9359bbd5009940832fb
Unknown <jenspeturtrondarson@gmail.com>
2019-04-17 12:32:21 +0200

    initial tests written

--- test/widgets/giraf_app_bar_widget_test.dart
+++ test/widgets/giraf_app_bar_widget_test.dart
@@ -4,6 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:weekplanner/blocs/auth_bloc.dart';
 import 'package:weekplanner/blocs/toolbar_bloc.dart';
 import 'package:weekplanner/di.dart';
+import 'package:weekplanner/models/enums/app_bar_icons_enum.dart';
 import 'package:weekplanner/providers/api/api.dart';
 import 'package:weekplanner/widgets/giraf_app_bar_widget.dart';
 import 'package:mockito/mockito.dart';
@@ -28,7 +29,7 @@ void main() {
 
   // Used to wrap a widget into a materialapp, otherwise the widget is not
   // testable
-  Widget makeTestableWidget ({Widget child})  {
+  Widget makeTestableWidget({Widget child}) {
     return MaterialApp(
       home: child,
     );
@@ -42,4 +43,23 @@ void main() {
     expect(find.text('Ugeplan'), findsOneWidget);
   });
 
+  testWidgets('Display default icons when given no icons to display', (WidgetTester tester) async {
+    final GirafAppBar girafAppBar =
+        GirafAppBar(title: 'Ugeplan', appBarIcons: null);
+
+    await tester.pumpWidget(makeTestableWidget(child: girafAppBar));
+    await tester.pump();
+    expect(find.byTooltip('Log ud'), findsOneWidget);
+    expect(find.byTooltip('Indstillinger'), findsOneWidget);
+  });
+
+  testWidgets('Change to citizen button is displayed', (WidgetTester tester) async {
+    final GirafAppBar girafAppBar =
+      GirafAppBar(title: 'Ugeplan', appBarIcons: <AppBarIcon>[AppBarIcon.changeToCitizen]);
+
+    await tester.pumpWidget(makeTestableWidget(child: girafAppBar));
+    await tester.pump();
+
+    expect(find.byTooltip('Skift til borger tilstand'), findsOneWidget);
+  });
 }
