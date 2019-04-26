64c61eda8c8ba2260b07e1556c5b1ca12a10b37c
Thue Iversen <Thue-V-Iversen@hotmail.com>
2019-04-26 12:18:59 +0200

    Changed text sized of ChooseCitizenScreen to be uniform across screen… (#197)
    * Changed text sized of ChooseCitizenScreen to be uniform across screen sizes
    
    * Removed comment

--- lib/widgets/citizen_avatar_widget.dart
+++ lib/widgets/citizen_avatar_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:api_client/models/username_model.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
@@ -8,47 +10,63 @@ class CitizenAvatar extends StatelessWidget {
   final UsernameModel usernameModel;
   final VoidCallback onPressed;
 
+  bool _isTablet(MediaQueryData query) {
+    final Size size = query.size;
+    final double diagonal =
+        sqrt((size.width * size.width) + (size.height * size.height));
+    return diagonal > 1100.0;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final MediaQueryData query = MediaQuery.of(context);
     return Padding(
       padding: const EdgeInsets.only(bottom: 30),
       child: GestureDetector(
         onTap: onPressed,
         child: Container(
             child: Column(
-              children: <Widget>[
-                Expanded(
-                  child: AspectRatio(
-                    aspectRatio: 1,
-                    child: Column(
-                      children: <Widget>[
-                        Expanded(
-                          child: AspectRatio(
-                            aspectRatio: 1,
-                            child: Container(
-                              child: const CircleAvatar(
-                                radius: 20,
-                                backgroundImage: AssetImage(
-                                    'assets/login_screen_background_image.png'),
-                              ),
-                            ),
+          children: <Widget>[
+            Expanded(
+              child: AspectRatio(
+                aspectRatio: 1,
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                      child: AspectRatio(
+                        aspectRatio: 1,
+                        child: Container(
+                          child: const CircleAvatar(
+                            radius: 20,
+                            backgroundImage: AssetImage(
+                                'assets/login_screen_background_image.png'),
                           ),
                         ),
-                        Center(
-                          child: AutoSizeText(
-                            usernameModel.name.length <= 15
-                                ? usernameModel.name
-                                : usernameModel.name.substring(0, 14) + '..',
-                            style: const TextStyle(fontSize: 40.0),
-                            maxLines: 1,
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
-                  ),
+                    ConstrainedBox(
+                      constraints: BoxConstraints(
+                        minWidth: 200.0,
+                        maxWidth: 200.0,
+                        minHeight: 15.0,
+                        maxHeight: _isTablet(query) ? 50.0 : 15.0,
+                      ),
+                      child: Center(
+                        child: AutoSizeText(
+                          usernameModel.name.length <= 15
+                              ? usernameModel.name
+                              : usernameModel.name.substring(0, 14) + '..',
+                          style:
+                              TextStyle(fontSize: _isTablet(query) ? 30.0 : 20),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-              ],
-            )),
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
