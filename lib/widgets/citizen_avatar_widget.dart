e3b3a1972b1ede1fae1ebbb66eb133b1d9608ed1
Casper Frydkjær <casperfryd@gmail.com>
2019-04-26 13:23:44 +0200

    Release/fix 09citizen avatar widget test (#199)
    * Flutter/feature 085 (#168)
    
    * Made a GirafButtonWidget
    
    The button follows the style guide on the wiki. To create a button you specify a String and an IconData, which will be placed on the button. Neither the String nor IconData is required.
    
    * Implement disabled button.
    
    * Add functionality to listen to a stream to determine whether the button should be enabled or disabled.
    
    Add documentation on all public members.
    
    * Fix linting error.
    
    * Change button to only call setState when necessary.
    Add required to the onPressed parameter.
    
    * Add the following changes to GirafButton:
    
    - Automatically adjust width of button to fit the text and icon.
    - The icon parameter is now of type Widget instead of IconData, so non-standard icons can be used.
    - The button now changes back to its unpressed state when released.
    - FontSize for text on button is set to 20.
    - Slight tweak to design to better comply with the Design Guide on the wiki.
    
    * Fix linting error.
    
    * Change from using Future.delay to Timer to delay the unpressing of the button.
    
    * Create giraf_button_widget_test.dart
    
    some of the tests for the button is made
    
    * Update giraf_button_widget_test.dart
    
    Button tests edited
    
    * Update giraf_button_widget.dart
    
    GirafButtonState made public
    
    * Change icon type from Widget to ImageIcon.
    
    * Fix test.
    
    * Update giraf_button_widget_test.dart
    
    Test edited.
    
    * Update giraf_button_widget_test.dart
    
    Small change in tests
    
    * Change so that the Timer which controls the unpressing of the button gets closed when the dialog closes.
    
    * Remove comment about design guide.
    
    * Added keys and tests to avatar widget
    
    * Added keys and tests to avatar widget
    
    * fix
    
    * Comments and fix on test
    
    * Merge fix

--- lib/widgets/citizen_avatar_widget.dart
+++ lib/widgets/citizen_avatar_widget.dart
@@ -4,16 +4,21 @@ import 'package:api_client/models/username_model.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
+/// Citizen avatar used for choose citizen screen
 class CitizenAvatar extends StatelessWidget {
+  /// Constructor for the citizens avatar
   const CitizenAvatar({this.usernameModel, this.onPressed});
 
+  /// Usermodel for displaying a user
   final UsernameModel usernameModel;
+
+  /// Callback when pressed
   final VoidCallback onPressed;
 
   bool _isTablet(MediaQueryData query) {
     final Size size = query.size;
     final double diagonal =
-        sqrt((size.width * size.width) + (size.height * size.height));
+    sqrt((size.width * size.width) + (size.height * size.height));
     return diagonal > 1100.0;
   }
 
@@ -26,48 +31,51 @@ class CitizenAvatar extends StatelessWidget {
         onTap: onPressed,
         child: Container(
             child: Column(
-          children: <Widget>[
-            Expanded(
-              child: AspectRatio(
-                aspectRatio: 1,
-                child: Column(
-                  children: <Widget>[
-                    Expanded(
-                      child: AspectRatio(
-                        aspectRatio: 1,
-                        child: Container(
-                          child: const CircleAvatar(
-                            radius: 20,
-                            backgroundImage: AssetImage(
-                                'assets/login_screen_background_image.png'),
+              children: <Widget>[
+                Expanded(
+                  child: AspectRatio(
+                    aspectRatio: 1,
+                    child: Column(
+                      children: <Widget>[
+                        Expanded(
+                          child: AspectRatio(
+                            aspectRatio: 1,
+                            child: Container(
+                              child: const CircleAvatar(
+                                key: Key('WidgetAvatar'),
+                                radius: 20,
+                                backgroundImage: AssetImage(
+                                    'assets/login_screen_background_image.png'),
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ),
-                    ConstrainedBox(
-                      constraints: BoxConstraints(
-                        minWidth: 200.0,
-                        maxWidth: 200.0,
-                        minHeight: 15.0,
-                        maxHeight: _isTablet(query) ? 50.0 : 15.0,
-                      ),
-                      child: Center(
-                        child: AutoSizeText(
-                          usernameModel.name.length <= 15
-                              ? usernameModel.name
-                              : usernameModel.name.substring(0, 14) + '..',
-                          style:
+                        ConstrainedBox(
+                          constraints: BoxConstraints(
+                            minWidth: 200.0,
+                            maxWidth: 200.0,
+                            minHeight: 15.0,
+                            maxHeight: _isTablet(query) ? 50.0 : 15.0,
+                          ),
+                          child: Center(
+                            child: AutoSizeText(
+                              usernameModel.name.length <= 15
+                                  ? usernameModel.name
+                                  : usernameModel.name.substring(0, 14) + '..',
+                              key: const Key('WidgetText'),
+                              style:
                               TextStyle(fontSize: _isTablet(query) ? 30.0 : 20),
-                        ),
-                      ),
-                    )
-                  ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          ],
-        )),
-      ),
+              ],
+            )),
+      )
+      ,
     );
   }
 }
