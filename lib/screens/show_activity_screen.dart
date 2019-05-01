08dd5d83541fd25cf4718b59871135484f62ee4a
Martin Simonsen <msimon16@student.aau.dk>
2019-05-01 15:21:06 +0200

    Dialog for choosing time can now be shown with a method

--- lib/screens/show_activity_screen.dart
+++ lib/screens/show_activity_screen.dart
@@ -6,18 +6,22 @@ import 'package:api_client/models/activity_model.dart';
 import 'package:api_client/models/enums/activity_state_enum.dart';
 import 'package:api_client/models/username_model.dart';
 import 'package:api_client/models/week_model.dart';
+import 'package:weekplanner/routes.dart';
 import 'package:weekplanner/widgets/giraf_app_bar_widget.dart';
+import 'package:weekplanner/widgets/giraf_button_widget.dart';
+import 'package:weekplanner/widgets/giraf_title_header.dart';
 
 /// Screen to show information about an activity, and change the state of it.
 class ShowActivityScreen extends StatelessWidget {
   /// Constructor
-  ShowActivityScreen(WeekModel weekModel, this._activity, UsernameModel
-  girafUser,
+  ShowActivityScreen(
+      WeekModel weekModel, this._activity, UsernameModel girafUser,
       {Key key})
       : super(key: key) {
     _pictoImageBloc.load(_activity.pictogram);
     _activityBloc.load(weekModel, _activity, girafUser);
   }
+
   final ActivityModel _activity;
 
   final PictogramImageBloc _pictoImageBloc =
@@ -30,23 +34,24 @@ class ShowActivityScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Orientation orientation = MediaQuery.of(context).orientation;
-
-    return buildScreenFromOrientation(orientation);
+    ///Used to check if the keyboard is visible
+    return buildScreenFromOrientation(orientation, context);
   }
 
   /// Build the activity screens in a row or column
   /// depending on the orientation of the device.
-  Scaffold buildScreenFromOrientation(Orientation orientation) {
+  Scaffold buildScreenFromOrientation(
+      Orientation orientation, BuildContext context) {
     Widget childContainer;
 
     if (orientation == Orientation.portrait) {
       childContainer = Column(
-        children: buildScreen(),
+        children: buildScreen(context),
       );
     } else if (orientation == Orientation.landscape) {
       childContainer = Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: buildScreen(),
+        children: buildScreen(context),
       );
     }
 
@@ -58,7 +63,7 @@ class ShowActivityScreen extends StatelessWidget {
   }
 
   /// Builds the activity.
-  List<Widget> buildScreen() {
+  List<Widget> buildScreen(BuildContext context) {
     return <Widget>[
       Expanded(
         flex: 6,
@@ -69,7 +74,7 @@ class ShowActivityScreen extends StatelessWidget {
               padding: const EdgeInsets.all(20),
               child: Card(
                 child: Column(
-                  children: buildActivity(),
+                  children: buildActivity(context),
                 ),
               ),
             ),
@@ -80,7 +85,7 @@ class ShowActivityScreen extends StatelessWidget {
   }
 
   /// Builds the activity widget.
-  List<Widget> buildActivity() {
+  List<Widget> buildActivity(BuildContext context) {
     return <Widget>[
       const Center(child: Padding(padding: EdgeInsets.all(8.0))),
       Expanded(
@@ -116,7 +121,7 @@ class ShowActivityScreen extends StatelessWidget {
                       );
                     }))),
       ),
-      buildButtonBar()
+      buildButtonBar(),
     ];
   }
 
@@ -162,5 +167,155 @@ class ShowActivityScreen extends StatelessWidget {
               // Key is used for testing the widget.
               key: Key(_activity.id.toString()));
         });
+  }
+
+  /// returns a dialog where time can be decided for an activity(timer)
+  Widget buildTimerDialog(BuildContext context) {
+    final bool keyboardVisible = MediaQuery.of(context).viewInsets.bottom > 0;
+    final bool isInPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+    final TextEditingController txtController = TextEditingController();
+    return AlertDialog(
+      contentPadding: const EdgeInsets.all(0.0),
+      titlePadding: const EdgeInsets.all(0.0),
+      shape: Border.all(
+          color: const Color.fromRGBO(112, 112, 112, 1), width: 5.0),
+      title: const Center(
+          child: GirafTitleHeader(
+            title: 'Vælg tid for aktivitet',
+          )),
+      content: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const Center(child: Text('Indtast tid')),
+          const Center(
+            child: Text(
+              'Timer : Minutter : Sekunder',
+              style: TextStyle(
+                  fontSize: 10,
+                  color: Color.fromRGBO(170, 170, 170, 1)),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+            child: Container(
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1),
+                  borderRadius:
+                  const BorderRadius.all(Radius.circular(20.0)),
+                  color: Colors.white),
+              padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
+              child: TextField(
+                key: const Key('TimerKey'),
+                keyboardType: TextInputType.number,
+                textAlign: TextAlign.center,
+                controller: txtController,
+                textDirection: TextDirection.ltr,
+                cursorColor: Colors.white,
+                decoration: const InputDecoration.collapsed(
+                  hintText: '00:00',
+                  hintStyle: TextStyle(
+                      color: Color.fromRGBO(170, 170, 170, 1)),
+                  fillColor: Colors.white,
+                ),
+                onChanged: (String input) {
+                  String placeholderText;
+                  String stringToAddd = '';
+                  if (input.length == 1) {
+                    txtController.text = '00:0' + input;
+                  } else {
+                    placeholderText =
+                        input.substring(0, input.length);
+                    placeholderText =
+                        placeholderText.replaceAll(':', '');
+                    for (int i = 0; i < placeholderText.length; i++) {
+                      if (placeholderText.substring(i, i + 1) !=
+                          '0') {
+                        if (placeholderText.length - i == 2) {
+                          stringToAddd = '00:' +
+                              placeholderText.substring(
+                                  i, placeholderText.length);
+                          break;
+                        } else if (placeholderText.length < 4) {
+                          stringToAddd = '0' + placeholderText;
+                          stringToAddd = stringToAddd.replaceRange(
+                              stringToAddd.length - 2,
+                              stringToAddd.length,
+                              ':' +
+                                  stringToAddd.substring(
+                                      stringToAddd.length - 2,
+                                      stringToAddd.length));
+                          break;
+                        } else {
+                          stringToAddd = placeholderText;
+                          if (stringToAddd.length > 4) {
+                            if (stringToAddd.substring(0, 1) == '0') {
+                              stringToAddd =
+                                  stringToAddd.replaceRange(0, 1, '');
+                            } else {
+                              stringToAddd =
+                                  stringToAddd.replaceRange(
+                                      stringToAddd.length - 4,
+                                      stringToAddd.length - 2,
+                                      ':' +
+                                          stringToAddd.substring(
+                                              stringToAddd.length - 4,
+                                              stringToAddd.length -
+                                                  2));
+                            }
+                          }
+                          stringToAddd = stringToAddd.replaceRange(
+                              stringToAddd.length - 2,
+                              stringToAddd.length,
+                              ':' +
+                                  stringToAddd.substring(
+                                      stringToAddd.length - 2,
+                                      stringToAddd.length));
+                          break;
+                        }
+                      }
+                    }
+                    txtController.text = stringToAddd;
+                  }
+                  txtController.selection = TextSelection.collapsed(
+                      offset: txtController.text.length);
+                },
+              ),
+            ),
+          ),
+          Visibility(
+            visible: (isInPortrait && !keyboardVisible) ||
+                (isInPortrait && keyboardVisible) ||
+                (!isInPortrait && !keyboardVisible),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Flexible(
+                  fit: FlexFit.loose,
+                  child: Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 0, 5, 10),
+                      child: GirafButton(
+                        onPressed: () => Routes.pop(context),
+                        icon: const ImageIcon(
+                            AssetImage('assets/icons/cancel.png')),
+                      )),
+                ),
+                Flexible(
+                  fit: FlexFit.loose,
+                  child: Padding(
+                      padding: const EdgeInsets.fromLTRB(5, 0, 10, 10),
+                      child: GirafButton(
+                        onPressed: () => null,
+                        icon: const ImageIcon(
+                            AssetImage('assets/icons/accept.png')),
+                      )),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
