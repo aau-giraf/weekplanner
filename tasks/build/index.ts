4772604624f2294ac8274f327604b282618a1004
Tricky <thue.v.iversen@gmail.com>
2019-05-01 10:47:52 +0200

    Trying to fix Azure

---
+++ tasks/build/index.ts
@@ -0,0 +1,124 @@
+import * as path from "path";
+import * as task from "azure-pipelines-task-lib/task";
+
+const FLUTTER_TOOL_PATH_ENV_VAR: string = 'FlutterToolPath';
+
+async function main(): Promise<void> {
+    // 1. Check flutter environment
+    var flutterPath = task.getVariable(FLUTTER_TOOL_PATH_ENV_VAR) || process.env[FLUTTER_TOOL_PATH_ENV_VAR] || task.getInput('flutterDirectory', false);
+    flutterPath = path.join(flutterPath, "flutter")
+    if (!flutterPath) {
+        throw new Error(`The '${FLUTTER_TOOL_PATH_ENV_VAR}' environment variable must be set before using this task (you can use 'flutterinstall' task).`);
+    }
+
+    // 2. Get target
+    let target = task.getInput('target', true);
+
+    // 3. Move current working directory to project
+    let projectDirectory = task.getPathInput('projectDirectory', false, false);
+    if (projectDirectory) {
+        task.debug(`Moving to ${projectDirectory}`);
+        task.cd(projectDirectory);
+    }
+    task.debug(`Project's directory : ${task.cwd()}`);
+
+    // 4. Get common input
+    let debugMode = task.getBoolInput('debugMode', false);
+    let buildName = task.getInput('buildName', false);
+    let buildNumber = task.getInput('buildNumber', false);
+    let buildFlavour = task.getInput('buildFlavour', false);
+
+    // 5. Builds
+    if (target === "all" || target === "ios") {
+        let targetPlatform = task.getInput('iosTargetPlatform', false);
+        let codesign = task.getBoolInput('iosCodesign', false);
+        await buildIpa(flutterPath, targetPlatform == "simulator", codesign, buildName, buildNumber, debugMode, buildFlavour);
+    }
+
+    if (target === "all" || target === "apk") {
+        let targetPlatform = task.getInput('apkTargetPlatform', false);
+        await buildApk(flutterPath, targetPlatform, buildName, buildNumber, debugMode, buildFlavour);
+    }
+
+    task.setResult(task.TaskResult.Succeeded, "Application built");
+}
+
+async function buildApk(flutter: string, targetPlatform?: string, buildName?: string, buildNumber?: string, debugMode?: boolean, buildFlavour?: string) {
+
+    var args = [
+        "build",
+        "apk"
+    ];
+
+    if (debugMode) {
+        args.push("--debug");
+    }
+
+    if (targetPlatform) {
+        args.push("--target-platform=" + targetPlatform);
+    }
+
+    if (buildName) {
+        args.push("--build-name=" + buildName);
+    }
+
+    if (buildNumber) {
+        args.push("--build-number=" + buildNumber);
+    }
+
+    if (buildFlavour) {
+        args.push("--flavor=" + buildFlavour);
+    }
+
+    var result = await task.exec(flutter, args);
+
+    if (result !== 0) {
+        throw new Error("apk build failed");
+    }
+}
+
+async function buildIpa(flutter: string, simulator?: boolean, codesign?: boolean, buildName?: string, buildNumber?: string, debugMode?: boolean, buildFlavour?: string) {
+
+    var args = [
+        "build",
+        "ios"
+    ];
+
+    if (debugMode) {
+        args.push("--debug");
+    }
+
+    if (simulator) {
+        args.push("--simulator");
+
+        if (!debugMode) {
+            args.push("--debug"); // simulator can only be built in debug
+        }
+    }
+    else if (codesign) {
+        args.push("--codesign");
+    }
+
+    if (buildName) {
+        args.push("--build-name=" + buildName);
+    }
+
+    if (buildNumber) {
+        args.push("--build-number=" + buildNumber);
+    }
+
+    if (!simulator) {
+        if (buildFlavour) {
+            args.push("--flavor=" + buildFlavour);
+        }
+    }
+
+    var result = await task.exec(flutter, args);
+    if (result !== 0) {
+        throw new Error("ios build failed");
+    }
+}
+
+main().catch(error => {
+    task.setResult(task.TaskResult.Failed, error);
+});
---
+++ tasks/build/tests/apk.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const path = require("path");
+const fs = require("fs");
+const mr = require("azure-pipelines-task-lib/mock-run");
+const taskPath = path.join(__dirname, "../index.js");
+var runner = new mr.TaskMockRunner(taskPath);
+function assertDirectory(path) {
+    if (!fs.existsSync(path)) {
+        fs.mkdirSync(path);
+    }
+}
+// ENV
+const rootPath = path.join(__dirname, "..", "..", "..");
+const tempPath = path.join(rootPath, "temp");
+const agentPath = path.join(tempPath, "agent");
+const dropPath = path.join(tempPath, "drop");
+process.env["BUILD_BUILDNUMBER"] = "1";
+assertDirectory(tempPath);
+assertDirectory(agentPath);
+assertDirectory(dropPath);
+assertDirectory(process.env["AGENT_HOMEDIRECTORY"] = path.join(agentPath, "home"));
+assertDirectory(process.env["AGENT_TOOLSDIRECTORY"] = path.join(agentPath, "tools"));
+assertDirectory(process.env["AGENT_TEMPDIRECTORY"] = path.join(agentPath, "temp"));
+assertDirectory(process.env["AGENT_BUILDDIRECTORY"] = path.join(agentPath, "build"));
+// Run install tests
+process.env["FlutterToolPath"] = path.join(agentPath, "tools", "Flutter", "0.9.6-dev", "macos", "flutter", "bin");
+runner.setInput("target", "apk");
+runner.setInput("buildName", "com.aloisdeniel.vsts");
+runner.setInput("buildNumber", "12");
+runner.setInput("projectDirectory", path.join(rootPath, "sample_project"));
+runner.setInput("outputDirectory", dropPath);
+runner.setInput("buildFlavour", "development");
+runner.setInput("debugMode", "false");
+runner.run(true);
