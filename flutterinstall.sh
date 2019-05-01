d75afddbb30e1bf97e837355126fc206219dcb43
Tricky <thue.v.iversen@gmail.com>
2019-05-01 11:19:45 +0200

    TesTestt

--- flutterinstall.sh
+++ flutterinstall.sh
@@ -2,5 +2,5 @@
 echo "Downloading Flutter..."
 wget https://storage.googleapis.com/flutter_infra/releases/stable/linux/flutter_linux_v1.2.1-stable.tar.xz
 echo "Extracting flutter..."
-tar xf $(FlutterToolPath)/flutter_linux_v1.2.1-stable.tar.xz
+tar xf flutter_linux_v1.2.1-stable.tar.xz
 echo "Done"
