0cdf12f6580b6dcbe3caf0ddb55c4ec4e9e21df1
Tricky <thue.v.iversen@gmail.com>
2019-05-01 11:31:50 +0200

    test

--- flutterinstall.sh
+++ flutterinstall.sh
@@ -3,4 +3,7 @@ echo "Downloading Flutter..."
 wget https://storage.googleapis.com/flutter_infra/releases/stable/linux/flutter_linux_v1.2.1-stable.tar.xz
 echo "Extracting flutter..."
 tar xf flutter_linux_v1.2.1-stable.tar.xz
+echo "Installing flutter"
+cd flutter
+./bin/flutter precache
 echo "Done"
