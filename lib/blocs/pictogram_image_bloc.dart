c3b389921cd2e480cea38ecd49eaef12e56de904
Tobias Palludan <tobiaspall@yahoo.dk>
2019-05-02 11:28:06 +0200

    Initial idea

--- lib/blocs/pictogram_image_bloc.dart
+++ lib/blocs/pictogram_image_bloc.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
+import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:weekplanner/blocs/bloc_base.dart';
 import 'package:api_client/models/pictogram_model.dart';
 import 'package:api_client/api/api.dart';
+import 'package:mutex/mutex.dart';
 
 /// Pictogram-Image Business Logic Component
 class PictogramImageBloc extends BlocBase {
@@ -19,6 +21,13 @@ class PictogramImageBloc extends BlocBase {
 
   final Api _api;
 
+  static Map<int, Image> _cache = <int, Image>{};
+  static Queue<int> _cacheQueue = Queue<int>();
+  static int _cacheMaxSize = 100;
+
+  /// Lock for adding pictograms to cache
+  static Mutex lock = Mutex();
+
   /// Initializes loading of a specific pictogram image
   ///
   /// The [pictogram] model should contain an ID which the API can then fetch.
@@ -27,8 +36,40 @@ class PictogramImageBloc extends BlocBase {
   }
 
   /// Initialize loading of a specific [PictogramModel] from its [id].
-  void loadPictogramById(int id) {
-    _api.pictogram.getImage(id).listen(_image.add);
+  void loadPictogramById(int id) async {
+    await lock.acquire();
+    try {
+      if (_cache.containsKey(id)) {
+        // Renew queue position
+        _cacheQueue.removeWhere((x) => x == id);
+        _cacheQueue.add(id);
+
+        _image.add(_cache[id]);
+      } else {
+        Observable<Image>.retry(() {
+          return _api.pictogram.getImage(id);
+        }, 3)
+            .listen((Image image) async {
+          _image.add(image);
+
+          await lock.acquire();
+          try {
+            _cache.putIfAbsent(id, () => image);
+            _cacheQueue.add(id);
+
+            while (_cacheQueue.length > _cacheMaxSize) {
+              _cache.remove(_cacheQueue.removeFirst());
+            }
+          } finally {
+            lock.release();
+          }
+        }, onError: (Object error) {
+          print("shit");
+        });
+      }
+    } finally {
+      lock.release();
+    }
   }
 
   @override
