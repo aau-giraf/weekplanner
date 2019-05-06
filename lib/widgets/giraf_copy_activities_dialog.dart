bacfbae9729bc95c602d71d8430f0d63fcabb552
Jonas Alsen <grann.alsen@gmail.com>
2019-05-06 11:54:38 +0200

    Fix some requested changes to copy_activities_dialog

--- lib/widgets/giraf_copy_activities_dialog.dart
+++ lib/widgets/giraf_copy_activities_dialog.dart
@@ -8,10 +8,10 @@ import 'package:weekplanner/widgets/giraf_title_header.dart';
 /// a list of checkboxes presented in the dialog. The dialog consists of a
 /// title, a description, and two buttons and a dropdown menu. One button to
 /// cancel the action and one button to accept and perform the action.
-class GirafCopyActivitiesDialog extends StatefulWidget {
+class GirafCopyActivitiesDialog extends StatelessWidget {
   /// The dialog displays the title and description, with two buttons and a
   /// list of checkboxes
-  const GirafCopyActivitiesDialog(
+  GirafCopyActivitiesDialog(
       {Key key,
       @required this.title,
       this.description,
@@ -35,45 +35,12 @@ class GirafCopyActivitiesDialog extends StatefulWidget {
   /// the method to call when the confirmation button is pressed
   final void Function(List<bool>, BuildContext) confirmOnPressed;
 
-  @override
-  _State createState() => _State();
-}
-
-class _State extends State<GirafCopyActivitiesDialog> {
   static const Color checkboxColor = Color(0xFFFF9D00);
 
   final List<bool> _checkboxDays = List<bool>.filled(7, false);
 
   void _checkboxChanged(bool value, Weekday day) =>
-      setState(() => _checkboxDays[day.index] = value);
-
-  void _checkboxMonChanged(bool value) {
-    _checkboxChanged(value, Weekday.Monday);
-  }
-
-  void _checkboxTueChanged(bool value) {
-    _checkboxChanged(value, Weekday.Tuesday);
-  }
-
-  void _checkboxWedChanged(bool value) {
-    _checkboxChanged(value, Weekday.Wednesday);
-  }
-
-  void _checkboxThuChanged(bool value) {
-    _checkboxChanged(value, Weekday.Thursday);
-  }
-
-  void _checkboxFriChanged(bool value) {
-    _checkboxChanged(value, Weekday.Friday);
-  }
-
-  void _checkboxSatChanged(bool value) {
-    _checkboxChanged(value, Weekday.Saturday);
-  }
-
-  void _checkboxSunChanged(bool value) {
-    _checkboxChanged(value, Weekday.Sunday);
-  }
+      _checkboxDays[day.index] = value;
 
   @override
   Widget build(BuildContext context) {
@@ -84,7 +51,7 @@ class _State extends State<GirafCopyActivitiesDialog> {
           Border.all(color: const Color.fromRGBO(112, 112, 112, 1), width: 5.0),
       title: Center(
           child: GirafTitleHeader(
-        title: widget.title,
+        title: title,
       )),
       content: Column(
         mainAxisAlignment: MainAxisAlignment.start,
@@ -99,7 +66,7 @@ class _State extends State<GirafCopyActivitiesDialog> {
                 padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
                 child: Text(
                   //if description is null, its replaced with empty.
-                  widget.description ?? '',
+                  description ?? '',
                   textAlign: TextAlign.center,
                 ),
               ))
@@ -116,30 +83,12 @@ class _State extends State<GirafCopyActivitiesDialog> {
                         flex: 5,
                         child: Column(
                           children: <Widget>[
-                            CheckboxListTile(
-                              key: const Key('MonCheckbox'),
-                              value: _checkboxDays[Weekday.Monday.index],
-                              onChanged: _checkboxMonChanged,
-                              title: const Text('Mandag'),
-                              controlAffinity: ListTileControlAffinity.trailing,
-                              activeColor: checkboxColor,
-                            ),
-                            CheckboxListTile(
-                              key: const Key('WedCheckbox'),
-                              value: _checkboxDays[Weekday.Wednesday.index],
-                              onChanged: _checkboxWedChanged,
-                              title: const Text('Onsdag'),
-                              controlAffinity: ListTileControlAffinity.trailing,
-                              activeColor: checkboxColor,
-                            ),
-                            CheckboxListTile(
-                              key: const Key('FriCheckbox'),
-                              value: _checkboxDays[Weekday.Friday.index],
-                              onChanged: _checkboxFriChanged,
-                              title: const Text('Fredag'),
-                              controlAffinity: ListTileControlAffinity.trailing,
-                              activeColor: checkboxColor,
-                            ),
+                            _buildCheckboxListTile(Weekday.Monday,
+                                const Key('MonCheckbox'), 'Mandag'),
+                            _buildCheckboxListTile(Weekday.Wednesday,
+                                const Key('WedCheckbox'), 'Onsdag'),
+                            _buildCheckboxListTile(Weekday.Friday,
+                                const Key('FriCheckbox'), 'Fredag')
                           ],
                         ),
                       ),
@@ -147,30 +96,12 @@ class _State extends State<GirafCopyActivitiesDialog> {
                         flex: 5,
                         child: Column(
                           children: <Widget>[
-                            CheckboxListTile(
-                              key: const Key('TueCheckbox'),
-                              value: _checkboxDays[Weekday.Tuesday.index],
-                              onChanged: _checkboxTueChanged,
-                              title: const Text('Tirsdag'),
-                              controlAffinity: ListTileControlAffinity.trailing,
-                              activeColor: checkboxColor,
-                            ),
-                            CheckboxListTile(
-                              key: const Key('ThuCheckbox'),
-                              value: _checkboxDays[Weekday.Thursday.index],
-                              onChanged: _checkboxThuChanged,
-                              title: const Text('Torsdag'),
-                              controlAffinity: ListTileControlAffinity.trailing,
-                              activeColor: checkboxColor,
-                            ),
-                            CheckboxListTile(
-                              key: const Key('SatCheckbox'),
-                              value: _checkboxDays[Weekday.Saturday.index],
-                              onChanged: _checkboxSatChanged,
-                              title: const Text('Lørdag'),
-                              controlAffinity: ListTileControlAffinity.trailing,
-                              activeColor: checkboxColor,
-                            )
+                            _buildCheckboxListTile(Weekday.Tuesday,
+                                const Key('TueCheckbox'), 'Tirsdag'),
+                            _buildCheckboxListTile(Weekday.Thursday,
+                                const Key('ThuCheckbox'), 'Torsdag'),
+                            _buildCheckboxListTile(Weekday.Saturday,
+                                const Key('SatCheckbox'), 'Lørdag')
                           ],
                         ),
                       )
@@ -182,14 +113,8 @@ class _State extends State<GirafCopyActivitiesDialog> {
                         flex: 5,
                         child: Column(
                           children: <Widget>[
-                            CheckboxListTile(
-                              key: const Key('SunCheckbox'),
-                              value: _checkboxDays[Weekday.Sunday.index],
-                              onChanged: _checkboxSunChanged,
-                              title: const Text('Søndag'),
-                              controlAffinity: ListTileControlAffinity.trailing,
-                              activeColor: checkboxColor,
-                            )
+                            _buildCheckboxListTile(Weekday.Sunday,
+                                const Key('SunCheckbox'), 'Søndag')
                           ],
                         ),
                       ),
@@ -229,10 +154,10 @@ class _State extends State<GirafCopyActivitiesDialog> {
                     padding: const EdgeInsets.only(left: 5),
                     child: GirafButton(
                       key: const Key('DialogConfirmButton'),
-                      text: widget.confirmButtonText,
-                      icon: widget.confirmButtonIcon,
+                      text: confirmButtonText,
+                      icon: confirmButtonIcon,
                       onPressed: () {
-                        widget.confirmOnPressed(_checkboxDays, context);
+                        confirmOnPressed(_checkboxDays, context);
                       },
                     ),
                   ),
@@ -242,6 +167,18 @@ class _State extends State<GirafCopyActivitiesDialog> {
           )
         ],
       ),
+    );
+  }
+
+  CheckboxListTile _buildCheckboxListTile(
+      Weekday weekday, Key checkboxKey, String checkboxTitle) {
+    return CheckboxListTile(
+      key: checkboxKey,
+      value: _checkboxDays[weekday.index],
+      onChanged: (bool value) => _checkboxChanged(value, weekday),
+      title: Text(checkboxTitle),
+      controlAffinity: ListTileControlAffinity.trailing,
+      activeColor: checkboxColor,
     );
   }
 }
