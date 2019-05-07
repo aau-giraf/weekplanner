e2f16b6fee491cc7f004f336e4ed45cc3d7dbab0
LukeRH <lukasronsholt@gmail.com>
2019-05-07 14:04:12 +0200

    Fixed linter

--- lib/screens/weekplan_screen.dart
+++ lib/screens/weekplan_screen.dart
@@ -6,7 +6,6 @@ import 'package:api_client/models/username_model.dart';
 import 'package:api_client/models/week_model.dart';
 import 'package:api_client/models/weekday_model.dart';
 import 'package:flutter/material.dart';
-import 'package:rxdart/rxdart.dart';
 import 'package:tuple/tuple.dart';
 import 'package:weekplanner/blocs/auth_bloc.dart';
 import 'package:weekplanner/blocs/pictogram_image_bloc.dart';
