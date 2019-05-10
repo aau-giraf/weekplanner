5c665a1e618cac342b8c321f446590458b230539
Luke <Lukasronsholt@gmail.com>
2019-05-10 12:23:29 +0200

    Update lib/blocs/add_activity_bloc.dart
    Co-Authored-By: Alexander A. <swuf@users.noreply.github.com>

--- lib/blocs/add_activity_bloc.dart
+++ lib/blocs/add_activity_bloc.dart
@@ -2,7 +2,7 @@ import 'package:weekplanner/blocs/bloc_base.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:flutter/material.dart';
 
-///Bloc used for adding activities to a citizen's day
+/// Bloc used for adding activities to a citizen's day
 class AddActivityBloc extends BlocBase{
 
   /// Stream holding the chosen pictogram
