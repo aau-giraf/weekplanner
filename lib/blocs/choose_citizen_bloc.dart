759b536ca206646d8f70cfc8de646767507319d5
LukeRH <lukasronsholt@gmail.com>
2019-05-10 11:13:18 +0200

    Fix linter warnings

--- lib/blocs/choose_citizen_bloc.dart
+++ lib/blocs/choose_citizen_bloc.dart
@@ -4,8 +4,9 @@ import 'package:api_client/models/username_model.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:weekplanner/blocs/bloc_base.dart';
 
+/// Bloc to obtain all citizens assigned to a guarding
 class ChooseCitizenBloc extends BlocBase {
-  ///
+  /// Default Constructor
   ChooseCitizenBloc(this._api) {
     _api.user.me().flatMap((GirafUserModel user) {
       return _api.user.getCitizens(user.id);
@@ -14,11 +15,12 @@ class ChooseCitizenBloc extends BlocBase {
     });
   }
 
+  /// The stream holding the citizens
+  Stream<List<UsernameModel>> get citizen => _citizens.stream;
+
   final Api _api;
   final BehaviorSubject<List<UsernameModel>> _citizens =
   BehaviorSubject<List<UsernameModel>>.seeded(<UsernameModel>[]);
-
-  Stream<List<UsernameModel>> get citizen => _citizens.stream;
 
   @override
   void dispose() {
