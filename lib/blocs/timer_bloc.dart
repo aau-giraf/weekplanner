2de99775ad595c84d51175de09e1ce86c2db3f66
Thue Vestergaard Iversen <tricky@h840.aau1x.klient.slv312.site.aau.dk>
2019-05-10 12:03:08 +0200

    Fixed comments

--- lib/blocs/timer_bloc.dart
+++ lib/blocs/timer_bloc.dart
@@ -9,6 +9,7 @@ import 'package:api_client/models/activity_model.dart';
 
 /// Logic for activities
 class TimerBloc extends BlocBase {
+  /// Constructor taking the API
   TimerBloc(this._api);
 
   final Api _api;
@@ -19,7 +20,7 @@ class TimerBloc extends BlocBase {
   /// Stream for the progress of the timer.
   Observable<double> get timerProgressStream => _timerProgressStream.stream;
 
-  /// BehaviorSubject for the updated weekModel.
+  /// BehaviorSubject for the progress of the timer.
   final BehaviorSubject<double> _timerProgressStream =
       BehaviorSubject<double>.seeded(0.0);
 
@@ -30,10 +31,10 @@ class TimerBloc extends BlocBase {
   final BehaviorSubject<bool> _timerRunningStream =
       BehaviorSubject<bool>.seeded(false);
 
-  /// stream for checking if the timer is running
+  /// Stream for checking if the timer is instantiated.
   Observable<bool> get timerIsInstantiated => _timerInstantiatedStream.stream;
 
-  /// BehaviorSubject for to check if timer is running.
+  /// BehaviorSubject for to check if timer is instantiated.
   final BehaviorSubject<bool> _timerInstantiatedStream =
       BehaviorSubject<bool>.seeded(false);
 
@@ -57,11 +58,13 @@ class TimerBloc extends BlocBase {
   /// Adds a timer to the activity loaded into the timerBloc.
   /// By default the timer will be paused.
   void addTimer(Duration duration) {
+    // Adds a timer to the activityModel
     _activityModel.timer = TimerModel(
         startTime: DateTime.now(),
         progress: 0,
         fullLength: duration.inMilliseconds,
         paused: true);
+    // Update the streams
     _timerInstantiatedStream.add(true);
     _timerProgressStream.add(0);
     _api.activity
@@ -73,12 +76,14 @@ class TimerBloc extends BlocBase {
   /// If the timer is playing the progressCircle will start immediately.
   /// Else it will be paused.
   void initTimer({int updatePeriod = 10}) {
+    // Checks if a stopWatch exist
     if (_stopwatch == null) {
       if (_activityModel.timer != null) {
+        // Calculates the end time of the timer
         final DateTime endTime = _activityModel.timer.startTime.add(Duration(
             milliseconds: _activityModel.timer.fullLength -
                 _activityModel.timer.progress));
-
+        // Checks if the timer is running
         if (_activityModel.timer.startTime.isBefore(DateTime.now()) &&
             DateTime.now().isBefore(endTime) &&
             !_activityModel.timer.paused) {
@@ -96,6 +101,7 @@ class TimerBloc extends BlocBase {
                     c.remaining.inMilliseconds));
           });
         } else if (_activityModel.timer.paused) {
+
           _timerRunningStream.add(false);
           _timerProgressStream.add(1 -
               (1 /
@@ -114,12 +120,13 @@ class TimerBloc extends BlocBase {
   /// the widget.
   /// Updates the timer in the database accordingly.
   void playTimer({int updatePeriod = 10}) {
+    // Makes sure that a timer exist
     if (_activityModel.timer != null && _activityModel.timer.paused) {
       _activityModel.timer.paused = false;
       _activityModel.timer.startTime = DateTime.now();
 
       _stopwatch = Stopwatch();
-
+      // Calculates the end time
       final DateTime _endTime = _activityModel.timer.startTime.add(Duration(
           milliseconds:
               _activityModel.timer.fullLength - _activityModel.timer.progress));
@@ -142,6 +149,7 @@ class TimerBloc extends BlocBase {
 
   /// Pauses the timer and updates the timer in the database accordingly.
   void pauseTimer() {
+    // First make sure that a timer exists and it is running
     if (_activityModel.timer != null &&
         _timerStream != null &&
         !_activityModel.timer.paused) {
@@ -181,11 +189,13 @@ class TimerBloc extends BlocBase {
   }
 
   void _resetCounterAndStopwatch() {
+    // Stops any timers and cancels all listners
     if (_stopwatch != null) {
       _stopwatch.stop();
       _countDown.cancel();
       _timerStream.cancel();
     }
+
     _stopwatch = null;
     _countDown = null;
     _timerStream = null;
