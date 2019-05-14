a2df70b20e45eb6402c6d1c6bf2aeee54b7ba453
Jonas <grann.alsen@gmail.com>
2019-05-14 13:04:40 +0200

    Release fix/275 (#280)
    * Add title to image selector
    
    * Add new gallery icon
    
    * Åh nej linter
    
    * Add save icon to 'Gem ugeplan' button
    
    * Åh nej test

--- lib/screens/new_weekplan_screen.dart
+++ lib/screens/new_weekplan_screen.dart
@@ -21,11 +21,10 @@ class NewWeekplanScreen extends StatelessWidget {
   }
 
   final NewWeekplanBloc _bloc;
+  final TextStyle _style = const TextStyle(fontSize: 20);
 
   @override
   Widget build(BuildContext context) {
-    const TextStyle _style = TextStyle(fontSize: 20);
-
     return Scaffold(
         appBar: GirafAppBar(title: 'Ny ugeplan'),
         body: ListView(children: <Widget>[
@@ -109,9 +108,9 @@ class NewWeekplanScreen extends StatelessWidget {
                 padding:
                     const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
                 child: GirafButton(
+                  icon: const ImageIcon(AssetImage('assets/icons/save.png')),
                   key: const Key('NewWeekplanSaveBtnKey'),
                   text: 'Gem ugeplan',
-                  width: 160,
                   isEnabled: false,
                   isEnabledStream: _bloc.allInputsAreValidStream,
                   onPressed: () {
@@ -132,7 +131,15 @@ class NewWeekplanScreen extends StatelessWidget {
       return GestureDetector(
         onTap: () => _openPictogramSearch(context),
         child: Card(
-          child: FittedBox(fit: BoxFit.contain, child: const Icon(Icons.image)),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: Text('Vælg billede til ugeplan', style: _style),
+              ),
+              Expanded(child: Image.asset('assets/icons/galleryBig.png')),
+            ],
+          ),
         ),
       );
     } else {
