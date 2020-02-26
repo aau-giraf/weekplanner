17a951d88ad92b214390406cfa2cbff550a57273
MrPekar98 <martin.pekar@outlook.dk>
2020-02-26 15:12:27 +0100

    11th version.

--- lib/blocs/timer_bloc.dart
+++ lib/blocs/timer_bloc.dart
@@ -147,7 +147,7 @@ class TimerBloc extends BlocBase {
         _timerProgressStream.add(1 -
             (1 / _activityModel.timer.fullLength * c.remaining.inMilliseconds));
 
-        if (DateTime.now().isAfter(_endTime)) {
+        if (_stopwatch.isRunning && DateTime.now().isAfter(_endTime)) {
           SystemSound.play(SystemSoundType.click);
           
           //audioPlayer.play("SomeULR");
