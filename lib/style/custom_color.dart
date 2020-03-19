c6dcedc93120169cce9b11a4b44486daa9d5c031
lisaniemeyer <d@d.d>
2020-03-19 13:19:10 +0100

    Added color sheet + Standardised colors for the login screen

---
+++ lib/style/custom_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class GirafColors {
+  GirafColors._();
+
+  static const Color transparentGrey = Color.fromRGBO(170, 170, 170, 1);
+  static const Color loginButtonColor = Color.fromRGBO(48, 81, 118, 1);
+
+}
