6c435873858920b637cfa74cbf6829faa91f5c23
Jeppe_Bjørn-Desktop <jbjarn17@student.aau.dk>
2020-03-19 14:51:24 +0100

    Created dependency injection for new_citizen_screen
    Added new method in bootstrap.dart for instantiating a NewCitizenBloc through dependency injection and linked this in the new_citizen_screen.dart.
    
    Note: right now the save citizen button only print mock values.
    Co-Authored-By: Jonas Rodell <jrodel17@student.aau.dk>

--- lib/screens/new_citizen_screen.dart
+++ lib/screens/new_citizen_screen.dart
@@ -1,7 +1,7 @@
 import 'package:api_client/models/username_model.dart';
 import 'package:api_client/models/week_model.dart';
 import 'package:flutter/material.dart';
-import 'package:weekplanner/blocs/new_weekplan_bloc.dart';
+import 'package:weekplanner/blocs/new_citizen_bloc.dart';
 import 'package:weekplanner/di.dart';
 import 'package:weekplanner/routes.dart';
 import 'package:weekplanner/widgets/giraf_app_bar_widget.dart';
@@ -10,7 +10,14 @@ import 'package:weekplanner/widgets/input_fields_weekplan.dart';
 
 /// Screen for creating a new citizen
 class NewCitizenScreen extends StatelessWidget {
-  // TODO: Write new_citizen_bloc and reference dependency
+  /// Screen for creating a new weekplan.
+  /// Requires a [UsernameModel] to be able to save the new weekplan.
+  NewCitizenScreen()
+      : _bloc = di.getDependency<NewCitizenBloc>() {
+    //_bloc.initialize(user);
+  }
+
+  final NewCitizenBloc _bloc;
 
   @override
   Widget build(BuildContext context) {
@@ -48,7 +55,7 @@ class NewCitizenScreen extends StatelessWidget {
             //TODO Fix width
             icon: const ImageIcon(AssetImage('assets/icons/save.png')),
             text: 'Gem borger',
-            onPressed: () {},
+            onPressed: () {_bloc.createCitizen("Jonas", "jonas", "password");},
           )
         ],
       ),
