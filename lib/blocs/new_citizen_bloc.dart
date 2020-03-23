a66982e12144a634948183749c53e19414b256cb
Jeppe_Bjørn-Desktop <jbjarn17@student.aau.dk>
2020-03-23 14:41:19 +0100

    Setup sinks in new_citizen_bloc.dart
    Added sinks in new_citizen_bloc.dart

--- lib/blocs/new_citizen_bloc.dart
+++ lib/blocs/new_citizen_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:api_client/api/api.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:weekplanner/blocs/bloc_base.dart';
 
 ///This bloc is used by a guardian to instantiate a new citizen.
@@ -8,6 +9,28 @@ class NewCitizenBloc extends BlocBase {
   NewCitizenBloc(this._api);
 
   final Api _api;
+
+   /// This field controls the display name input field
+  final BehaviorSubject<String> displayNameController =
+      BehaviorSubject<String>();
+   /// This field controls the username input field
+  final BehaviorSubject<String> usernameController =
+      BehaviorSubject<String>();
+   /// This field controls the password input field
+  final BehaviorSubject<String> passwordController =
+      BehaviorSubject<String>();
+   /// This field controls the password verification input field
+  final BehaviorSubject<String> passwordVerifyController =
+      BehaviorSubject<String>();
+
+  /// Handles when the entered display name is changed.
+  Sink<String> get onDisplayNameChange => displayNameController.sink;
+  /// Handles when the entered display name is changed.
+  Sink<String> get onUsernameChange => usernameController.sink;
+  /// Handles when the entered display name is changed.
+  Sink<String> get onPasswordChange => passwordController.sink;
+  /// Handles when the entered display name is changed.
+  Sink<String> get onPasswordVerifyChange => passwordVerifyController.sink;
 
   ///Method called with information about the new citizen.
   ///[displayName] is the name of the citizen.
