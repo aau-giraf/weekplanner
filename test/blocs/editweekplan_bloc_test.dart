e64c23ab1e1f9bcccdcf66eafddcec1c853e01a4
MrPekar98 <martin.pekar@outlook.dk>
2020-03-25 12:22:46 +0100

    Test file setup.

--- test/blocs/editweekplan_bloc_test.dart
+++ test/blocs/editweekplan_bloc_test.dart
@@ -1,7 +1,11 @@
 import 'package:api_client/api/api.dart';
 import 'package:api_client/api/week_api.dart';
+import 'package:api_client/models/username_model.dart';
+import 'package:api_client/models/week_model.dart';
+import 'package:api_client/models/week_name_model.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:weekplanner/blocs/edit_weekplan_bloc.dart';
 
 class MockWeekApi extends Mock implements WeekApi {}
@@ -10,12 +14,31 @@ void main() {
   EditWeekplanBloc bloc;
   Api api;
   MockWeekApi weekApi;
+  final UsernameModel mockUser =
+      UsernameModel(name: 'test', id: 'test', role: 'test');
+  final WeekModel weekModel = WeekModel(weekNumber: 8, weekYear: 2020);
+  final WeekNameModel weekNameModel =
+      WeekNameModel(name: 'name', weekNumber: 8, weekYear: 2020);
+  final List<WeekNameModel> weekNameModelList = <WeekNameModel>[];
 
   void setupApiCalls() {
+    weekNameModelList.add(weekNameModel);
 
+    when(weekApi.getNames('test')).thenAnswer(
+        (_) => BehaviorSubject<List<WeekNameModel>>.seeded(weekNameModelList));
+
+    when(weekApi
+        .get('test', weekNameModel.weekYear, weekNameModel.weekNumber))
+        .thenAnswer((_) => BehaviorSubject<WeekModel>.seeded(weekModel));
   }
 
   setUp(() {
+    api = Api('any');
+    weekApi = MockWeekApi();
+    api.week = weekApi;
+    bloc = EditWeekplanBloc(api);
+    bloc.initializeEditBloc(mockUser, weekModel);
 
+    setupApiCalls();
   });
 }
