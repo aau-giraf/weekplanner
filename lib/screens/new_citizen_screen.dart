11a836c0de964271c62fb416cd64cce1652aad6d
Jeppe_Bjørn-Desktop <jbjarn17@student.aau.dk>
2020-03-25 15:40:50 +0100

    Refactoring
    + Removed unsued imports
    + Removed finished TODO

--- lib/screens/new_citizen_screen.dart
+++ lib/screens/new_citizen_screen.dart
@@ -1,14 +1,11 @@
 import 'package:api_client/models/giraf_user_model.dart';
 import 'package:api_client/models/username_model.dart';
-import 'package:api_client/models/week_model.dart';
 import 'package:flutter/material.dart';
-import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:weekplanner/blocs/new_citizen_bloc.dart';
 import 'package:weekplanner/di.dart';
 import 'package:weekplanner/routes.dart';
 import 'package:weekplanner/widgets/giraf_app_bar_widget.dart';
 import 'package:weekplanner/widgets/giraf_button_widget.dart';
-import 'package:weekplanner/widgets/input_fields_weekplan.dart';
 
 /// Screen for creating a new citizen
 class NewCitizenScreen extends StatelessWidget {
@@ -23,7 +20,6 @@ class NewCitizenScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: GirafAppBar(title: 'Ny borger',),
       body: ListView(
