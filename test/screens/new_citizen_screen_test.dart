1bf9740b029e2a1a3073c6005010454a2ee8b853
jrodel17 <jrodel17@student.aau.dk>
2020-03-30 14:30:13 +0200

    Update new_citizen_screen_test.dart
    +Added UI rendering tests
    
    Co-Authored-By: Jeppe Skovby Bjørn <jbjarn17@users.noreply.github.com>

--- test/screens/new_citizen_screen_test.dart
+++ test/screens/new_citizen_screen_test.dart
@@ -2,12 +2,18 @@ import 'package:api_client/api/api.dart';
 import 'package:api_client/api/user_api.dart';
 import 'package:api_client/models/enums/role_enum.dart';
 import 'package:api_client/models/giraf_user_model.dart';
+import 'package:api_client/models/username_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:weekplanner/blocs/auth_bloc.dart';
 import 'package:weekplanner/blocs/new_citizen_bloc.dart';
+import 'package:weekplanner/blocs/toolbar_bloc.dart';
+import 'package:weekplanner/di.dart';
 import 'package:weekplanner/screens/new_citizen_screen.dart';
+import 'package:weekplanner/widgets/giraf_app_bar_widget.dart';
+import 'package:weekplanner/widgets/giraf_button_widget.dart';
 import '../test_image.dart';
 
 class MockNewCitizenBloc extends NewCitizenBloc {
@@ -57,10 +63,49 @@ class MockUserApi extends Mock implements UserApi {
 }
 
 void main() {
-  MockNewCitizenBloc mockNewCitizenBloc;
   Api api;
+  MockNewCitizenBloc mockNewCitizenBloc;
+
+  setUp(() {
+    api = Api('any');
+    mockNewCitizenBloc = MockNewCitizenBloc(api);
+
+    di.clearAll();
+    di.registerDependency<AuthBloc>((_) => AuthBloc(api));
+    di.registerDependency<ToolbarBloc>((_) => ToolbarBloc());
+    di.registerDependency<NewCitizenBloc>((_) => mockNewCitizenBloc);
+  });
 
   testWidgets('Screen renders', (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(home: NewCitizenScreen()));
   });
+
+  testWidgets('The screen has a Giraf App Bar', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: NewCitizenScreen()));
+
+    expect(find.byWidgetPredicate((Widget widget) => widget is GirafAppBar),
+        findsOneWidget);
+  });
+
+  testWidgets('Input fields are rendered', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: NewCitizenScreen()));
+
+    expect(find.byType(TextFormField), findsNWidgets(4));
+  });
+
+  testWidgets('Buttons are rendered', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: NewCitizenScreen()));
+
+    expect(find.byType(GirafButton), findsNWidgets(2));
+  });
+
+  testWidgets('Error text is shown on invalid title input',
+          (WidgetTester tester) async {
+        mockNewCitizenBloc.acceptAllInputs = false;
+        await tester.pumpWidget(MaterialApp(home: NewCitizenScreen()));
+        await tester.pump();
+
+        expect(find.text('Titel skal angives'), findsOneWidget);
+      });
+
 }
