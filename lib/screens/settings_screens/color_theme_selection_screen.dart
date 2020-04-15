d706c46c7835523cbcbc81c34f6ca61e6e8b09d3
Andreas <atorp17@student.aau.dk>
2020-04-15 12:36:11 +0200

    added temporary hardcoded settingsCheckMarkButtons

--- lib/screens/settings_screens/color_theme_selection_screen.dart
+++ lib/screens/settings_screens/color_theme_selection_screen.dart
@@ -1,26 +1,68 @@
+import 'package:api_client/models/settings_model.dart';
 import 'package:api_client/models/username_model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:weekplanner/blocs/settings_bloc.dart';
+import 'package:weekplanner/di.dart';
 import 'package:weekplanner/widgets/giraf_app_bar_widget.dart';
+import 'package:weekplanner/widgets/settings_widgets/settings_section.dart';
+import 'package:weekplanner/widgets/settings_widgets/settings_section_checkboxButton.dart';
+import 'package:weekplanner/widgets/settings_widgets/settings_section_item.dart';
 
 /// This class is used to select the color theme for a citizen's weekplans
 class ColorThemeSelectorScreen extends StatelessWidget {
   /// Constructor
   ColorThemeSelectorScreen({@required UsernameModel user}) : _user = user {
-    //_settingsBloc.loadSettings(_user);
+    _settingsBloc.loadSettings(_user);
   }
 
+  final SettingsBloc _settingsBloc = di.getDependency<SettingsBloc>();
   final UsernameModel _user;
 
-
-@override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: GirafAppBar(
-          title: _user.name + ': indstillinger',
+          title: _user.name + ': Farver på ugeplan',
         ),
-        body: Container(width: 0, height: 0),
-    );
+        body: StreamBuilder<SettingsModel>(
+            stream: _settingsBloc.settings,
+            builder: (BuildContext context,
+                AsyncSnapshot<SettingsModel> settingsSnapshot) {
+              if (settingsSnapshot.hasData) {
+                final SettingsModel _settingsModel = settingsSnapshot.data;
+                return ListView(
+                  children: <Widget>[
+                    SettingsSection(
+                        'Farvetema', _createSettingList(_settingsModel)),
+                  ],
+                );
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            }));
   }
 
+  List<SettingsSectionItem> _createSettingList(SettingsModel _settingsModel) {
+    final List<SettingsSectionItem> settingsList = <SettingsSectionItem>[];
+    settingsList.add(SettingsCheckMarkButton(
+        1, _settingsModel.nrOfDaysToDisplay, 'Standard', () {
+      _settingsModel.nrOfDaysToDisplay = 1;
+      _settingsBloc.updateSettings(_user.id, _settingsModel);
+    }));
+    settingsList.add(SettingsCheckMarkButton(
+        5, _settingsModel.nrOfDaysToDisplay, 'Blå/Hvid', () {
+      _settingsModel.nrOfDaysToDisplay = 5;
+      _settingsBloc.updateSettings(_user.id, _settingsModel);
+    }));
+    settingsList.add(SettingsCheckMarkButton(
+        7, _settingsModel.nrOfDaysToDisplay, 'Sort/Hvid', () {
+      _settingsModel.nrOfDaysToDisplay = 7;
+      _settingsBloc.updateSettings(_user.id, _settingsModel);
+    }));
+
+    return settingsList;
+  }
 }
