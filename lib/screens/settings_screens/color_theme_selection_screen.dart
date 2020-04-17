03f4d09cd670ded8a6fcac1bd1e292b5c68ff18d
Simkort <sim465vman@live.dk>
2020-04-17 10:50:40 +0200

    Standard Colors now implemented with tests.

--- lib/screens/settings_screens/color_theme_selection_screen.dart
+++ lib/screens/settings_screens/color_theme_selection_screen.dart
@@ -1,12 +1,15 @@
+import 'package:api_client/models/enums/weekday_enum.dart';
 import 'package:api_client/models/settings_model.dart';
 import 'package:api_client/models/username_model.dart';
+import 'package:api_client/models/weekday_color_model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:weekplanner/blocs/settings_bloc.dart';
 import 'package:weekplanner/di.dart';
+import 'package:weekplanner/style/custom_color.dart';
 import 'package:weekplanner/widgets/giraf_app_bar_widget.dart';
 import 'package:weekplanner/widgets/settings_widgets/settings_section.dart';
-import 'package:weekplanner/widgets/settings_widgets/settings_section_checkboxButton.dart';
+import 'package:weekplanner/widgets/settings_widgets/settings_section_colorThemeButton.dart';
 import 'package:weekplanner/widgets/settings_widgets/settings_section_item.dart';
 
 /// This class is used to select the color theme for a citizen's weekplans
@@ -51,22 +54,125 @@ class ColorThemeSelectorScreen extends StatelessWidget {
   // _settingsModel.weekDayColors.
   List<SettingsSectionItem> _createSettingList(SettingsModel _settingsModel) {
     final List<SettingsSectionItem> settingsList = <SettingsSectionItem>[];
-    settingsList.add(SettingsCheckMarkButton(
-        1, _settingsModel.nrOfDaysToDisplay, 'Standard', () {
-      _settingsModel.nrOfDaysToDisplay = 1;
+    settingsList.add(ColorThemeCheckMarkButton(_standardColorSetting(),
+        _settingsModel.weekDayColors , 'Standard', () {
+      _settingsModel.weekDayColors = _standardColorSetting();
       _settingsBloc.updateSettings(_user.id, _settingsModel);
     }));
-    settingsList.add(SettingsCheckMarkButton(
-        5, _settingsModel.nrOfDaysToDisplay, 'Blå/Hvid', () {
-      _settingsModel.nrOfDaysToDisplay = 5;
+    settingsList.add(ColorThemeCheckMarkButton(_blueWhiteColorSetting(),
+        _settingsModel.weekDayColors, 'Blå/Hvid', () {
+      _settingsModel.weekDayColors = _blueWhiteColorSetting();
       _settingsBloc.updateSettings(_user.id, _settingsModel);
     }));
-    settingsList.add(SettingsCheckMarkButton(
-        7, _settingsModel.nrOfDaysToDisplay, 'Sort/Hvid', () {
-      _settingsModel.nrOfDaysToDisplay = 7;
+    settingsList.add(ColorThemeCheckMarkButton(_greyWhiteColorSetting(),
+        _settingsModel.weekDayColors, 'Grå/Hvid', () {
+      _settingsModel.weekDayColors = _greyWhiteColorSetting();
       _settingsBloc.updateSettings(_user.id, _settingsModel);
     }));
 
     return settingsList;
   }
+
+  List<WeekdayColorModel> _standardColorSetting(){
+    final List<WeekdayColorModel> weekDayColors = <WeekdayColorModel>[];
+    weekDayColors.add(WeekdayColorModel(
+        hexColor: '#08A045',
+        day: Weekday.Monday
+    ));
+    weekDayColors.add(WeekdayColorModel(
+        hexColor: '#540D6E',
+        day: Weekday.Tuesday
+    ));
+    weekDayColors.add(WeekdayColorModel(
+        hexColor: '#F77F00',
+        day: Weekday.Wednesday
+    ));
+    weekDayColors.add(WeekdayColorModel(
+        hexColor: '#004777',
+        day: Weekday.Thursday
+    ));
+    weekDayColors.add(WeekdayColorModel(
+        hexColor: '#F9C80E',
+        day: Weekday.Friday
+    ));
+    weekDayColors.add(WeekdayColorModel(
+        hexColor: '#DB2B39',
+        day: Weekday.Saturday
+    ));
+    weekDayColors.add(WeekdayColorModel(
+        hexColor: '#FFFFFF',
+        day: Weekday.Sunday
+    ));
+
+    return weekDayColors;
+  }
+
+  List<WeekdayColorModel> _blueWhiteColorSetting(){
+    final List<WeekdayColorModel> weekDayColors = <WeekdayColorModel>[];
+    weekDayColors.add(WeekdayColorModel(
+        hexColor: GirafColors.blue.toString(),
+        day: Weekday.Monday
+    ));
+    weekDayColors.add(WeekdayColorModel(
+        hexColor: GirafColors.white.toString(),
+        day: Weekday.Tuesday
+    ));
+    weekDayColors.add(WeekdayColorModel(
+        hexColor: GirafColors.blue.toString(),
+        day: Weekday.Wednesday
+    ));
+    weekDayColors.add(WeekdayColorModel(
+        hexColor: GirafColors.white.toString(),
+        day: Weekday.Thursday
+    ));
+    weekDayColors.add(WeekdayColorModel(
+        hexColor: GirafColors.blue.toString(),
+        day: Weekday.Friday
+    ));
+    weekDayColors.add(WeekdayColorModel(
+        hexColor: GirafColors.white.toString(),
+        day: Weekday.Saturday
+    ));
+    weekDayColors.add(WeekdayColorModel(
+        hexColor: GirafColors.blue.toString(),
+        day: Weekday.Sunday
+    ));
+
+    return weekDayColors;
+  }
+
+  List<WeekdayColorModel> _greyWhiteColorSetting(){
+    final List<WeekdayColorModel> weekDayColors = <WeekdayColorModel>[];
+    weekDayColors.add(WeekdayColorModel(
+        hexColor: GirafColors.grey.toString(),
+        day: Weekday.Monday
+    ));
+    weekDayColors.add(WeekdayColorModel(
+        hexColor: GirafColors.white.toString(),
+        day: Weekday.Tuesday
+    ));
+    weekDayColors.add(WeekdayColorModel(
+        hexColor: GirafColors.grey.toString(),
+        day: Weekday.Wednesday
+    ));
+    weekDayColors.add(WeekdayColorModel(
+        hexColor: GirafColors.white.toString(),
+        day: Weekday.Thursday
+    ));
+    weekDayColors.add(WeekdayColorModel(
+        hexColor: GirafColors.grey.toString(),
+        day: Weekday.Friday
+    ));
+    weekDayColors.add(WeekdayColorModel(
+        hexColor: GirafColors.white.toString(),
+        day: Weekday.Saturday
+    ));
+    weekDayColors.add(WeekdayColorModel(
+        hexColor: GirafColors.grey.toString(),
+        day: Weekday.Sunday
+    ));
+
+    return weekDayColors;
+  }
+
 }
