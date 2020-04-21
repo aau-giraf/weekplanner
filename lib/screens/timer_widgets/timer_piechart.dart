cde812bc814d98c73468129890a1cd526b9a8637
kristiantg <kristiantgregersen@gmail.com>
2020-04-21 14:51:00 +0200

    began building hourglass timer
    Co-Authored-By: mads-bo <mads-bo@users.noreply.github.com>

--- lib/screens/timer_widgets/timer_piechart.dart
+++ lib/screens/timer_widgets/timer_piechart.dart
@@ -1,21 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:weekplanner/blocs/activity_bloc.dart';
-import 'package:weekplanner/blocs/auth_bloc.dart';
-import 'package:weekplanner/blocs/pictogram_image_bloc.dart';
-import 'package:weekplanner/blocs/settings_bloc.dart';
 import 'package:weekplanner/blocs/timer_bloc.dart';
-import 'package:weekplanner/di.dart';
-import 'package:api_client/models/activity_model.dart';
-import 'package:api_client/models/enums/activity_state_enum.dart';
-import 'package:api_client/models/username_model.dart';
-import 'package:weekplanner/models/enums/app_bar_icons_enum.dart';
-import 'package:weekplanner/models/enums/timer_running_mode.dart';
-import 'package:weekplanner/models/enums/weekplan_mode.dart';
-import 'package:weekplanner/routes.dart';
-import 'package:weekplanner/widgets/giraf_activity_time_picker_dialog.dart';
-import 'package:weekplanner/widgets/giraf_app_bar_widget.dart';
-import 'package:weekplanner/widgets/giraf_button_widget.dart';
-import 'package:weekplanner/widgets/giraf_confirm_dialog.dart';
 import '../../style/custom_color.dart' as theme;
 
 /// Class for drawing a timer piechart
