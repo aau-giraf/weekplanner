a99d3f6e2a2c760d63f453720ab0f9dbffe5fded
rasmushr97 <rasmushr97@gmail.com>
2020-04-21 13:19:00 +0200

    Added test to see if the the copyToCitizens screen is launched when trying to copy a weekplan to other citizens

---
+++ lib/screens/copy_to_citizens_screen.dart
@@ -0,0 +1,78 @@
+import 'package:api_client/models/username_model.dart';
+import 'package:flutter/material.dart';
+import 'package:weekplanner/blocs/choose_citizen_bloc.dart';
+import 'package:weekplanner/di.dart';
+import 'package:weekplanner/models/enums/app_bar_icons_enum.dart';
+import 'package:weekplanner/routes.dart';
+import 'package:weekplanner/screens/weekplan_selector_screen.dart';
+import 'package:weekplanner/widgets/citizen_avatar_widget.dart';
+import 'package:weekplanner/widgets/giraf_app_bar_widget.dart';
+
+/// The screen to choose a citizen
+class copyToCitizensScreen extends StatelessWidget {
+  final ChooseCitizenBloc _bloc = di.getDependency<ChooseCitizenBloc>();
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+
+    final bool portrait =
+      MediaQuery.of(context).orientation == Orientation.portrait;
+
+    return Scaffold(
+      body: Container(
+        width: screenSize.width,
+        height: screenSize.height,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/login_screen_background_image.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        // Creates a new Dialog
+        child: Dialog(
+          child: Scaffold(
+            appBar: GirafAppBar(
+              title: 'Vælg borger',
+              appBarIcons: const <AppBarIcon, VoidCallback>{
+                AppBarIcon.logout: null
+              },
+            ),
+            body: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: StreamBuilder<List<UsernameModel>>(
+                  stream: _bloc.citizen,
+                  builder: (BuildContext context,
+                    AsyncSnapshot<List<UsernameModel>> snapshot) {
+                    if (snapshot.connectionState != ConnectionState.waiting) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 0,
+                          horizontal: 20,
+                        ),
+                        child: GridView.count(
+                          crossAxisCount: portrait ? 2 : 4,
+                          children: snapshot.data
+                            .map<Widget>((UsernameModel user) =>
+                            CitizenAvatar(
+                              usernameModel: user,
+                              onPressed: () => Routes.push(context,
+                                WeekplanSelectorScreen(user))))
+                            .toList()),
+                      );
+                    } else {
+                      return Container(
+                        child: const Text('Loading...'),
+                      );
+                    }
+                  },
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
