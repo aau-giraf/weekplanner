c16509b2b80f017ae05163e61a8159b19aa9ab49
kristiantg <kristiantgregersen@gmail.com>
2020-04-24 11:06:03 +0200

    Centered working hourglass
    Co-Authored-By: mads-bo <mads-bo@users.noreply.github.com>

--- lib/screens/timer_widgets/timer_hourglass.dart
+++ lib/screens/timer_widgets/timer_hourglass.dart
@@ -27,15 +27,15 @@ class TimerHourglass extends StatelessWidget {
   }
 }
 
-Stack _drawHourglass(AsyncSnapshot<double> timerProgressSnapshot) {
-  return Stack(
-    children: <Widget>[
-      Image(
-        image: const AssetImage('assets/hourglass_sand.png'),
-      ),
-      LayoutBuilder(
-          builder: (BuildContext context3, BoxConstraints constraints) {
-        return Column(
+LayoutBuilder _drawHourglass(AsyncSnapshot<double> timerProgressSnapshot) {
+  return LayoutBuilder(
+      builder: (BuildContext context3, BoxConstraints constraints) {
+    return Stack(
+      children: <Widget>[
+        Image(
+            image: const AssetImage('assets/hourglass_sand.png'),
+            width: constraints.maxWidth),
+        Column(
           children: <Widget>[
             // Hourglass is implemented using 3 containers expanding
             // based values from the other containers and time stream
@@ -86,20 +86,16 @@ Stack _drawHourglass(AsyncSnapshot<double> timerProgressSnapshot) {
                 width: constraints.maxWidth,
                 color: Colors.white),
           ],
-        );
-      }),
-      Image(
-        image: const AssetImage('assets/hourglass.png'),
-      ),
-    ],
-  );
+        ),
+        Image(
+          image: const AssetImage('assets/hourglass.png'),
+          width: constraints.maxWidth,
+        ),
+      ],
+    );
+  });
 }
 
 Image _drawDoneHourglass() {
   return Image(image: const AssetImage('assets/hourglass_done.png'));
-}
-
-CircularProgressIndicator _drawLoading() {
-  debugPrint('Drawing loading');
-  return CircularProgressIndicator();
 }
