e810caa286094b15cd1e1f2e1157995df8301b8f
Kasjus <kasper.jepsen@hotmail.com>
2020-04-24 12:21:20 +0200

    Checking for conflicts first steps.

--- lib/blocs/copy_weekplan_bloc.dart
+++ lib/blocs/copy_weekplan_bloc.dart
@@ -1,23 +1,53 @@
 import 'package:api_client/api/api.dart';
+import 'package:api_client/api/week_api.dart';
+import 'package:api_client/models/giraf_user_model.dart';
 import 'package:api_client/models/username_model.dart';
 import 'package:api_client/models/week_model.dart';
+import 'package:quiver/async.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:weekplanner/blocs/choose_citizen_bloc.dart';
 
 /// Bloc to copy a weekplan to other users
 class CopyWeekplanBloc extends ChooseCitizenBloc {
   /// Default constructor
-  CopyWeekplanBloc(Api api) : super(api);
+  CopyWeekplanBloc(this._api) : super(_api);
 
   /// The stream that emits the marked activities
   Stream<List<UsernameModel>> get markedUserModels => _markedUserModels.stream;
 
   final BehaviorSubject<List<UsernameModel>> _markedUserModels =
-  BehaviorSubject<List<UsernameModel>>.seeded(<UsernameModel>[]);
+      BehaviorSubject<List<UsernameModel>>.seeded(<UsernameModel>[]);
+  final Api _api;
 
   /// Copies weekplan to all selected citizens
-  void copyToMarkedCitizens(WeekModel weekModel){
+  void copyToMarkedCitizens(WeekModel weekModel, List<UsernameModel> users) {
+    List<UsernameModel> conflictingUsers =
+        getConflictingUsers(users, weekModel);
+    for (UsernameModel user in users) {
+      if (conflictingUsers.contains(user)) {
+        users.remove(user);
+      }
+    }
+  }
 
+  /// Returns a list of all users which already have a weekplan in the same week
+  List getConflictingUsers(List<UsernameModel> users, WeekModel weekModel) {
+    List<UsernameModel> conflictingUsers = <UsernameModel>[];
+    for (UsernameModel user in users) {
+      if (checkCitizenWeekNumber(user, weekModel)) {
+        conflictingUsers.add(user);
+      }
+    }
+    return conflictingUsers;
+  }
+
+  /// Compares a single Citizen's Weekplans with the copied weekplan
+  bool checkCitizenWeekNumber(UsernameModel user, WeekModel weekModel) {
+    if (_api.week.get(user.id, weekModel.weekYear, weekModel.weekYear) !=
+        null) {
+      return true;
+    }
+    return false;
   }
 
   /// Adds a new marked week model to the stream
@@ -31,6 +61,4 @@ class CopyWeekplanBloc extends ChooseCitizenBloc {
 
     _markedUserModels.add(localMarkedUserModels);
   }
-
-
 }
