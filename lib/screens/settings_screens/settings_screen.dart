c08839778f961685128cf367f189a247a436b4d8
MrPekar98 <martin.pekar@outlook.dk>
2020-04-24 18:19:29 +0200

    Re-added handler for color settings.

--- lib/screens/settings_screens/settings_screen.dart
+++ lib/screens/settings_screens/settings_screen.dart
@@ -1,17 +1,19 @@
+import 'package:api_client/models/settings_model.dart';
 import 'package:api_client/models/username_model.dart';
 import 'package:flutter/material.dart';
 import 'package:weekplanner/blocs/settings_bloc.dart';
 import 'package:weekplanner/routes.dart';
-import 'package:weekplanner/screens/settings_screens/color_theme_selection_screen.dart';
 import 'package:weekplanner/screens/settings_screens/number_of_days_selection_screen.dart';
-import 'package:weekplanner/screens/settings_screens/completed_activity_icon_selection_screen.dart';
+import 'package:weekplanner/screens/settings_screens/color_theme_selection_screen.dart';
 import 'package:weekplanner/widgets/giraf_app_bar_widget.dart';
 import 'package:weekplanner/widgets/settings_widgets/settings_section.dart';
 import 'package:weekplanner/widgets/settings_widgets/settings_section_arrow_button.dart';
 import 'package:weekplanner/widgets/settings_widgets/settings_section_checkboxButton.dart';
 import 'package:weekplanner/widgets/settings_widgets/settings_section_item.dart';
+import 'package:weekplanner/widgets/settings_widgets/settings_theme_display_box.dart';
 
 import '../../di.dart';
+import 'completed_activity_icon_selection_screen.dart';
 
 /// Shows all the users settings, and lets them change them
 class SettingsScreen extends StatelessWidget {
@@ -44,13 +46,35 @@ class SettingsScreen extends StatelessWidget {
   }
 
   Widget _buildThemeSection(BuildContext context) {
-    return SettingsSection('Tema', <SettingsSectionItem>[
-      SettingsArrowButton('Farver på ugeplan',
-              () =>
-              Routes.push(context, ColorThemeSelectorScreen(user: _user,))),
-      SettingsArrowButton('Tegn for udførelse',
-              () => Routes.push(context, CompletedActivityIconScreen(_user))),
-    ]);
+    return StreamBuilder<SettingsModel>(
+        stream: _settingsBloc.settings,
+        builder: (BuildContext context,
+            AsyncSnapshot<SettingsModel> settingsSnapshot) {
+          if(settingsSnapshot.hasData) {
+            final SettingsModel settingsModel = settingsSnapshot.data;
+            return SettingsSection('Tema', <SettingsSectionItem>[
+              SettingsArrowButton('Farver på ugeplan',
+                      () =>
+                      Routes.push(
+                          context, ColorThemeSelectorScreen(user: _user)
+                      ).then((Object object) =>
+                          _settingsBloc.loadSettings(_user)),
+                  titleTrailing: ThemeBox.fromHexValues(
+                      settingsModel.weekDayColors[0].hexColor,
+                      settingsModel.weekDayColors[1].hexColor
+                  )
+              ),
+              SettingsArrowButton('Tegn for udførelse',
+                      () => Routes.push(context,
+                          CompletedActivityIconScreen(_user)))
+            ]);
+          }else{
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        }
+    );
   }
 
   Widget _buildOrientationSection() {
