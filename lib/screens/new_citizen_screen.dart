13e078b87a58f33644f1b6b5b87647d474edef52
Riasalit <daniel-vilslev@hotmail.com>
2020-09-30 23:10:36 +0200

    Removed unused imports

--- lib/screens/new_citizen_screen.dart
+++ lib/screens/new_citizen_screen.dart
@@ -1,10 +1,8 @@
-import 'package:api_client/api/api_exception.dart';
 import 'package:api_client/models/giraf_user_model.dart';
 import 'package:flutter/material.dart';
 import 'package:weekplanner/blocs/new_citizen_bloc.dart';
 import 'package:weekplanner/api/errorcode_translater.dart';
 import 'package:weekplanner/di.dart';
-import 'package:weekplanner/widgets/giraf_notify_dialog.dart';
 import 'package:weekplanner/routes.dart';
 import 'package:weekplanner/widgets/giraf_app_bar_widget.dart';
 import 'package:weekplanner/widgets/giraf_button_widget.dart';
