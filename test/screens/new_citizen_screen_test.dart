adf17087f368bfdebfb877d186391b2346e162c4
Kristian Benny Winther Sørensen <43601483+kbwsoorensen@users.noreply.github.com>
2020-10-02 13:46:50 +0200

    Release 2020 S0 R1 (#578)
    * Releasefix 570: Created class for converting Error codes to Danish messages (#574)
    
    * Created class for converting Error codes to Danish messages
    
    The class can be expanded with more of the error codes from the ErrorKey class such that it can be used with all comunication with the api_client
    
    * added unit tests for creating new citizens
    
    Also moved the error handling to the error code translater class for easier use when it needs to be implemented in more classes
    
    * Added comments for some helper functions in the Widget tests
    
    * Removed unused imports
    
    * en test mere til at gøre code coverage glad
    
    Den case burde den aldrig komme ind i normalvis, men nu er testen der i guess
    
    * Removed another unused package
    
    why linter no tell though?
    
    * Releasefix 573: Username validation expanded in new_citizen_bloc.dart (#575)
    
    * Username validation expanded in new_citizen_bloc.dart
    
    Pull Request for #573
    The _usernamevalidation method has been revised from blacklisting spaces only to a whitelist regex that contains only all letters, ÆØÅ, numbers, underscore( _ ) or a hyphen ( - )
    
    The warning message under the text field has been updated to explain this change.
    
    A test has been added to verify this feature in new_citizen_bloc_test.dart.
    
    * Update new_citizen_screen.dart
    
    missing letter in "tom"
    
    Co-authored-by: Riasalit <daniel-vilslev@hotmail.com>
    Co-authored-by: ProgDaniel <43956461+ProgDaniel@users.noreply.github.com>

--- test/screens/new_citizen_screen_test.dart
+++ test/screens/new_citizen_screen_test.dart
@@ -1,5 +1,11 @@
+import 'package:api_client/api/account_api.dart';
+import 'package:api_client/api_client.dart';
+import 'package:api_client/persistence/persistence_client.dart';
+import 'package:http/http.dart' as http;
 import 'package:api_client/api/api.dart';
+import 'package:api_client/api/api_exception.dart';
 import 'package:api_client/api/user_api.dart';
+import 'package:api_client/http/http.dart';
 import 'package:api_client/models/enums/role_enum.dart';
 import 'package:api_client/models/giraf_user_model.dart';
 import 'package:flutter/material.dart';
@@ -14,18 +20,84 @@ import 'package:weekplanner/screens/new_citizen_screen.dart';
 import 'package:weekplanner/widgets/giraf_app_bar_widget.dart';
 import 'package:weekplanner/widgets/giraf_button_widget.dart';
 
+/// Mock Account Api needed to insert errors int othe datastream from a place
+/// where listen().onError could catch it
+class MockAccountApi extends AccountApi {
+  MockAccountApi(PersistenceClient persist)
+      : super(HttpClient(baseUrl: null, persist: persist), persist);
+
+  /// override of the register function, which returns an error
+  /// if 'username' == alreadyExists. Returns a normal GirafUserModel otherwise
+  @override
+  Observable<GirafUserModel> register(
+      String username, String password, String displayName,
+      {@required int departmentId, @required Role role}) {
+    final Map<String, dynamic> body = <String, dynamic>{
+      'username': username,
+      'displayName': displayName,
+      'password': password,
+      'department': departmentId,
+      'id': '2',
+      'role': 2,
+      'roleName': '' + role.toString().split('.').last,
+    };
+
+    if (username == 'alreadyExists') {
+      final http.Response mockHttpResponse = http.Response(
+          // ignore: lines_longer_than_80_chars
+          'message: User already exists, details: A user with the given username already exists, errorKey: UserAlreadyExists',
+          401);
+      final Observable<Response> mockResponse = Observable<Response>.fromFuture(
+          userAlreadyExistsResponse(mockHttpResponse));
+      return mockResponse.map((Response res) => throw ApiException(res));
+    } else if (username == 'defaultError') {
+      final http.Response mockHttpResponse = http.Response(
+          // ignore: lines_longer_than_80_chars
+          'message: something went wrong, details: unexpected error, errorKey: Error',
+          401);
+      final Observable<Response> mockResponse = Observable<Response>.fromFuture(
+          unexpectedErrorResponse(mockHttpResponse));
+      return mockResponse.map((Response res) => throw ApiException(res));
+    }
+    return Observable<GirafUserModel>.fromFuture(createMockUserModel(body));
+  }
+
+  /// Creates the Error for the stream
+  Future<Response> userAlreadyExistsResponse(http.Response response) async {
+    return Response(response, <String, dynamic>{
+      'message': 'User already exists',
+      'details': 'A user with the given username already exists',
+      'errorKey': 'UserAlreadyExists'
+    });
+  }
+
+  Future<Response> unexpectedErrorResponse(http.Response response) async {
+    return Response(response, <String, dynamic>{
+      'message': 'something went wrong',
+      'details': 'unexpected error',
+      'errorKey': 'Error'
+    });
+  }
+
+  /// Creates the model for the stream
+  Future<GirafUserModel> createMockUserModel(Map<String, dynamic> body) async {
+    return GirafUserModel.fromJson(body);
+  }
+}
+
+/// Mock api needed to chance the UserApi to MockUserApi
+class MockApi extends Api {
+  MockApi(String baseUrl) : super(baseUrl) {
+    account = MockAccountApi(PersistenceClient());
+    user = MockUserApi();
+  }
+}
+
 class MockNewCitizenBloc extends NewCitizenBloc {
   MockNewCitizenBloc(this.api) : super(api);
 
   bool acceptAllInputs = true;
   Api api;
-
-  @override
-  Observable<GirafUserModel> createCitizen() {
-    return api.account
-        .register('mockUserName', 'password', 'mockDisplayName',
-        departmentId: null, role: null);
-  }
 
   @override
   Observable<bool> get validDisplayNameStream =>
@@ -65,7 +137,7 @@ void main() {
   MockNewCitizenBloc mockNewCitizenBloc;
 
   setUp(() {
-    api = Api('any');
+    api = MockApi('any');
     mockNewCitizenBloc = MockNewCitizenBloc(api);
 
     di.clearAll();
@@ -142,5 +214,56 @@ void main() {
             .widget<GirafButton>(find.byKey(const Key('saveButton')))
             .isEnabled,
         isFalse);
+  });
+
+  testWidgets('"Brugernavnet eksisterer allerede" error message',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: NewCitizenScreen()));
+    await tester.pump();
+
+    await tester.enterText(
+        find.byKey(const Key('displayNameField')), 'mockDisplayName');
+    await tester.enterText(
+        find.byKey(const Key('usernameField')), 'alreadyExists');
+    await tester.enterText(find.byKey(const Key('passwordField')), 'password');
+    await tester.enterText(
+        find.byKey(const Key('passwordVerifyField')), 'password');
+
+    await tester.tap(find.byKey(const Key('saveButton')));
+    await tester.pumpAndSettle();
+    expect(find.byKey(const Key('ErrorMessageDialog')), findsNWidgets(1));
+  });
+  testWidgets('New user so, no error message should appear',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: NewCitizenScreen()));
+    await tester.pump();
+
+    await tester.enterText(
+        find.byKey(const Key('displayNameField')), 'mockDisplayName');
+    await tester.enterText(find.byKey(const Key('usernameField')), 'NewUser');
+    await tester.enterText(find.byKey(const Key('passwordField')), 'password');
+    await tester.enterText(
+        find.byKey(const Key('passwordVerifyField')), 'password');
+
+    await tester.tap(find.byKey(const Key('saveButton')));
+    await tester.pumpAndSettle();
+    expect(find.byKey(const Key('ErrorMessageDialog')), findsNWidgets(0));
+  });
+  testWidgets('Unexpected error from the api_client',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: NewCitizenScreen()));
+    await tester.pump();
+
+    await tester.enterText(
+        find.byKey(const Key('displayNameField')), 'mockDisplayName');
+    await tester.enterText(
+        find.byKey(const Key('usernameField')), 'defaultError');
+    await tester.enterText(find.byKey(const Key('passwordField')), 'password');
+    await tester.enterText(
+        find.byKey(const Key('passwordVerifyField')), 'password');
+
+    await tester.tap(find.byKey(const Key('saveButton')));
+    await tester.pumpAndSettle();
+    expect(find.byKey(const Key('ErrorMessageDialog')), findsNWidgets(1));
   });
 }
