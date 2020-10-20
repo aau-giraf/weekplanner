70d13c3e9f13205941f50d90c5106de7e3c501f1
LJensen0403 <Lasseon.jensen@gmail.com>
2020-10-20 11:21:06 +0200

    Testing stuff

--- lib/widgets/pictogram_text.dart
+++ lib/widgets/pictogram_text.dart
@@ -4,7 +4,9 @@ import 'package:api_client/models/pictogram_model.dart';
 import 'package:api_client/models/settings_model.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:tuple/tuple.dart';
 import 'package:weekplanner/blocs/auth_bloc.dart';
+import 'package:weekplanner/blocs/pictogram_bloc.dart';
 import 'package:weekplanner/blocs/settings_bloc.dart';
 import 'package:weekplanner/di.dart';
 import 'package:weekplanner/models/enums/weekplan_mode.dart';
@@ -15,10 +17,11 @@ class PictogramText extends StatelessWidget {
   /// Constructor
   PictogramText(this._activity, this._user, {this.minFontSize = 100}) {
     _settingsBloc.loadSettings(_user);
+    //_pictogramBloc.load(_activity.pictograms.first);
   }
 
   final ActivityModel _activity;
-
+  //final PictogramBloc _pictogramBloc;
   final DisplayNameModel _user;
 
   /// The settings bloc which we get the settings from, you need to make sure
@@ -50,8 +53,8 @@ class PictogramText extends StatelessWidget {
                     if (_activity.isChoiceBoard) {
                       return _buildPictogramText(context, 'ChoiceBoard');
                     } else {
-                      final String pictogramText =
-                          _pictogram.title.toUpperCase();
+                      final String pictogramText = setPictogramText();
+                      //final String pictogramText = _pictogram.title;
                       return _buildPictogramText(context, pictogramText);
                     }
                   }
@@ -73,7 +76,8 @@ class PictogramText extends StatelessWidget {
           padding: EdgeInsets.symmetric(
               horizontal: MediaQuery.of(context).size.width * 0.05),
           child: AutoSizeText(
-            pictogramText[0] + pictogramText.substring(1).toLowerCase(),
+            pictogramText[0].toUpperCase()
+                + pictogramText.substring(1).toLowerCase(),
             minFontSize: minFontSize,
             maxLines: 2,
             textAlign: TextAlign.center,
@@ -83,5 +87,26 @@ class PictogramText extends StatelessWidget {
                 fontSize: GirafFont.pictogram),
           ),
         ));
+  }
+
+  /// Sets the pictogram text
+  String setPictogramText(){
+    final List<Tuple2<String, String>> testTuple = <Tuple2<String, String>>[
+      const Tuple2<String, String>('a', 'tr'),
+      const Tuple2<String, String>('b', 'GU'),
+      const Tuple2<String, String>('c', 'hjul'),
+      const Tuple2<String, String>('e', 'BITCH'),
+      const Tuple2<String, String>('d', 'iu')
+    ];
+
+    if(testTuple.isNotEmpty){
+      for(int i = 0; testTuple.length > i; i++){
+        if(testTuple[i].item1 == _user.id){
+          return testTuple[i].item2;
+        }
+      }
+    }
+
+    return _activity.pictograms.first.title;
   }
 }
