844ca0808e30af1577dc8832646bfade7b876f54
AlexanderS <31063192+ggAlexS@users.noreply.github.com>
2020-11-25 20:45:13 +0100

    Taking pictures is now working
    Co-Authored-By: JeppeKLau <44167691+JeppeKLau@users.noreply.github.com>

---
+++ lib/blocs/take_image_with_camera_bloc.dart
@@ -0,0 +1,118 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:api_client/api/api.dart';
+import 'package:api_client/models/enums/access_level_enum.dart';
+import 'package:api_client/models/pictogram_model.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:rxdart/rxdart.dart' as rx_dart;
+import 'package:weekplanner/blocs/bloc_base.dart';
+import 'package:image/image.dart';
+
+/// Bloc for retriving an image from a phones gallery,
+/// and send it to the pictogram database
+class TakePictureWithCameraBloc extends BlocBase {
+  ///
+  TakePictureWithCameraBloc(this._api);
+  final Api _api;
+  String _pictogramName;
+
+  /// Publishes the image file, while it is nut null
+  Stream<File> get file => _file.stream.where((File f) => f != null);
+
+  /// Publishes true while waiting for the pictogram to be uploaded
+  Stream<bool> get isUploading => _isUploading.stream;
+
+  /// Publishes the accessLevel for the pictogram
+  Stream<String> get accessLevel => _accessString.stream;
+
+  /// Publishes if the input fields are filled
+  Stream<bool> get isInputValid => _isInputValid.stream;
+
+  final rx_dart.BehaviorSubject<bool> _isInputValid =
+  rx_dart.BehaviorSubject<bool>.seeded(false);
+  final rx_dart.BehaviorSubject<File> _file = rx_dart.BehaviorSubject<File>();
+  final rx_dart.BehaviorSubject<String> _accessString =
+  rx_dart.BehaviorSubject<String>.seeded('Offentlig');
+  final rx_dart.BehaviorSubject<bool> _isUploading =
+  rx_dart.BehaviorSubject<bool>.seeded(false);
+
+  AccessLevel _accessLevel = AccessLevel.PUBLIC;
+
+  /// pushes an imagePicker screen, then sets the pictogram image,
+  /// to the selected image from the gallery
+  void takePictureWithCamera() {
+    ImagePicker.pickImage(source: ImageSource.camera).then((File f) {
+      if (f != null) {
+        _publishImage(f);
+        _checkInput();
+      }
+    });
+  }
+
+  /// Checks if the input fields are filled out
+  void _checkInput() {
+    if (_file.value != null && _pictogramName != null &&
+        _pictogramName.isNotEmpty) {
+      _isInputValid.add(true);
+    } else {
+      _isInputValid.add(false);
+    }
+  }
+
+  /// set accessLevel for the pictogram
+  void setAccessLevel(String access) {
+    _accessString.add(access);
+    switch (access) {
+      case 'Beskyttet':
+        _accessLevel = AccessLevel.PROTECTED;
+        break;
+      case 'Privat':
+        _accessLevel = AccessLevel.PRIVATE;
+        break;
+      default:
+        _accessLevel = AccessLevel.PUBLIC;
+    }
+  }
+
+  /// sets the pictogram name
+  void setPictogramName(String newName) {
+    _pictogramName = newName;
+    _checkInput();
+  }
+
+  void _publishImage(File file) {
+    _file.add(file);
+  }
+
+  Uint8List _encodePng(File file) {
+    return encodePng(copyResize(decodeImage(file.readAsBytesSync()),
+        width: 512)); // 512 bytes chosen as a reasonable input size.
+  }
+
+  /// Creates a [PictogramModel]
+  /// from the seleted [Image], [AccessLevel], and title
+  Stream<PictogramModel> createPictogram() {
+    _isUploading.add(true);
+    return _api.pictogram
+        .create(PictogramModel(
+      accessLevel: _accessLevel,
+      title: _pictogramName,
+    ))
+        .flatMap((PictogramModel pictogram) {
+      return _api.pictogram.updateImage(pictogram.id, _encodePng(_file.value));
+    }).map((PictogramModel pictogram) {
+      _isUploading.add(false);
+      return pictogram;
+    }).doOnError((Object error, StackTrace trace) {
+      _isUploading.add(false);
+    }).take(1);
+  }
+
+  @override
+  void dispose() {
+    _file.close();
+    _accessString.close();
+    _isInputValid.close();
+    _isUploading.close();
+  }
+}
--- lib/screens/pictogram_search_screen.dart
+++ lib/screens/pictogram_search_screen.dart
@@ -4,6 +4,7 @@ import 'package:weekplanner/blocs/pictogram_bloc.dart';
 import 'package:weekplanner/di.dart';
 import 'package:api_client/models/pictogram_model.dart';
 import 'package:weekplanner/routes.dart';
+import 'package:weekplanner/screens/take_picture_with_camera_screen.dart';
 import 'package:weekplanner/screens/upload_image_from_phone_screen.dart';
 import 'package:weekplanner/widgets/giraf_app_bar_widget.dart';
 import 'package:weekplanner/widgets/giraf_button_widget.dart';
@@ -95,6 +96,13 @@ class PictogramSearch extends StatelessWidget {
                 onPressed: () async {
                   await Routes.push(context, UploadImageFromPhone());
                 },
+              ),
+              GirafButton(
+                  icon: const ImageIcon(AssetImage('assets/icons/camera.png')),
+                  text: 'Tag billede med kamera',
+                  onPressed: () async {
+                    await Routes.push(context, TakePictureWithCamera());
+                  }
               ),
             ],
           ),
