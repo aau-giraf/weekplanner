84aa2d1ea172cf6f137dae976d4e23324085a794
Jakob Precht <sporvogn@live.com>
2020-12-08 09:06:59 +0100

    Release/2020es3r1 Merge release into develop (#709)
    * feature/696: Inconsistent buttons (#701)
    
    * Inconsistent buttons
    
    Only partly solved the issue.
    The buttons now correctly remove the timer and add choiceboard options when marking an activity as completed as well as cancelled. Furthermore an isEnabled has been added to the cancel button, so it becomes disables when the state is completed.
    The buttons however only become disabled, when you reenter the activity. This is due to the fact the other button does not check for a new state, when the first button is pressed and as such does not get the new state information, when an acitivty is completed or cancelled. There is not enough time to fix this before release, and as such only the most system breaking part of the bug has been fixed, as it would do some funky things, when trying to make a choiceboard on a completed activity.
    
    * Update show_activity_screen.dart
    
    Fixed missing Add Choiceboard on new activities
    
    * Changed values for release (#706)
    
    Co-authored-by: Rikke Husted Østergaard <raster16@student.aau.dk>

--- lib/screens/show_activity_screen.dart
+++ lib/screens/show_activity_screen.dart
@@ -121,7 +121,8 @@ class ShowActivityScreen extends StatelessWidget {
           builder: (BuildContext context,
               AsyncSnapshot<ActivityModel> activitySnapshot) {
             return (activitySnapshot.hasData &&
-                    activitySnapshot.data.state == ActivityState.Canceled)
+                   (activitySnapshot.data.state == ActivityState.Canceled ||
+                    activitySnapshot.data.state == ActivityState.Completed))
                 ? _resetTimerAndBuildEmptyContainer()
                 : _buildTimer(context);
           }),
@@ -138,7 +139,8 @@ class ShowActivityScreen extends StatelessWidget {
                 if (authSnapshot.hasData &&
                     activitySnapshot.hasData &&
                     authSnapshot.data != WeekplanMode.citizen &&
-                    activitySnapshot.data.state != ActivityState.Canceled) {
+                    (activitySnapshot.data.state != ActivityState.Canceled &&
+                    activitySnapshot.data.state != ActivityState.Completed)) {
                   return _buildChoiceBoardButton(context);
                 } else {
                   return _buildEmptyContainer();
@@ -663,6 +665,8 @@ class ShowActivityScreen extends StatelessWidget {
                             _activityBloc.cancelActivity();
                             _activity.state = _activityBloc.getActivity().state;
                           },
+                          isEnabled: activitySnapshot.data.state !=
+                              ActivityState.Completed,
                           text: activitySnapshot.data.state !=
                               ActivityState.Canceled
                               ? 'Aflys'
