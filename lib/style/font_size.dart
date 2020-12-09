e876d22661fc16eaf268b1443753b85fe203ba46
Jeppe Krogh Laursen <jeppe.k.l.97@gmail.com>
2020-12-09 16:49:44 +0100

    Linter fix

--- lib/style/font_size.dart
+++ lib/style/font_size.dart
@@ -1,15 +1,24 @@
 /// Font sizes for the GIRAF project.
 
-class GirafFont
-{
+class GirafFont {
   GirafFont._();
 
+  /// Font size for pictograms
   static const double pictogram = 150;
+
+  /// Font size for timers
+
   static const double timer = 50;
+
+  /// Font size for large text
   static const double large = 30;
+
+  /// Font size for small text
   static const double small = 20;
+
+  /// Font size for medium text
   static const double medium = 24;
+
+  /// Font size for activity screen buttons
   static const double activity_screen_buttons = 23;
-
-
 }
