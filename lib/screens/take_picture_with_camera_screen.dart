120a60e38d5cda46915be0f44374bf2417a565d9
Micklas <9057749+Micklas422@users.noreply.github.com>
2021-10-19 09:47:44 +0200

    Revert "Feature/666 (#732)" (#791)
    This reverts commit 251b947f4cc4414a99b666b22259eaeb028f4c15.

--- lib/screens/take_picture_with_camera_screen.dart
+++ lib/screens/take_picture_with_camera_screen.dart
@@ -86,7 +86,7 @@ class TakePictureWithCamera extends StatelessWidget {
                       onChanged: (String newValue) {
                         _takePictureWithCamera.setAccessLevel(newValue);
                       },
-                      items: <String>['Institution', 'Privat']
+                      items: <String>['Offentlig', 'Privat']
                           .map<DropdownMenuItem<String>>((String value) {
                         return DropdownMenuItem<String>(
                           value: value,
