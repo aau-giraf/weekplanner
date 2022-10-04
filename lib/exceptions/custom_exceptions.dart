187ca40767968bb7673324ce1733abb0c30d1c0a
henr9533 <heno-2000@hotmail.com>
2022-10-04 11:32:42 +0200

    Documentation
    Added some documentation for the custom exception "serverException"

--- lib/exceptions/custom_exceptions.dart
+++ lib/exceptions/custom_exceptions.dart
@@ -1,6 +1,10 @@
+/// The file containing all custom exceptions, which
+/// are relevant for the weekplaner (Not ApiClient)
 class serverException implements Exception
 {
+  /// The cause of the error
   String errorCause;
 
+  /// Constructor for the exception
   serverException(this.errorCause);
 }
