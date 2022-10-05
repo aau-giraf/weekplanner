d6cae60102f1684fb92cf81a6dc0611eff6167a3
KasperBahrentz <71833276+KasperBahrentz@users.noreply.github.com>
2022-10-05 10:54:09 +0200

    Added container, so that the width of the column can be set
    This is only when showing only one day

--- lib/screens/weekplan_screen.dart
+++ lib/screens/weekplan_screen.dart
@@ -403,7 +403,10 @@ class WeekplanScreen extends StatelessWidget {
                       key: const Key('SingleWeekdayRow'),
                       children: <Widget>[
                         const Spacer(flex: 1),
-                        weekDays.first,
+                        // The Container is used to set the width of the column
+                        // to half the width of the screen
+                        Container(width: MediaQuery.of(context).size.width / 2,
+                            child: weekDays.first),
                         const Spacer(flex: 1),
                       ],
                     );
