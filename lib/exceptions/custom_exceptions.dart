2bd7819575c032e065544c45adc1393d0fd2eaf2
henr9533 <heno-2000@hotmail.com>
2022-10-25 10:35:42 +0200

    Update custom_exceptions.dart
    Added details to the custom exceptions

--- lib/exceptions/custom_exceptions.dart
+++ lib/exceptions/custom_exceptions.dart
@@ -1,19 +1,23 @@
 /// The file containing all custom exceptions, which
 /// are relevant for the weekplanner (Not ApiClient)
-class ServerException implements Exception
-{
+class ServerException implements Exception {
   /// Constructor for the exception
-  ServerException(this.errorCause);
+  ServerException(this.errorCause, this.details);
 
   /// The cause of the error
   String errorCause;
+
+  /// Details regarding the error
+  String details;
 }
 
-class SaveButtonException implements Exception
-{
+class SaveButtonException implements Exception {
   /// Constructor for the exception
-  SaveButtonException(this.errorCause);
+  SaveButtonException(this.errorCause, this.details);
 
   /// The cause of the error
   String errorCause;
+
+  /// Details regarding the error
+  String details;
 }
