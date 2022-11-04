905d5414f381df03367527a6350b6610e763877e
Martin Algreen <martin@algreen.dk>
2022-11-04 12:12:13 +0100

    For real this time

---
+++ lib/screens/new_pictogram_password_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:weekplanner/widgets/giraf_app_bar_widget.dart';
+
+class NewPictogramPasswordScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: GirafAppBar(title: 'Ny bruger'),
+        body: ListView(children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+            child: StreamBuilder<bool>(builder: (context, snapshot) {
+              return const Text('Opret piktogram kode til bruger',
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontWeight: FontWeight.bold));
+            }),
+          ),
+        ]));
+  }
+}
