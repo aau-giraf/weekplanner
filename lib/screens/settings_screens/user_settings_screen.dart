98afeba7528da42fef7cf103fbdec18b3f9086d0
JJTM20 <jjtm20@student.aau.dk>
2022-11-08 11:39:38 +0100

    Works for now. Needs refactoring in ChangePasswordScreen + elimination of dead code and comments

--- lib/screens/settings_screens/user_settings_screen.dart
+++ lib/screens/settings_screens/user_settings_screen.dart
@@ -7,6 +7,8 @@ import 'package:flutter/material.dart';
 import 'package:weekplanner/blocs/choose_citizen_bloc.dart';
 import 'package:weekplanner/blocs/settings_bloc.dart';
 import 'package:weekplanner/routes.dart';
+import 'package:weekplanner/screens/settings_screens/change_password_screen.dart';
+import 'package:weekplanner/screens/settings_screens/change_username_screen.dart';
 import 'package:weekplanner/screens/settings_screens/'
     'number_of_days_selection_screen.dart';
 import 'package:weekplanner/screens/settings_screens/'
@@ -31,18 +33,10 @@ import '../../di.dart';
 import '../../widgets/settings_widgets/settings_section_arrow_button.dart';
 import 'completed_activity_icon_selection_screen.dart';
 
-class UserSettingsScreen extends StatefulWidget {
-  @override
-  _UserSettingsScreenState createState() => _UserSettingsScreenState();
-}
-
-class _UserSettingsScreenState extends State<UserSettingsScreen> {
+class UserSettingsScreen extends StatelessWidget {
   final ChooseCitizenBloc _cBloc = di.getDependency<ChooseCitizenBloc>();
-  final SettingsBloc _settingsBloc = di.getDependency<SettingsBloc>();
-  GirafUserModel _user;
-  String appBarTitleText = "Placeholder";
 
-  /*Future<GirafUserModel> FetchUser() async {
+/*Future<GirafUserModel> FetchUser() async {
     return await _cBloc.GetCurrentUser();
     setState(() {
       appBarTitleText = "New placeholder";
@@ -50,61 +44,46 @@ class _UserSettingsScreenState extends State<UserSettingsScreen> {
     //return user;
   }*/
 
-  void InitState() {
-    super.initState();
-    //_user = FetchUser();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: GirafAppBar(title: appBarTitleText + ' Indstillinger'),
+        appBar: GirafAppBar(title: 'Indstillinger'),
         body: _buildAllSettings(context));
   }
 
   Widget _buildAllSettings(BuildContext context) {
     return ListView(
-      children: <Widget>[_buildUserSettings()],
+      children: <Widget>[
+        _buildUserSettings(),
+      ],
     );
   }
 
   Widget _buildUserSettings() {
-    return Scaffold(
-        appBar: GirafAppBar(
-          title: appBarTitleText + ' indstillinger',
-        ),
-        body: StreamBuilder<GirafUserModel>(
-            stream: _cBloc.guardian,
-            builder:
-                (BuildContext context, AsyncSnapshot<GirafUserModel> snapshot) {
-              if (snapshot.connectionState == ConnectionState.active) {
-                print("Active state: ${snapshot.connectionState}");
-              }
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return CircularProgressIndicator();
-              } else if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.hasError) {
-                  print(snapshot.error);
-                  return Center(child: Text("Error occured"));
-                } else if (snapshot.hasData) {
-                  print("Snapshot has Girafuser: " + snapshot.data.displayName);
-                  _user = snapshot.data;
-
-                  setState(() {
-                    appBarTitleText = _user.displayName;
-                    _settingsBloc.loadSettingsGirafUser(_user);
-                  });
-                } else {
-                  print("Empty snapshot");
-                  return Center(child: Text("Empty snapshot / No data"));
-                }
-              } else {
-                print(snapshot.connectionState);
-                return Center(
-                  child: Text('State: ${snapshot.connectionState}'),
-                );
-              }
-            }));
+    return StreamBuilder<GirafUserModel>(
+        stream: _cBloc.guardian,
+        builder:
+            (BuildContext context, AsyncSnapshot<GirafUserModel> snapshot) {
+          if (snapshot.hasData) {
+            print("Snapshot has Girafuser: " + snapshot.data.displayName);
+            DisplayNameModel user =
+                DisplayNameModel.fromGirafUser(snapshot.data);
+            return SettingsSection(
+                snapshot.data.displayName + ' - skift personlig information',
+                <SettingsSectionItem>[
+                  SettingsArrowButton('Skift brugernavn', () {
+                    Routes.push(context, ChangeUsernameScreen(user));
+                  }),
+                  SettingsArrowButton('Skift kodeord', () {
+                    Routes.push(context, ChangePasswordScreen(user));
+                  }),
+                ]);
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        });
 
     /*return StreamBuilder<SettingsModel>(
         stream: _settingsBloc.settings,
@@ -127,7 +106,7 @@ class _UserSettingsScreenState extends State<UserSettingsScreen> {
               */
   }
 
-  Widget _buildChangePasswordAndUsername(BuildContext context) {
+  Widget _buildChangePasswordAndUsername() {
     return SettingsSection(
         "_user.displayName" + ' - skift personlig information',
         <SettingsSectionItem>[
