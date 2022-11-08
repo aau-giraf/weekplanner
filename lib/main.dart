9e85b1b251d70124fb629b995ff4f635d7744ecc
Andreas Knudsen Alstrup <aalstr20@student.aau.dk>
2022-11-08 09:50:56 +0100

    Display WeekPlanSelectorScreen when logged in as citizen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:api_client/api/api.dart';
+import 'package:api_client/models/displayname_model.dart';
+import 'package:api_client/models/enums/role_enum.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:weekplanner/blocs/auth_bloc.dart';
 import 'package:weekplanner/bootstrap.dart';
@@ -7,9 +10,11 @@ import 'package:weekplanner/screens/login_screen.dart';
 import 'package:weekplanner/providers/environment_provider.dart' as environment;
 import 'package:weekplanner/routes.dart';
 import 'package:weekplanner/screens/choose_citizen_screen.dart';
+import 'package:weekplanner/screens/weekplan_selector_screen.dart';
 import 'package:weekplanner/widgets/giraf_notify_dialog.dart';
 
 final Api _api = di.getDependency<Api>();
+final AuthBloc _authBloc = di.getDependency<AuthBloc>();
 
 
 void main() {
@@ -60,8 +65,17 @@ void _runApp() {
             }
             firstTimeLogIn = false;
             if (snapshot.data) {
-              // In case logged in show ChooseCitizenScreen
-              return ChooseCitizenScreen();
+              // Show screen dependent on logged in role
+              switch (_authBloc.loggedInUser.role) {
+                case Role.Citizen:
+                  return WeekplanSelectorScreen(
+                      DisplayNameModel(
+                          displayName: _authBloc.loggedInUser.displayName,
+                          role: describeEnum(_authBloc.loggedInUser.role),
+                          id: _authBloc.loggedInUser.id));
+                default:
+                  return ChooseCitizenScreen();
+              }
             } else {
               // Not loggedIn pop context to login screen.
               Routes.goHome(context);
