77e0500821aba83ac4c15fa944ecc70339df9a32
Emil Brandt <73188508+emilnormann@users.noreply.github.com>
2022-11-16 11:28:10 +0100

    Inline ;)

--- lib/widgets/pictogram_widgets/pictogram_password_choices.dart
+++ lib/widgets/pictogram_widgets/pictogram_password_choices.dart
@@ -18,18 +18,20 @@ class PictogramChoices extends StatefulWidget {
 
 /// The pictograms to choose between for the code.
 /// If these are changed every previously made passwords will become unusable
-const List<int> CHOSENPICTOGRAMS = <int>[1,2,3,4,5,6,7,8,9,10];
+const List<int> CHOSENPICTOGRAMS = <int>[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 class _PictogramChoiceState extends State<PictogramChoices> {
   /// Api to use for pictogram calls
   Api _api;
+
   /// List of currently chosen pictograms for the code
   List<PictogramModel> _inputCode;
+
   /// Stream of usable pictograms
   Stream<List<PictogramModel>> _pictogramChoices;
 
   @override
-  void initState(){
+  void initState() {
     _inputCode = List<PictogramModel>.filled(4, null);
     _api = di.getDependency<Api>();
     _pictogramChoices = getStream();
@@ -38,34 +40,30 @@ class _PictogramChoiceState extends State<PictogramChoices> {
 
   ///Called when one of the possible pictograms are pressed
   ///and adds it to the code
-  void addToPass(PictogramModel pictogram){
+  void addToPass(PictogramModel pictogram) {
     final int index = _inputCode.indexOf(null);
-    if (index != -1){
+    if (index != -1) {
       _inputCode[index] = pictogram;
     }
     //Reloads the widget with the new input
-    setState(() {
-
-    });
+    setState(() {});
   }
 
   ///Called when a pictogram is pressed in the code and removes the pressed icon
-  void removeFromPass(int index){
+  void removeFromPass(int index) {
     _inputCode[index] = null;
     //Reloads the widget with the new code
-    setState(() {
-
-    });
+    setState(() {});
   }
 
   ///
   Stream<List<PictogramModel>> getStream() async* {
-    final List<PictogramModel> list = List<PictogramModel>.filled(
-        CHOSENPICTOGRAMS.length, null);
+    final List<PictogramModel> list =
+        List<PictogramModel>.filled(CHOSENPICTOGRAMS.length, null);
     yield list;
-    for (int i = 0; i < list.length; i++){
+    for (int i = 0; i < list.length; i++) {
       final int index = CHOSENPICTOGRAMS[i];
-      await for (final PictogramModel model in _api.pictogram.get(index)){
+      await for (final PictogramModel model in _api.pictogram.get(index)) {
         list[i] = model;
         yield list;
       }
@@ -74,20 +72,18 @@ class _PictogramChoiceState extends State<PictogramChoices> {
 
   List<Widget> passwordList() {
     final List<Widget> password = List<Widget>.filled(4, null);
-    for (int i = 0; i < 4; i++){
+    for (int i = 0; i < 4; i++) {
       final PictogramModel pictogram = _inputCode[i];
       Widget widget;
-      if (pictogram == null){
+      if (pictogram == null) {
         widget = Container(
           width: 100,
           height: 100,
           color: Colors.red,
         );
-      } else{
+      } else {
         widget = PictogramImage(
-            pictogram: pictogram,
-            onPressed: () => removeFromPass(i)
-        );
+            pictogram: pictogram, onPressed: () => removeFromPass(i));
       }
       password[i] = widget;
     }
@@ -95,68 +91,46 @@ class _PictogramChoiceState extends State<PictogramChoices> {
   }
 
   @override
-  Widget build (BuildContext context) {
-    return Column(
-        children: <Widget>[
-          //First column element
-          Container(
-              width: 500,
-              height: 500,
-              child: StreamBuilder<List<PictogramModel>>(
-                  stream: _pictogramChoices,
-                  builder: (BuildContext context,
-                      AsyncSnapshot<List<PictogramModel>> snapshot) {
-                    if (snapshot.hasError){
-                      print(snapshot.error);
-                      return const Text(
-                          'Fejl i forbindelse med piktogrammer.');
-                    }
-                    else if (snapshot.hasData){
-                      return Expanded(
-                          child:
-                          GridView.count(
-                              crossAxisCount: 5,
-                              children:
-                              snapshot.data.map<Widget>(
-                                      (PictogramModel pictogram) {
-                                if (pictogram == null) {
-                                  return Container(
-                                    height: 80,
-                                    child: const Center(
-                                        child: CircularProgressIndicator()
-                                    ),
-                                  );
-                                } else{
-                                  return PictogramImage(
-                                      pictogram: pictogram,
-                                      onPressed: () => addToPass(pictogram)
-                                  );
-                                }
-                              }).toList()
-                          )
-                      );
-
-                    }
-                    else{
-                      return const Text('Fejl');
-                    }
-
-                  })
-          ),
-          //Second column element
-
-
-          Container(
-            width: 500,
-            height: 500,
-            child: GridView.count(
-                crossAxisCount: 4,
-                children: passwordList()
-            )
-          ),
-        ]);
-
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      //First column element
+      Container(
+          width: 500,
+          height: 500,
+          child: StreamBuilder<List<PictogramModel>>(
+              stream: _pictogramChoices,
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<PictogramModel>> snapshot) {
+                if (snapshot.hasError) {
+                  print(snapshot.error);
+                  return const Text('Fejl i forbindelse med piktogrammer.');
+                } else if (snapshot.hasData) {
+                  return Expanded(
+                      child: GridView.count(
+                          crossAxisCount: 5,
+                          children: snapshot.data
+                              .map<Widget>((PictogramModel pictogram) {
+                            if (pictogram == null) {
+                              return Container(
+                                height: 80,
+                                child: const Center(
+                                    child: CircularProgressIndicator()),
+                              );
+                            } else {
+                              return PictogramImage(
+                                  pictogram: pictogram,
+                                  onPressed: () => addToPass(pictogram));
+                            }
+                          }).toList()));
+                } else {
+                  return const Text('Fejl');
+                }
+              })),
+      //Second column element
+      Container(
+          width: 500,
+          height: 500,
+          child: GridView.count(crossAxisCount: 4, children: passwordList())),
+    ]);
   }
-
-
 }
