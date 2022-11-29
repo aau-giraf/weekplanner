d9f250cb3430850e9015714f9b8d037969aaf6d7
Makel <mikkel@dissing.one>
2022-11-29 14:49:29 +0100

    added title to activity

--- lib/widgets/weekplan_screen_widgets/weekplan_day_column.dart
+++ lib/widgets/weekplan_screen_widgets/weekplan_day_column.dart
@@ -413,7 +413,6 @@ class WeekplanDayColumn extends StatelessWidget {
                         double _width = 1;
                         final int _daysToDisplay =
                             settingsSnapshot.data.nrOfDaysToDisplay;
-
                         if (MediaQuery.of(context).orientation ==
                             Orientation.portrait) {
                           if (modeSnapshot.data == WeekplanMode.citizen) {
@@ -577,7 +576,8 @@ class WeekplanDayColumn extends StatelessWidget {
                                       ],
                                       order: weekday.activities.length,
                                       state: ActivityState.Normal,
-                                      isChoiceBoard: false),
+                                      isChoiceBoard: false,
+                                      title: object.title),
                                   weekday.day.index);
                             }
                           });
