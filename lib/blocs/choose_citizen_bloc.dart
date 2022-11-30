4cca0c964d19f3058912eb5832b3980b95c239e2
Karl Barnholdt Kragsaa <62882434+karlbarnholdt@users.noreply.github.com>
2022-11-30 14:10:35 +0100

    Feature/826 (#920)
    * Added error handling for unproblematic error from the web-api. Guardians should not see a list of citizens if they don't have any assigned. The web-api, however, raises an exception if this is the case. This is an unproblematic error in the scenario of a guardian choosing citizen after login, if they aren't assigned to any citizens at that moment. If no citizens are assigend, the screen should show no citizens and no error should be shown.
    
    * Test fixes
    
    Co-authored-by: karl <kkrags20@student.aau.dk>
    Co-authored-by: henr9533 <heno-2000@hotmail.com>

--- lib/blocs/choose_citizen_bloc.dart
+++ lib/blocs/choose_citizen_bloc.dart
@@ -21,13 +21,22 @@ class ChooseCitizenBloc extends BlocBase {
       return _api.user.getCitizens(user.id);
     }).listen((List<DisplayNameModel> citizens) {
       _citizens.add(citizens);
+    }).onError((Object error) {
+      if (error.toString() == '[ApiException]: UserHasNoCitizens') {
+        // Do not return any citizens if the web-api throws UserHasNoCitzens.
+        // See issue #826 on GitHub for more info.
+        return null;
+      } else {
+        // Return the error if it is not a UserHasNoCitzens error.
+        return Future<void>.error(error);
+      }
     });
   }
 
   final Api _api;
   final rx_dart.BehaviorSubject<List<DisplayNameModel>> _citizens =
-  rx_dart.BehaviorSubject<List<DisplayNameModel>>.seeded(
-      <DisplayNameModel>[]);
+      rx_dart.BehaviorSubject<List<DisplayNameModel>>.seeded(
+          <DisplayNameModel>[]);
 
   @override
   void dispose() {
