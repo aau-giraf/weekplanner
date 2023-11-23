e28d080818a8bfe90997cc5119008d7511bcb789
SadAndBroken <lasse.stocklund@hotmail.com>
2023-11-23 13:16:57 +0100

    Revisited giraf_button_widget_test.dart. Comment added for more clarity. Two test needs to be revisited

--- test/widgets/giraf_button_widget_test.dart
+++ test/widgets/giraf_button_widget_test.dart
@@ -36,33 +36,42 @@ class MockScreen extends StatelessWidget {
 
 void main() {
   testWidgets('Test if GirafButton is shown', (WidgetTester tester) async {
+    // Opens the MockScreen widget
     await tester.pumpWidget(MaterialApp(home: MockScreen()));
+    // Checks that the GirafButton exists on the MockScreen
     expect(find.byType(GirafButton), findsOneWidget);
   });
 
   testWidgets('GirafButton has a title', (WidgetTester tester) async {
+    // Opens the MockScreen widget
     await tester.pumpWidget(MaterialApp(home: MockScreen()));
-
+    // Checks that the text PressButton exists on the MockScreen
     expect(find.text('PressButton'), findsOneWidget);
   });
 
   testWidgets('GirafButton has an icon', (WidgetTester tester) async {
+    // Opens the MockScreen widget
     await tester.pumpWidget(MaterialApp(home: MockScreen()));
-
+    // Checks that the acceptIcon widget exists on the MockScreen
     expect(find.byWidget(acceptIcon), findsOneWidget);
   });
 
   testWidgets(
       'GirafButton is pressed and'
       ' works when enabled', (WidgetTester tester) async {
+        // TODO; Skal revuderes, mange af disse ting er nok unødvendige
     final Completer<bool> done = Completer<bool>();
     final MockScreen screen = MockScreen();
+    // Opens the MockScreen widget and enables the GirafButton
     await tester.pumpWidget(MaterialApp(home: screen));
     screen.btnEnabled.add(true);
+    // Runs all stacked frames and finds widget with Button key
     await tester.pumpAndSettle();
     await tester.tap(find.byKey(const Key('Button')));
+    //Runs next frame and creates listener for isPressed
     await tester.pump();
     screen.isPressed.listen((bool status) {
+      // Checks that the status of the button is true
       expect(status, isTrue);
       done.complete();
     });
@@ -72,13 +81,18 @@ void main() {
   testWidgets(
       'GirafButton is pressed and'
       ' does not work when disabled', (WidgetTester tester) async {
+    // TODO; Skal revuderes, mange af disse ting er nok unødvendige
     final Completer<bool> done = Completer<bool>();
     final MockScreen screen = MockScreen();
+    // Opens the MockScreen widget and enables the GirafButton
     await tester.pumpWidget(MaterialApp(home: screen));
     await tester.pump();
+    // Finds the widget with key Button and runs next frame
     await tester.tap(find.byKey(const Key('Button')));
+    //Runs next frame and creates listener for isPressed
     await tester.pump();
     screen.isPressed.listen((bool status) {
+      // Checks that the status of the button is false
       expect(status, isFalse);
       done.complete();
     });
