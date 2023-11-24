9b211ae34d1c7b147eec44a49bf0fb04b9c49cc2
BicaniWolfie <mathiasj20@live.dk>
2023-11-24 14:48:20 +0100

    Revisited Tests
    Updated tests:
    - copy_resolve_bloc_test.dart
    - pictogram_bloc_test.dart
    - environment_test.dart
    - giraf_app_bar_widget_test.dart

--- test/providers/environment_test.dart
+++ test/providers/environment_test.dart
@@ -18,29 +18,47 @@ void main() {
     ''';
 
   test('Should get SERVER_HOST from environment file (DEBUG)', () {
+    // Parses the variables from the 'debugEnvironments' string.
     environment.setContent(debugEnvironments);
+
+    // Compare the loaded environment variable "SERVER_HOST" from
+    // the DEBUG environment to an expected value.
+    // If "SERVER_HOST" wasn't loaded or did not have the expected value,
+    // the test fails. Otherwise the test succeeds.
     expect(environment.getVar<String>('SERVER_HOST'),
         equals('http://web.giraf.cs.aau.dk:5000'));
   });
 
   test('Should get SERVER_HOST from environment file (PRODUCTION)', () {
+    // Parses the variables from the 'prodEnvironments' string.
     environment.setContent(prodEnvironments);
+
+    // Compare the loaded environment variable "SERVER_HOST" from
+    // the PRODUCTION environment to an expected value.
+    // This test does the same as the test above, just ausing the PRODUCTION
+    // environment instead of the DEBUG environment.
     expect(environment.getVar<String>('SERVER_HOST'),
         equals('http://web.giraf.cs.aau.dk:5000'));
   });
 
   test('Should get DEBUG from environment file (PRODUCTION)', () {
+    // Parses the variables from the 'prodEnvironments' string.
     environment.setContent(prodEnvironments);
+
     expect(environment.getVar<bool>('DEBUG'), equals(false));
   });
 
-  test('Should get USERNAME from environment file (PRODUCTION)', () {
+  test('Should get USERNAME from environment file (DEBUG)', () {
+    // Parses the variables from the 'debugEnvironments' string.
     environment.setContent(debugEnvironments);
+
     expect(environment.getVar<String>('USERNAME'), equals('Graatand'));
   });
 
-  test('Should get PASSWORD from environment file (PRODUCTION)', () {
+  test('Should get PASSWORD from environment file (DEBUG)', () {
+    // Parses the variables from the 'debugEnvironments' string.
     environment.setContent(debugEnvironments);
+
     expect(environment.getVar<String>('PASSWORD'), equals('password'));
   });
 }
