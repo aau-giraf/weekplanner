3f7ce19c1712ebaf5f8c22b8832a2ff7f6b7baed
omegaMindCoder619 <emildnice@gmail.com>
2023-11-29 10:51:38 +0100

    test files reviewed

--- test/widgets/citizen_avatar_widget_test.dart
+++ test/widgets/citizen_avatar_widget_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:weekplanner/widgets/citizen_avatar_widget.dart';
 
+//Creates a mock for the test
 class MockScreen extends StatelessWidget {
   MockScreen({@required this.callback});
 
@@ -24,44 +25,57 @@ class MockScreen extends StatelessWidget {
 }
 
 void main() {
+  //Setting up the environment
   int i = 0;
   const Key widgetAvatar = Key('WidgetAvatar');
   const Key widgetText = Key('WidgetText');
   final MockScreen mockScreen = MockScreen(callback: () => ++i);
 
   testWidgets('Test if citizen text appears', (WidgetTester tester) async {
+    //Pumps the mockScreen until there are no more frames scheduled
     await tester.pumpWidget(MaterialApp(home: mockScreen));
     await tester.pumpAndSettle();
+    //Checks if it can find the text (widgetText)
     expect(find.byKey(widgetText), findsOneWidget);
   });
 
   testWidgets('Test if citizen avatar appears', (WidgetTester tester) async {
+    //Pumps the mockScreen until there are no more frames scheduled
     await tester.pumpWidget(MaterialApp(home: mockScreen));
     await tester.pumpAndSettle();
+    //Checks if it can find the citizen avatar (widgetAvatar)
     expect(find.byKey(widgetAvatar), findsOneWidget);
   });
 
   testWidgets('Test if text is the username', (WidgetTester tester) async {
+    //Pumps the mockScreen until there are no more frames scheduled
     await tester.pumpWidget(MaterialApp(home: mockScreen));
     await tester.pumpAndSettle();
+    //Checks if "Testname" is the username
     expect(find.text('Testname'), findsOneWidget);
   });
 
   testWidgets('Test if callback is working on avatar',
       (WidgetTester tester) async {
     i = 0;
+    //Pumps the mockScreen until there are no more frames scheduled
     await tester.pumpWidget(MaterialApp(home: mockScreen));
     await tester.pumpAndSettle();
+    //Uses tester.tap to check if callback works when clicking on an avatar
     await tester.tap(find.byKey(widgetAvatar));
+    //If it works "i" would have been iterated to 1 and the assert will be true
     expect(i, 1);
   });
 
   testWidgets('Test if callback is working on text',
       (WidgetTester tester) async {
     i = 0;
+    //Pumps the mockScreen until there are no more frames scheduled
     await tester.pumpWidget(MaterialApp(home: mockScreen));
     await tester.pumpAndSettle();
+    //Uses tester.tap to check if callback works when clicking on an avatar
     await tester.tap(find.byKey(widgetText));
+    //If it works "i" would have been iterated to 1 and the assert will be true
     expect(i, 1);
   });
 }
--- test/widgets/giraf_title_header_test.dart
+++ test/widgets/giraf_title_header_test.dart
@@ -5,21 +5,25 @@ import 'package:weekplanner/widgets/giraf_title_header.dart';
 void main() {
   testWidgets('Test that a null title is replaced with empty',
       (WidgetTester tester) async {
+    //Pumps a widget containing a GirafTitleHeader with the title "TitleHeader"
     await tester.pumpWidget(const MaterialApp(
         home: Scaffold(
       body: GirafTitleHeader(title: 'TitleHeader'),
     )));
     await tester.pump();
+    //Checks if the text is "TitleHeader"
     expect(find.text('TitleHeader'), findsOneWidget);
   });
 
   testWidgets('Test that a null title is replaced with empty',
       (WidgetTester tester) async {
+    //Pumps a widget containing a GirafTitleHeader with no title assigned"
     await tester.pumpWidget(const MaterialApp(
         home: Scaffold(
       body: GirafTitleHeader(),
     )));
     await tester.pump();
+    //Checks if the text is empty
     expect(find.text(''), findsOneWidget);
   });
 }
